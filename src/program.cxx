fbbea3eb32bf0bee43488c3aacce0ad1c06788ad
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-02-19 16:31:51 +0100

    Added creation of grid

--- src/program.cxx
+++ src/program.cxx
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "grid.h"
 
 int main()
 {
+  // read the settings
+  // load the grid
+  grid dnsgrid;
+
+  // load the fields
+  //
+  // start the time loop
+  // 1. boundary conditions
+  // 2. advection
+  // 3. diffusion
+  // 4. gravity
+  // 5. large scale forcings
+  // 6. pressure
   std::cout << "Hello world!" << std::endl;
   return 0;
 }
