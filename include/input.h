8a4045588fb23c2de56fe8bafeb266dc48ec30aa
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-04-03 00:04:25 +0200

    And of course I forgot to add the files to the repository

---
+++ include/input.h
@@ -0,0 +1,18 @@
+#ifndef INPUT
+#define INPUT
+
+#include <map>
+#include <string>
+
+class cinput
+{
+  public:
+    cinput();
+    ~cinput();
+    int getItem(int *, std::string, std::string);
+    int getItem(double *, std::string, std::string);
+
+  private:
+    std::map<std::string, std::map<std::string, std::string> > inputlist;
+};
+#endif
---
+++ src/input.cxx
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <map>
+#include <sstream>
+#include "input.h"
+
+cinput::cinput()
+{
+  std::printf("Creating instance of object input\n");
+
+  // construct input list before implementing file reading
+  // setup Moser 180 case
+  inputlist["grid"]["itot"] = "64";
+  inputlist["grid"]["jtot"] = "64";
+  inputlist["grid"]["ktot"] = "64";
+
+  inputlist["grid"]["xsize"] = "6.28";
+  inputlist["grid"]["ysize"] = "3.14";
+  inputlist["grid"]["zsize"] = "2.";
+
+  // set the time properties
+  inputlist["time"]["runtime"] = "10000";
+  inputlist["time"]["cflmax" ] = "0.8";
+  // end setup Moser case
+}
+
+cinput::~cinput()
+{
+  std::printf("Destroying instance of object input\n");
+}
+
+// overloaded return functions
+int cinput::getItem(int *value, std::string cat, std::string item)
+{
+  std::stringstream ss(std::string(inputlist[cat][item]));
+  ss >> *value;
+
+  return 0;
+}
+
+int cinput::getItem(double *value, std::string cat, std::string item)
+{
+  std::stringstream ss(std::string(inputlist[cat][item]));
+  ss >> *value;
+
+  return 0;
+}
+
