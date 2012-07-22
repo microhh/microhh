cee2bd0b9a7ca7445534fee1fdf8d9d083f5da3d
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-07-22 14:32:35 +0200

    Added the 42 scheme to the taylor green vortex test

--- cases/taylorgreen/run.sh
+++ cases/taylorgreen/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2nd order scheme
 cd taylorgreen32_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
@@ -22,6 +23,35 @@ rm -f *.00* taylorgreen.out
 cd ..
 
 cd taylorgreen256_2nd
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+# 42 order scheme
+cd taylorgreen32_42
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen64_42
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen128_42
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen256_42
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
 ../init taylorgreen
--- cases/taylorgreen/taylorgreenconv.py
+++ cases/taylorgreen/taylorgreenconv.py
@@ -5,6 +5,10 @@ t    = 400
 time = 1.
 visc = (8.*pi**2. * 100.)**(-1.)
 
+ns    = array([32, 64, 128, 256])
+dxs   = 1./ns
+
+# 2nd order data
 data32_2nd  = microhh(t,  32,  16, 'taylorgreen32_2nd' )
 data64_2nd  = microhh(t,  64,  32, 'taylorgreen64_2nd' )
 data128_2nd = microhh(t, 128,  64, 'taylorgreen128_2nd')
@@ -20,8 +24,6 @@ err64_2nd  = geterror(data64_2nd , ref64_2nd )
 err128_2nd = geterror(data128_2nd, ref128_2nd)
 err256_2nd = geterror(data256_2nd, ref256_2nd)
 
-ns    = array([32, 64, 128, 256])
-dxs   = 1./ns
 errsu_2nd = array([err32_2nd.u, err64_2nd.u, err128_2nd.u, err256_2nd.u])
 errsw_2nd = array([err32_2nd.w, err64_2nd.w, err128_2nd.w, err256_2nd.w])
 errsp_2nd = array([err32_2nd.p, err64_2nd.p, err128_2nd.p, err256_2nd.p])
@@ -31,6 +33,32 @@ if(t > 0):
   print('convergence u_2nd', (log(errsu_2nd[-1])-log(errsu_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
   print('convergence w_2nd', (log(errsw_2nd[-1])-log(errsw_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
 print('convergence p_2nd', (log(errsp_2nd[-1])-log(errsp_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
+
+# 42 order data
+data32_42  = microhh(t,  32,  16, 'taylorgreen32_42' )
+data64_42  = microhh(t,  64,  32, 'taylorgreen64_42' )
+data128_42 = microhh(t, 128,  64, 'taylorgreen128_42')
+data256_42 = microhh(t, 256, 128, 'taylorgreen256_42')
+
+ref32_42  = getref(data32_42 .x, data32_42 .xh, data32_42 .z, data32_42 .zh, visc, time)
+ref64_42  = getref(data64_42 .x, data64_42 .xh, data64_42 .z, data64_42 .zh, visc, time)
+ref128_42 = getref(data128_42.x, data128_42.xh, data128_42.z, data128_42.zh, visc, time)
+ref256_42 = getref(data256_42.x, data256_42.xh, data256_42.z, data256_42.zh, visc, time)
+
+err32_42  = geterror(data32_42 , ref32_42 )
+err64_42  = geterror(data64_42 , ref64_42 )
+err128_42 = geterror(data128_42, ref128_42)
+err256_42 = geterror(data256_42, ref256_42)
+
+errsu_42 = array([err32_42.u, err64_42.u, err128_42.u, err256_42.u])
+errsw_42 = array([err32_42.w, err64_42.w, err128_42.w, err256_42.w])
+errsp_42 = array([err32_42.p, err64_42.p, err128_42.p, err256_42.p])
+
+print('errors p_42', errsp_42)
+if(t > 0):
+  print('convergence u_42', (log(errsu_42[-1])-log(errsu_42[0])) / (log(dxs[-1])-log(dxs[0])) )
+  print('convergence w_42', (log(errsw_42[-1])-log(errsw_42[0])) / (log(dxs[-1])-log(dxs[0])) )
+print('convergence p_42', (log(errsp_42[-1])-log(errsp_42[0])) / (log(dxs[-1])-log(dxs[0])) )
 
 off2 = 0.01
 off4 = 0.002
@@ -43,7 +71,10 @@ figure()
 if(t > 0):
   loglog(dxs, errsu_2nd, 'bo-', label="u_2nd")
   loglog(dxs, errsw_2nd, 'bv-', label="w_2nd")
+  loglog(dxs, errsu_42 , 'go-', label="u_42" )
+  loglog(dxs, errsw_42 , 'gv-', label="w_42" )
 loglog(dxs, errsp_2nd, 'b^-', label="p_2nd")
+loglog(dxs, errsp_42 , 'g^-', label="p_42" )
 loglog(dxs, slope2, 'k--', label="2nd")
 loglog(dxs, slope4, 'k:' , label="4th")
 legend(loc=4, frameon=False)
