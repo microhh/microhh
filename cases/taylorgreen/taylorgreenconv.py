108919439783c475bbb953fa0e500d3e5ab02197
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-07-22 14:23:25 +0200

    Prepared plot script for high order schemes in taylor green vortex

--- cases/taylorgreen/taylorgreenconv.py
+++ cases/taylorgreen/taylorgreenconv.py
@@ -5,32 +5,32 @@ t    = 400
 time = 1.
 visc = (8.*pi**2. * 100.)**(-1.)
 
-data32  = microhh(t,  32,  16, 'taylorgreen32_2nd' )
-data64  = microhh(t,  64,  32, 'taylorgreen64_2nd' )
-data128 = microhh(t, 128,  64, 'taylorgreen128_2nd')
-data256 = microhh(t, 256, 128, 'taylorgreen256_2nd')
+data32_2nd  = microhh(t,  32,  16, 'taylorgreen32_2nd' )
+data64_2nd  = microhh(t,  64,  32, 'taylorgreen64_2nd' )
+data128_2nd = microhh(t, 128,  64, 'taylorgreen128_2nd')
+data256_2nd = microhh(t, 256, 128, 'taylorgreen256_2nd')
 
-ref32  = getref(data32 .x, data32 .xh, data32 .z, data32 .zh, visc, time)
-ref64  = getref(data64 .x, data64 .xh, data64 .z, data64 .zh, visc, time)
-ref128 = getref(data128.x, data128.xh, data128.z, data128.zh, visc, time)
-ref256 = getref(data256.x, data256.xh, data256.z, data256.zh, visc, time)
+ref32_2nd  = getref(data32_2nd .x, data32_2nd .xh, data32_2nd .z, data32_2nd .zh, visc, time)
+ref64_2nd  = getref(data64_2nd .x, data64_2nd .xh, data64_2nd .z, data64_2nd .zh, visc, time)
+ref128_2nd = getref(data128_2nd.x, data128_2nd.xh, data128_2nd.z, data128_2nd.zh, visc, time)
+ref256_2nd = getref(data256_2nd.x, data256_2nd.xh, data256_2nd.z, data256_2nd.zh, visc, time)
 
-err32  = geterror(data32 , ref32 )
-err64  = geterror(data64 , ref64 )
-err128 = geterror(data128, ref128)
-err256 = geterror(data256, ref256)
+err32_2nd  = geterror(data32_2nd , ref32_2nd )
+err64_2nd  = geterror(data64_2nd , ref64_2nd )
+err128_2nd = geterror(data128_2nd, ref128_2nd)
+err256_2nd = geterror(data256_2nd, ref256_2nd)
 
 ns    = array([32, 64, 128, 256])
 dxs   = 1./ns
-errsu = array([err32.u, err64.u, err128.u, err256.u])
-errsw = array([err32.w, err64.w, err128.w, err256.w])
-errsp = array([err32.p, err64.p, err128.p, err256.p])
+errsu_2nd = array([err32_2nd.u, err64_2nd.u, err128_2nd.u, err256_2nd.u])
+errsw_2nd = array([err32_2nd.w, err64_2nd.w, err128_2nd.w, err256_2nd.w])
+errsp_2nd = array([err32_2nd.p, err64_2nd.p, err128_2nd.p, err256_2nd.p])
 
-print('errors p', errsp)
+print('errors p_2nd', errsp_2nd)
 if(t > 0):
-  print('convergence u', (log(errsu[-1])-log(errsu[0])) / (log(dxs[-1])-log(dxs[0])) )
-  print('convergence w', (log(errsw[-1])-log(errsw[0])) / (log(dxs[-1])-log(dxs[0])) )
-print('convergence p', (log(errsp[-1])-log(errsp[0])) / (log(dxs[-1])-log(dxs[0])) )
+  print('convergence u_2nd', (log(errsu_2nd[-1])-log(errsu_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
+  print('convergence w_2nd', (log(errsw_2nd[-1])-log(errsw_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
+print('convergence p_2nd', (log(errsp_2nd[-1])-log(errsp_2nd[0])) / (log(dxs[-1])-log(dxs[0])) )
 
 off2 = 0.01
 off4 = 0.002
@@ -41,14 +41,15 @@ close('all')
 
 figure()
 if(t > 0):
-  loglog(dxs, errsu, 'bo-', label="u")
-  loglog(dxs, errsw, 'go-', label="w")
-loglog(dxs, errsp, 'ro-', label="p")
+  loglog(dxs, errsu_2nd, 'bo-', label="u_2nd")
+  loglog(dxs, errsw_2nd, 'bv-', label="w_2nd")
+loglog(dxs, errsp_2nd, 'b^-', label="p_2nd")
 loglog(dxs, slope2, 'k--', label="2nd")
 loglog(dxs, slope4, 'k:' , label="4th")
 legend(loc=4, frameon=False)
 #xlim(0.01, 0.2)
 
+"""
 figure()
 subplot(121)
 pcolormesh(data32.xh, data32.z, data32.u[:,0,:])
@@ -112,4 +113,4 @@ ylabel('z')
 title('u')
 title('p err')
 colorbar()
-
+"""
