83e24bdb4e5a3ab50f0ae0a12203943c4fad8633
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-08-07 10:30:03 +0200

    Added 2nd and 4th order conservation testing

--- cases/conservation/conservationplot.py
+++ cases/conservation/conservationplot.py
@@ -1,47 +1,84 @@
 from pylab import *
 
-data100 = loadtxt('conservation100/conservation.out', skiprows=1)
-data200 = loadtxt('conservation200/conservation.out', skiprows=1)
-data400 = loadtxt('conservation400/conservation.out', skiprows=1)
+data100_2nd = loadtxt('conservation100_2nd/conservation.out', skiprows=1)
+data200_2nd = loadtxt('conservation200_2nd/conservation.out', skiprows=1)
+data400_2nd = loadtxt('conservation400_2nd/conservation.out', skiprows=1)
 
-time100 = data100[:,1]
-mom100  = data100[:,7]
-tke100  = data100[:,8]
-mass100 = data100[:,9]
+data100_4th = loadtxt('conservation100_4th/conservation.out', skiprows=1)
+data200_4th = loadtxt('conservation200_4th/conservation.out', skiprows=1)
+data400_4th = loadtxt('conservation400_4th/conservation.out', skiprows=1)
 
-time200 = data200[:,1]
-mom200  = data200[:,7]
-tke200  = data200[:,8]
-mass200 = data200[:,9]
+time100_2nd = data100_2nd[:,1]
+mom100_2nd  = data100_2nd[:,7]
+tke100_2nd  = data100_2nd[:,8]
+mass100_2nd = data100_2nd[:,9]
 
-time400 = data400[:,1]
-mom400  = data400[:,7]
-tke400  = data400[:,8]
-mass400 = data400[:,9]
+time200_2nd = data200_2nd[:,1]
+mom200_2nd  = data200_2nd[:,7]
+tke200_2nd  = data200_2nd[:,8]
+mass200_2nd = data200_2nd[:,9]
+
+time400_2nd = data400_2nd[:,1]
+mom400_2nd  = data400_2nd[:,7]
+tke400_2nd  = data400_2nd[:,8]
+mass400_2nd = data400_2nd[:,9]
+
+time100_4th = data100_4th[:,1]
+mom100_4th  = data100_4th[:,7]
+tke100_4th  = data100_4th[:,8]
+mass100_4th = data100_4th[:,9]
+
+time200_4th = data200_4th[:,1]
+mom200_4th  = data200_4th[:,7]
+tke200_4th  = data200_4th[:,8]
+mass200_4th = data200_4th[:,9]
+
+time400_4th = data400_4th[:,1]
+mom400_4th  = data400_4th[:,7]
+tke400_4th  = data400_4th[:,8]
+mass400_4th = data400_4th[:,9]
 
 figure()
 subplot(131)
-plot(time100[1:100], mom100 [1:100] - mom100 [1], 'b-' , label = 'momdiff100 ')
-plot(time200[1:200], mom200 [1:200] - mom200 [1], 'b--', label = 'momdiff200 ')
-plot(time400[1:400], mom400 [1:400] - mom400 [1], 'b:' , label = 'momdiff400 ')
+plot(time100_2nd[1:200], mom100_2nd [1:200] - mom100_2nd [1], 'b-' , label = 'momdiff100 ')
+plot(time200_2nd[1:400], mom200_2nd [1:400] - mom200_2nd [1], 'b--', label = 'momdiff200 ')
+plot(time400_2nd[1:800], mom400_2nd [1:800] - mom400_2nd [1], 'b:' , label = 'momdiff400 ')
 legend(loc=0, frameon=False)
 subplot(132)
-plot(time100[1:100], tke100 [1:100] - tke100 [1], 'g-' , label = 'tkediff100 ')
-plot(time200[1:200], tke200 [1:200] - tke200 [1], 'g--', label = 'tkediff200 ')
-plot(time400[1:400], tke400 [1:400] - tke400 [1], 'g:' , label = 'tkediff400 ')
+plot(time100_2nd[1:200], tke100_2nd [1:200] - tke100_2nd [1], 'g-' , label = 'tkediff100 ')
+plot(time200_2nd[1:400], tke200_2nd [1:400] - tke200_2nd [1], 'g--', label = 'tkediff200 ')
+plot(time400_2nd[1:800], tke400_2nd [1:800] - tke400_2nd [1], 'g:' , label = 'tkediff400 ')
 legend(loc=0, frameon=False)
 subplot(133)
-plot(time100[1:100], mass100[1:100] - mass100[1], 'r-' , label = 'massdiff100')
-plot(time200[1:200], mass200[1:200] - mass200[1], 'r--', label = 'massdiff200')
-plot(time400[1:400], mass400[1:400] - mass400[1], 'r:' , label = 'massdiff400')
+plot(time100_2nd[1:200], mass100_2nd[1:200] - mass100_2nd[1], 'r-' , label = 'massdiff100')
+plot(time200_2nd[1:400], mass200_2nd[1:400] - mass200_2nd[1], 'r--', label = 'massdiff200')
+plot(time400_2nd[1:800], mass400_2nd[1:800] - mass400_2nd[1], 'r:' , label = 'massdiff400')
 legend(loc=0, frameon=False)
 
+figure()
+subplot(131)
+plot(time100_4th[1:200], mom100_4th [1:200] - mom100_4th [1], 'b-' , label = 'momdiff100 ')
+plot(time200_4th[1:400], mom200_4th [1:400] - mom200_4th [1], 'b--', label = 'momdiff200 ')
+plot(time400_4th[1:800], mom400_4th [1:800] - mom400_4th [1], 'b:' , label = 'momdiff400 ')
+legend(loc=0, frameon=False)
+subplot(132)
+plot(time100_4th[1:200], tke100_4th [1:200] - tke100_4th [1], 'g-' , label = 'tkediff100 ')
+plot(time200_4th[1:400], tke200_4th [1:400] - tke200_4th [1], 'g--', label = 'tkediff200 ')
+plot(time400_4th[1:800], tke400_4th [1:800] - tke400_4th [1], 'g:' , label = 'tkediff400 ')
+legend(loc=0, frameon=False)
+subplot(133)
+plot(time100_4th[1:200], mass100_4th[1:200] - mass100_4th[1], 'r-' , label = 'massdiff100')
+plot(time200_4th[1:400], mass200_4th[1:400] - mass200_4th[1], 'r--', label = 'massdiff200')
+plot(time400_4th[1:800], mass400_4th[1:800] - mass400_4th[1], 'r:' , label = 'massdiff400')
+legend(loc=0, frameon=False)
+
+
+"""
 timesteps = array([100, 200, 400])
 momerror  = array([mom100[-1]-mom100[1], mom200[-1]-mom200[1], mom400[-1]-mom400[1]])
 tkeerror  = array([tke100[-1]-tke100[1], tke200[-1]-tke200[1], tke400[-1]-tke400[1]])
 masserror = array([mass100[-1]-mass100[1], mass200[-1]-mass200[1], mass400[-1]-mass400[1]])
 
-"""
 figure()
 subplot(131)
 loglog(timesteps, abs(momerror))
--- cases/conservation/run.sh
+++ cases/conservation/run.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-cd conservation100
+cd conservation100_2nd
 python conservationprof.py
 rm -f *.00* conservation.out
 ../init conservation
 ../dns conservation
 cd ..
 
-cd conservation200
+cd conservation200_2nd
 python conservationprof.py
 rm -f *.00* conservation.out
 ../init conservation
 ../dns conservation
 cd ..
 
-cd conservation400
+cd conservation400_2nd
+python conservationprof.py
+rm -f *.00* conservation.out
+../init conservation
+../dns conservation
+cd ..
+
+cd conservation100_4th
+python conservationprof.py
+rm -f *.00* conservation.out
+../init conservation
+../dns conservation
+cd ..
+
+cd conservation200_4th
+python conservationprof.py
+rm -f *.00* conservation.out
+../init conservation
+../dns conservation
+cd ..
+
+cd conservation400_4th
 python conservationprof.py
 rm -f *.00* conservation.out
 ../init conservation
