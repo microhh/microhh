4f349e419b997372367222d87abb048c1d7a7f44
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-09-17 10:30:24 +0200

    Added a transposed version of the MPI-IO

--- test/plotdump2d.py
+++ test/plotdump2d.py
@@ -3,14 +3,15 @@ import struct
 from pylab import *
 
 # only read the first 2D slice to check the layout
-nx = 32
-ny = 32
+nx = 64
+ny = 64
+nz = 64
 
 fin = open("u.dump","rb")
-raw = fin.read(nx*ny*8)
-tmp = numpy.array(struct.unpack('{}d'.format(nx*ny), raw))
-u   = tmp.reshape((ny, nx))
+raw = fin.read(nx*ny*nz*8)
+tmp = numpy.array(struct.unpack('{}d'.format(nx*ny*nz), raw))
+u   = tmp.reshape((nz, ny, nx))
 
-pcolormesh(u)
+pcolormesh(u[0,:,:])
 colorbar()
 
