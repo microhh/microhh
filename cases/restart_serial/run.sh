6a3ffbabe6167d5137309e7a9e92db7ee1c132aa
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-10-16 17:39:20 +0200

    Add restart test for serial mode

---
+++ cases/restart_serial/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+rm -f *.000*
+rm -f *.out
+./init drycbl_flow
+./dns drycbl_flow
+mv u.0000002 u.0000002ref
+mv v.0000002 v.0000002ref
+mv w.0000002 w.0000002ref
+mv s.0000002 s.0000002ref
+./dns drycbl_flow_restart
+cmp u.0000002 u.0000002ref
+diffu=$?
+cmp v.0000002 v.0000002ref
+diffv=$?
+cmp w.0000002 w.0000002ref
+diffw=$?
+cmp s.0000002 s.0000002ref
+diffs=$?
+error=$(($diffu + $diffv + $diffw + $diffs))
+if [ $error = 0 ]; then
+  echo "TEST PASSED!"
+else
+  echo "TEST FAILED!"
+fi
+
