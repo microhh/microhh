4c945fe34acc3129ceed6c33ba1b334c631f1c52
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-10-25 00:20:20 +0200

    Add diffusion dominated case for taylor green vortex

---
+++ cases/taylorgreen_diff/run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# 2nd order scheme
+cd taylorgreen16_2nd
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen32_2nd
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen64_2nd
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen128_2nd
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+# 4th order scheme Morinishi
+cd taylorgreen16_44
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen32_44
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen64_44
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen128_44
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+# 4th order scheme
+cd taylorgreen16_4th
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen32_4th
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen64_4th
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
+
+cd taylorgreen128_4th
+python taylorgreenprof.py
+rm -f *.00* taylorgreen.out
+../init taylorgreen
+../dns taylorgreen
+cd ..
