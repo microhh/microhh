903dc64644e2f114727e8fcb3f8f3c2ac354426b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2012-11-02 16:53:47 +0100

    Plot a time convergence plot for the conservation test

--- cases/conservation/conservationplot.py
+++ cases/conservation/conservationplot.py
@@ -38,82 +38,60 @@ mom400_4th  = data400_4th[:,7] / data400_4th[1,7]
 tke400_4th  = data400_4th[:,8] / data400_4th[1,8]
 mass400_4th = data400_4th[:,9] / data400_4th[1,9]
 
-"""
 figure()
 subplot(131)
-plot(time100_2nd[1:200], mom100_2nd [1:200] - mom100_2nd [1], 'b-' , label = 'momdiff100 ')
-plot(time200_2nd[1:400], mom200_2nd [1:400] - mom200_2nd [1], 'b--', label = 'momdiff200 ')
-plot(time400_2nd[1:800], mom400_2nd [1:800] - mom400_2nd [1], 'b:' , label = 'momdiff400 ')
-legend(loc=0, frameon=False)
-subplot(132)
-plot(time100_2nd[1:200], tke100_2nd [1:200] - tke100_2nd [1], 'b-' , label = 'tkediff100 ')
-plot(time200_2nd[1:400], tke200_2nd [1:400] - tke200_2nd [1], 'b--', label = 'tkediff200 ')
-plot(time400_2nd[1:800], tke400_2nd [1:800] - tke400_2nd [1], 'b:' , label = 'tkediff400 ')
-legend(loc=0, frameon=False)
-subplot(133)
-plot(time100_2nd[1:200], mass100_2nd[1:200] - mass100_2nd[1], 'b-' , label = 'massdiff100')
-plot(time200_2nd[1:400], mass200_2nd[1:400] - mass200_2nd[1], 'b--', label = 'massdiff200')
-plot(time400_2nd[1:800], mass400_2nd[1:800] - mass400_2nd[1], 'b:' , label = 'massdiff400')
-legend(loc=0, frameon=False)
-
-figure()
-subplot(131)
-plot(time100_4th[1:200], mom100_4th [1:200] - mom100_4th [1], 'r-' , label = 'momdiff100 ')
-plot(time200_4th[1:400], mom200_4th [1:400] - mom200_4th [1], 'r--', label = 'momdiff200 ')
-plot(time400_4th[1:800], mom400_4th [1:800] - mom400_4th [1], 'r:' , label = 'momdiff400 ')
-legend(loc=0, frameon=False)
-subplot(132)
-plot(time100_4th[1:200], tke100_4th [1:200] - tke100_4th [1], 'r-' , label = 'tkediff100 ')
-plot(time200_4th[1:400], tke200_4th [1:400] - tke200_4th [1], 'r--', label = 'tkediff200 ')
-plot(time400_4th[1:800], tke400_4th [1:800] - tke400_4th [1], 'r:' , label = 'tkediff400 ')
-legend(loc=0, frameon=False)
-subplot(133)
-plot(time100_4th[1:200], mass100_4th[1:200] - mass100_4th[1], 'r-' , label = 'massdiff100')
-plot(time200_4th[1:400], mass200_4th[1:400] - mass200_4th[1], 'r--', label = 'massdiff200')
-plot(time400_4th[1:800], mass400_4th[1:800] - mass400_4th[1], 'r:' , label = 'massdiff400')
-legend(loc=0, frameon=False)
-"""
-
-figure()
-subplot(131)
-plot(time100_2nd[1: 500], mom100_2nd [1: 500] - mom100_2nd [1], 'b-' , label = 'cfl08_2nd' )
-plot(time100_4th[1: 500], mom100_4th [1: 500] - mom100_4th [1], 'r-' , label = 'cfl08_4th' )
-plot(time200_2nd[1:1000], mom200_2nd [1:1000] - mom200_2nd [1], 'b--', label = 'cfl04_2nd' )
-plot(time200_4th[1:1000], mom200_4th [1:1000] - mom200_4th [1], 'r--', label = 'cfl04_4th' )
-plot(time400_2nd[1:2000], mom400_2nd [1:2000] - mom400_2nd [1], 'b:' , label = 'cfl02_2nd' )
-plot(time400_4th[1:2000], mom400_4th [1:2000] - mom400_4th [1], 'r:' , label = 'cfl02_4th' )
+plot(time100_2nd[1: 500], mom100_2nd [1: 500] - mom100_2nd [1], 'b-' , label='cfl08_2nd')
+plot(time100_4th[1: 500], mom100_4th [1: 500] - mom100_4th [1], 'r-' , label='cfl08_4th')
+plot(time200_2nd[1:1000], mom200_2nd [1:1000] - mom200_2nd [1], 'b--', label='cfl04_2nd')
+plot(time200_4th[1:1000], mom200_4th [1:1000] - mom200_4th [1], 'r--', label='cfl04_4th')
+plot(time400_2nd[1:2000], mom400_2nd [1:2000] - mom400_2nd [1], 'b:' , label='cfl02_2nd')
+plot(time400_4th[1:2000], mom400_4th [1:2000] - mom400_4th [1], 'r:' , label='cfl02_4th')
 legend(loc=0, frameon=False)
 title('momentum conservation')
 subplot(132)
-plot(time100_2nd[1: 500], tke100_2nd [1: 500] - tke100_2nd [1], 'b-' , label = 'cfl08_2nd' )
-plot(time100_4th[1: 500], tke100_4th [1: 500] - tke100_4th [1], 'r-' , label = 'cfl08_4th' )
-plot(time200_2nd[1:1000], tke200_2nd [1:1000] - tke200_2nd [1], 'b--', label = 'cfl04_2nd' )
-plot(time200_4th[1:1000], tke200_4th [1:1000] - tke200_4th [1], 'r--', label = 'cfl04_4th' )
-plot(time400_2nd[1:2000], tke400_2nd [1:2000] - tke400_2nd [1], 'b:' , label = 'cfl02_2nd' )
-plot(time400_4th[1:2000], tke400_4th [1:2000] - tke400_4th [1], 'r:' , label = 'cfl02_4th' )
+plot(time100_2nd[1: 500], tke100_2nd [1: 500] - tke100_2nd [1], 'b-' , label='cfl08_2nd')
+plot(time100_4th[1: 500], tke100_4th [1: 500] - tke100_4th [1], 'r-' , label='cfl08_4th')
+plot(time200_2nd[1:1000], tke200_2nd [1:1000] - tke200_2nd [1], 'b--', label='cfl04_2nd')
+plot(time200_4th[1:1000], tke200_4th [1:1000] - tke200_4th [1], 'r--', label='cfl04_4th')
+plot(time400_2nd[1:2000], tke400_2nd [1:2000] - tke400_2nd [1], 'b:' , label='cfl02_2nd')
+plot(time400_4th[1:2000], tke400_4th [1:2000] - tke400_4th [1], 'r:' , label='cfl02_4th')
 legend(loc=0, frameon=False)
 title('energy conservation')
 subplot(133)
-plot(time100_2nd[1: 500], mass100_2nd[1: 500] - mass100_2nd[1], 'b-' , label = 'cfl08_2nd')
-plot(time100_4th[1: 500], mass100_4th[1: 500] - mass100_4th[1], 'r-' , label = 'cfl08_4th')
-plot(time200_2nd[1:1000], mass200_2nd[1:1000] - mass200_2nd[1], 'b--', label = 'cfl04_2nd')
-plot(time200_4th[1:1000], mass200_4th[1:1000] - mass200_4th[1], 'r--', label = 'cfl04_4th')
-plot(time400_2nd[1:2000], mass400_2nd[1:2000] - mass400_2nd[1], 'b:' , label = 'cfl02_2nd')
-plot(time400_4th[1:2000], mass400_4th[1:2000] - mass400_4th[1], 'r:' , label = 'cfl02_4th')
+plot(time100_2nd[1: 500], mass100_2nd[1: 500] - mass100_2nd[1], 'b-' , label='cfl08_2nd')
+plot(time100_4th[1: 500], mass100_4th[1: 500] - mass100_4th[1], 'r-' , label='cfl08_4th')
+plot(time200_2nd[1:1000], mass200_2nd[1:1000] - mass200_2nd[1], 'b--', label='cfl04_2nd')
+plot(time200_4th[1:1000], mass200_4th[1:1000] - mass200_4th[1], 'r--', label='cfl04_4th')
+plot(time400_2nd[1:2000], mass400_2nd[1:2000] - mass400_2nd[1], 'b:' , label='cfl02_2nd')
+plot(time400_4th[1:2000], mass400_4th[1:2000] - mass400_4th[1], 'r:' , label='cfl02_4th')
 legend(loc=0, frameon=False)
 title('mass conservation')
 
-"""
-timesteps = array([100, 200, 400])
-momerror  = array([mom100[-1]-mom100[1], mom200[-1]-mom200[1], mom400[-1]-mom400[1]])
-tkeerror  = array([tke100[-1]-tke100[1], tke200[-1]-tke200[1], tke400[-1]-tke400[1]])
-masserror = array([mass100[-1]-mass100[1], mass200[-1]-mass200[1], mass400[-1]-mass400[1]])
+tkeloss100_2nd = tke100_2nd[100] - tke100_2nd[1]
+tkeloss200_2nd = tke200_2nd[200] - tke100_2nd[1]
+tkeloss400_2nd = tke400_2nd[400] - tke100_2nd[1]
+tkeloss100_4th = tke100_4th[100] - tke100_4th[1]
+tkeloss200_4th = tke200_4th[200] - tke100_4th[1]
+tkeloss400_4th = tke400_4th[400] - tke100_4th[1]
+
+dts           = array([8., 4., 2.])
+tkelosses_2nd = array([tkeloss100_2nd, tkeloss200_2nd, tkeloss400_2nd])
+tkelosses_4th = array([tkeloss100_4th, tkeloss200_4th, tkeloss400_4th])
+
+off2 = 0.0001
+off3 = 0.00005
+off4 = 0.00002
+slope2 = off2*(dts[:] / dts[0])**2.
+slope3 = off3*(dts[:] / dts[0])**3.
+slope4 = off4*(dts[:] / dts[0])**4.
+
 
 figure()
-subplot(131)
-loglog(timesteps, abs(momerror))
-subplot(132)
-loglog(timesteps, abs(tkeerror))
-subplot(133)
-loglog(timesteps, abs(masserror))
-"""
+loglog(dts, abs(tkelosses_2nd), 'bo-', label='2ndx')
+loglog(dts, abs(tkelosses_4th), 'go-', label='4thx')
+loglog(dts, slope2, 'k--', label='2nd')
+loglog(dts, slope3, 'k:' , label='3rd')
+loglog(dts, slope4, 'k-.', label='4th')
+xlabel('dt')
+ylabel('tke loss')
+legend(loc=0, frameon=False)
