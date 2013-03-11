795e427d6ae8507b6de76e380d01ed0ff2e038ba
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2013-03-12 00:31:46 +0100

    Fixed run script of taylor green vortex for new structure

--- cases/taylorgreen/run.sh
+++ cases/taylorgreen/run.sh
@@ -4,107 +4,107 @@
 cd taylorgreen16_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen32_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen64_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen128_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen256_2nd
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 # 4th order scheme Morinishi
 cd taylorgreen16_44
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen32_44
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen64_44
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen128_44
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen256_44
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 # 4th order scheme
 cd taylorgreen16_4th
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen32_4th
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen64_4th
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen128_4th
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
 cd taylorgreen256_4th
 python taylorgreenprof.py
 rm -f *.00* taylorgreen.out
-../init taylorgreen
-../dns taylorgreen
+../microhh init taylorgreen
+../microhh run taylorgreen
 cd ..
 
