ffe8dda9f8bf546c32933c0a91d5e8a566beb095
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2013-03-21 14:04:53 +0100

    Split dns and les statistics in order to be able to run all test cases

--- include/stats.h
+++ include/stats.h
@@ -5,6 +5,8 @@
 #include "grid.h"
 #include "fields.h"
 #include "mpiinterface.h"
+#include "stats_dns.h"
+#include "stats_les.h"
 
 class cstats
 {
@@ -22,69 +24,10 @@ class cstats
     cfields *fields;
     cmpi    *mpi;
 
-    bool allocated;
-    bool initialized;
+    cstats_dns *stats_dns;
+    cstats_les *stats_les;
 
     int istats;
-
-    NcFile *dataFile;
-    NcDim  *z_dim, *zh_dim, *t_dim;
-    NcVar  *z_var, *zh_var, *t_var, *iter_var;
-    NcVar  *u_var , *v_var , *w_var , *s_var;
-    NcVar  *evisc_var;
-    NcVar  *u2_var, *v2_var, *w2_var, *s2_var;
-    NcVar  *u3_var, *v3_var, *w3_var, *s3_var;
-    NcVar  *ugrad_var, *vgrad_var, *sgrad_var;
-    NcVar  *wu_var, *wv_var, *ws_var;
-    NcVar  *udiff_var, *vdiff_var, *sdiff_var;
-    NcVar  *uflux_var, *vflux_var, *sflux_var;
-    NcVar  *u2_shear_var, *v2_shear_var, *tke_shear_var;
-    NcVar  *u2_turb_var, *v2_turb_var, *w2_turb_var, *tke_turb_var;
-    NcVar  *u2_visc_var, *v2_visc_var, *w2_visc_var, *tke_visc_var;
-    NcVar  *u2_diss_var, *v2_diss_var, *w2_diss_var, *tke_diss_var;
-    NcVar  *w2_pres_var, *tke_pres_var;
-    NcVar  *u2_rdstr_var, *v2_rdstr_var, *w2_rdstr_var;
-    NcVar  *w2_buoy_var, *tke_buoy_var;
-
-    double *u , *v , *w , *s ;
-    double *evisc;
-    double *u2, *v2, *w2, *s2;
-    double *u3, *v3, *w3, *s3;
-    double *wu , *wv , *ws ;
-    double *ugrad, *vgrad, *sgrad;
-    double *udiff, *vdiff, *sdiff;
-    double *uflux, *vflux, *sflux;
-    double *u2_shear, *v2_shear, *tke_shear;
-    double *u2_turb, *v2_turb, *w2_turb, *tke_turb;
-    double *u2_visc, *v2_visc, *w2_visc, *tke_visc;
-    double *u2_diss, *v2_diss, *w2_diss, *tke_diss;
-    double *w2_pres, *tke_pres;
-    double *u2_rdstr, *v2_rdstr, *w2_rdstr;
-    double *w2_buoy, *tke_buoy;
-
-    int calcmean     (double *, double *);
-    int calcmoment   (double *, double *, double *, double, int);
-    int calcflux     (double *, double *, double *, double *, int, int);
-    int calcdiff     (double *, double *, double *, double);
-    int calcgrad     (double *, double *, double *);
-    int calctkebudget(double *, double *, double *, double *, double *,
-                      double *, double *,
-                      double *, double *,
-                      double *, double *, double *,
-                      double *, double *, double *, double *,
-                      double *, double *, double *, double *,
-                      double *, double *, double *, double *,
-                      double *, double *,
-                      double *, double *, double *,
-                      double *, double *,
-                      double *, double *, double);
-
-    // 2nd order statistics
-    int calcdiff_les_2nd(double *, double *, double *, double *, double *, double *, double);
-    int calcgrad_2nd    (double *, double *, double *);
-    int calcflux_2nd    (double *, double *, double *, double *, int, int);
-
-    int nstats;
 };
 #endif
 
---
+++ src/stats_les.cxx
@@ -0,0 +1,420 @@
+#include <cstdio>
+#include <cmath>
+#include <netcdfcpp.h>
+#include "grid.h"
+#include "fields.h"
+#include "stats.h"
+#include "defines.h"
+
+cstats_les::cstats_les(cgrid *gridin, cfields *fieldsin, cmpi *mpiin)
+{
+  grid   = gridin;
+  fields = fieldsin;
+  mpi    = mpiin;
+
+  allocated   = false;
+  initialized = false;
+}
+
+cstats_les::~cstats_les()
+{
+  if(initialized)
+    delete dataFile;
+
+  if(allocated)
+  {
+    delete[] u;
+    delete[] v;
+    delete[] w;
+    delete[] s;
+
+    delete[] u2;
+    delete[] v2;
+    delete[] w2;
+    delete[] s2;
+
+    delete[] u3;
+    delete[] v3;
+    delete[] w3;
+    delete[] s3;
+
+    delete[] evisc;
+
+    delete[] ugrad;
+    delete[] vgrad;
+    delete[] sgrad;
+
+    delete[] wu;
+    delete[] wv;
+    delete[] ws;
+
+    delete[] udiff;
+    delete[] vdiff;
+    delete[] sdiff;
+
+    delete[] uflux;
+    delete[] vflux;
+    delete[] sflux;
+  }
+}
+
+int cstats_les::init()
+{
+  u = new double[grid->kcells];
+  v = new double[grid->kcells];
+  w = new double[grid->kcells];
+  s = new double[grid->kcells];
+
+  u2 = new double[grid->kcells];
+  v2 = new double[grid->kcells];
+  w2 = new double[grid->kcells];
+  s2 = new double[grid->kcells];
+
+  u3 = new double[grid->kcells];
+  v3 = new double[grid->kcells];
+  w3 = new double[grid->kcells];
+  s3 = new double[grid->kcells];
+
+  evisc = new double[grid->kcells];
+
+  ugrad = new double[grid->kcells];
+  vgrad = new double[grid->kcells];
+  sgrad = new double[grid->kcells];
+
+  wu = new double[grid->kcells];
+  wv = new double[grid->kcells];
+  ws = new double[grid->kcells];
+
+  udiff = new double[grid->kcells];
+  vdiff = new double[grid->kcells];
+  sdiff = new double[grid->kcells];
+
+  uflux = new double[grid->kcells];
+  vflux = new double[grid->kcells];
+  sflux = new double[grid->kcells];
+
+  allocated = true;
+
+  // set the number of stats to zero
+  nstats = 0;
+
+  return 0;
+}
+
+int cstats_les::create(int n)
+{
+  // create a NetCDF file for the statistics
+  if(mpi->mpiid == 0)
+  {
+    char filename[256];
+    std::sprintf(filename, "%s.%07d.nc", mpi->simname.c_str(), n);
+    dataFile = new NcFile(filename, NcFile::New);
+    if(!dataFile->is_valid())
+    {
+      std::printf("ERROR cannot write statistics file\n");
+      return 1;
+    }
+    else
+      initialized = true;
+
+    // create dimensions
+    z_dim  = dataFile->add_dim("z" , grid->kmax);
+    zh_dim = dataFile->add_dim("zh", grid->kmax+1);
+    t_dim  = dataFile->add_dim("t");
+
+    // create variables
+    iter_var = dataFile->add_var("iter", ncInt   , t_dim );
+    t_var    = dataFile->add_var("t"   , ncDouble, t_dim );
+    z_var    = dataFile->add_var("z"   , ncDouble, z_dim );
+    zh_var   = dataFile->add_var("zh"  , ncDouble, zh_dim);
+
+    u_var = dataFile->add_var("u", ncDouble, t_dim, z_dim );
+    v_var = dataFile->add_var("v", ncDouble, t_dim, z_dim );
+    w_var = dataFile->add_var("w", ncDouble, t_dim, zh_dim);
+    s_var = dataFile->add_var("s", ncDouble, t_dim, z_dim );
+
+    u2_var = dataFile->add_var("u2", ncDouble, t_dim, z_dim );
+    v2_var = dataFile->add_var("v2", ncDouble, t_dim, z_dim );
+    w2_var = dataFile->add_var("w2", ncDouble, t_dim, zh_dim);
+    s2_var = dataFile->add_var("s2", ncDouble, t_dim, z_dim );
+
+    u3_var = dataFile->add_var("u3", ncDouble, t_dim, z_dim );
+    v3_var = dataFile->add_var("v3", ncDouble, t_dim, z_dim );
+    w3_var = dataFile->add_var("w3", ncDouble, t_dim, zh_dim);
+    s3_var = dataFile->add_var("s3", ncDouble, t_dim, z_dim );
+
+    ugrad_var = dataFile->add_var("ugrad", ncDouble, t_dim, zh_dim );
+    vgrad_var = dataFile->add_var("vgrad", ncDouble, t_dim, zh_dim );
+    sgrad_var = dataFile->add_var("sgrad", ncDouble, t_dim, zh_dim );
+
+    wu_var = dataFile->add_var("uw", ncDouble, t_dim, zh_dim );
+    wv_var = dataFile->add_var("vw", ncDouble, t_dim, zh_dim );
+    ws_var = dataFile->add_var("sw", ncDouble, t_dim, zh_dim );
+
+    udiff_var = dataFile->add_var("udiff", ncDouble, t_dim, zh_dim );
+    vdiff_var = dataFile->add_var("vdiff", ncDouble, t_dim, zh_dim );
+    sdiff_var = dataFile->add_var("sdiff", ncDouble, t_dim, zh_dim );
+
+    uflux_var = dataFile->add_var("uflux", ncDouble, t_dim, zh_dim );
+    vflux_var = dataFile->add_var("vflux", ncDouble, t_dim, zh_dim );
+    sflux_var = dataFile->add_var("sflux", ncDouble, t_dim, zh_dim );
+
+    evisc_var = dataFile->add_var("evisc", ncDouble, t_dim, z_dim );
+
+    // save the grid variables
+    z_var ->put(&grid->z [grid->kstart], grid->kmax  );
+    zh_var->put(&grid->zh[grid->kstart], grid->kmax+1);
+
+    dataFile->sync();
+  }
+
+  return 0;
+}
+
+int cstats_les::exec(int iteration, double time)
+{
+  if(mpi->mpiid == 0) std::printf("Saving stats for iteration %d\n", iteration);
+
+  if(mpi->mpiid == 0)
+  {
+    t_var   ->put_rec(&time     , nstats);
+    iter_var->put_rec(&iteration, nstats);
+  }
+
+  // PROFILES
+  // calculate means
+  calcmean(fields->u->data, u);
+  calcmean(fields->v->data, v);
+  calcmean(fields->w->data, w);
+  calcmean(fields->s["s"]->data, s);
+  calcmean(fields->s["evisc"]->data, evisc);
+
+  // calc variances
+  calcmoment(fields->u->data, u, u2, 2., 0);
+  calcmoment(fields->v->data, v, v2, 2., 0);
+  calcmoment(fields->w->data, w, w2, 2., 1);
+  calcmoment(fields->s["s"]->data, s, s2, 2., 0);
+
+  // calc skewnesses
+  calcmoment(fields->u->data, u, u3, 3., 0);
+  calcmoment(fields->v->data, v, v3, 3., 0);
+  calcmoment(fields->w->data, w, w3, 3., 1);
+  calcmoment(fields->s["s"]->data, s, s3, 3., 0);
+
+  calcgrad(fields->u->data, ugrad, grid->dzhi);
+  calcgrad(fields->v->data, vgrad, grid->dzhi);
+  calcgrad(fields->s["s"]->data, sgrad, grid->dzhi);
+
+  // calculate turbulent fluxes
+  calcflux(fields->u->data, fields->w->data, wu, fields->s["tmp1"]->data, 1, 0);
+  calcflux(fields->v->data, fields->w->data, wv, fields->s["tmp1"]->data, 0, 1);
+  calcflux(fields->s["s"]->data, fields->w->data, ws, fields->s["tmp1"]->data, 0, 0);
+
+  // calculate diffusive fluxes
+  calcdiff(fields->u->data, fields->s["evisc"]->data, udiff, grid->dzhi, fields->u->datafluxbot, fields->u->datafluxtop, 1.);
+  calcdiff(fields->v->data, fields->s["evisc"]->data, vdiff, grid->dzhi, fields->v->datafluxbot, fields->v->datafluxtop, 1.);
+  calcdiff(fields->s["s"]->data, fields->s["evisc"]->data, sdiff, grid->dzhi, fields->s["s"]->datafluxbot, fields->s["s"]->datafluxtop, fields->tPr);
+
+  // add the turbulent and diffusive fluxes
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+  {
+    uflux[k] = wu[k] + udiff[k];
+    vflux[k] = wv[k] + vdiff[k];
+    sflux[k] = ws[k] + sdiff[k];
+  }
+
+  // sync the data
+  if(mpi->mpiid == 0) 
+    dataFile->sync();
+
+  nstats++;
+
+  return 0;
+}
+
+int cstats_les::calcmean(double * restrict data, double * restrict prof)
+{
+  int ijk,ii,jj,kk;
+
+  ii = 1;
+  jj = grid->icells;
+  kk = grid->icells*grid->jcells;
+  
+  for(int k=0; k<grid->kcells; k++)
+  {
+    prof[k] = 0.;
+    for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+      for(int i=grid->istart; i<grid->iend; i++)
+      {
+        ijk  = i + j*jj + k*kk;
+        prof[k] += data[ijk];
+      }
+  }
+
+  double n = grid->imax*grid->jmax;
+
+  for(int k=0; k<grid->kcells; k++)
+    prof[k] /= n;
+
+  grid->getprof(prof, grid->kcells);
+
+  return 0;
+}
+
+int cstats_les::calcmoment(double * restrict data, double * restrict datamean, double * restrict prof, double power, int a)
+{
+  int ijk,ii,jj,kk;
+
+  ii = 1;
+  jj = grid->icells;
+  kk = grid->icells*grid->jcells;
+  
+  for(int k=grid->kstart; k<grid->kend+a; k++)
+  {
+    prof[k] = 0.;
+    for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+      for(int i=grid->istart; i<grid->iend; i++)
+      {
+        ijk  = i + j*jj + k*kk;
+        prof[k] += std::pow(data[ijk]-datamean[k], power);
+      }
+  }
+
+  double n = grid->imax*grid->jmax;
+
+  for(int k=grid->kstart; k<grid->kend+a; k++)
+    prof[k] /= n;
+
+  grid->getprof(prof, grid->kcells);
+
+  return 0;
+}
+
+int cstats_les::calcflux(double * restrict data, double * restrict w, double * restrict prof, double * restrict tmp1, int locx, int locy)
+{
+  int ijk,ii,jj,kk;
+
+  ii = 1;
+  jj = grid->icells;
+  kk = grid->icells*grid->jcells;
+
+  // CvH create 2nd order interpolation function
+  // set a pointer to the field that contains w, either interpolated or the original
+  double * restrict calcw = w;
+  if(locx == 1)
+  {
+    grid->interpolatex_4th(tmp1, w, 0);
+    calcw = tmp1;
+  }
+  else if(locy == 1)
+  {
+    grid->interpolatey_4th(tmp1, w, 0);
+    calcw = tmp1;
+  }
+  
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+  {
+    prof[k] = 0.;
+    for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+      for(int i=grid->istart; i<grid->iend; i++)
+      {
+        ijk  = i + j*jj + k*kk;
+        prof[k] += 0.5*(data[ijk-kk]+data[ijk])*calcw[ijk];
+      }
+  }
+
+  double n = grid->imax*grid->jmax;
+
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+    prof[k] /= n;
+
+  grid->getprof(prof, grid->kcells);
+
+  return 0;
+}
+
+int cstats_les::calcgrad(double * restrict data, double * restrict prof, double * restrict dzhi)
+{
+  int ijk,ii,jj,kk;
+
+  ii = 1;
+  jj = grid->icells;
+  kk = grid->icells*grid->jcells;
+  
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+  {
+    prof[k] = 0.;
+    for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+      for(int i=grid->istart; i<grid->iend; i++)
+      {
+        ijk  = i + j*jj + k*kk;
+        prof[k] += (data[ijk]-data[ijk-kk])*dzhi[k];
+      }
+  }
+
+  double n = grid->imax*grid->jmax;
+
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+    prof[k] /= n;
+
+  grid->getprof(prof, grid->kcells);
+
+  return 0;
+}
+
+int cstats_les::calcdiff(double * restrict data, double * restrict evisc, double * restrict prof, double * restrict dzhi, double * restrict fluxbot, double * restrict fluxtop, double tPr)
+{
+  int ijk,ij,ii,jj,kk,kstart,kend;
+
+  ii = 1;
+  jj = grid->icells;
+  kk = grid->icells*grid->jcells;
+  kstart = grid->kstart;
+  kend   = grid->kend;
+
+  // CvH add horizontal interpolation for u and v and interpolate the eddy viscosity properly
+  // bottom boundary
+  prof[kstart] = 0.;
+  for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+    for(int i=grid->istart; i<grid->iend; i++)
+    {
+      ij = i + j*jj;
+      prof[kstart] += fluxbot[ij];
+    }
+
+  for(int k=grid->kstart+1; k<grid->kend; k++)
+  {
+    prof[k] = 0.;
+    for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+      for(int i=grid->istart; i<grid->iend; i++)
+      {
+        ijk  = i + j*jj + k*kk;
+        prof[k] += -0.5*(evisc[ijk-kk]+evisc[ijk])/tPr*(data[ijk]-data[ijk-kk])*dzhi[k];
+      }
+  }
+
+  // top boundary
+  prof[kend] = 0.;
+  for(int j=grid->jstart; j<grid->jend; j++)
+#pragma ivdep
+    for(int i=grid->istart; i<grid->iend; i++)
+    {
+      ij = i + j*jj;
+      prof[kend] += fluxtop[ij];
+    }
+
+  double n = grid->imax*grid->jmax;
+
+  for(int k=grid->kstart; k<grid->kend+1; k++)
+    prof[k] /= n;
+
+  grid->getprof(prof, grid->kcells);
+
+  return 0;
+}
