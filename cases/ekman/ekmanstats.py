182aae13ca9ecd7e4cdf49b9e855c76b196f66d3
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2013-08-12 18:25:45 +0200

    Added a high resolution version

--- cases/ekman/ekmanstats.py
+++ cases/ekman/ekmanstats.py
@@ -1,8 +1,6 @@
 from pylab import *
 import netCDF4
 
-# set the height
-kmax  = 64
 zsize = 5.
 
 # analytical solution
@@ -51,16 +49,30 @@ for k in range(z128.size):
   vref128[k] = ug*(    exp(-gamma*z128[k]) * sin(gamma*z128[k]))
 dz128 = zsize / 128
 
+stats256 = netCDF4.Dataset("ekman256/ekman.0000000.nc","r")
+t256 = stats256.variables["t"][:]
+z256 = stats256.variables["z"][:]
+u256 = stats256.variables["u"][:,:]
+v256 = stats256.variables["v"][:,:]
+uref256 = zeros(z256.size)
+vref256 = zeros(z256.size)
+for k in range(z256.size):
+  uref256[k] = ug*(1.- exp(-gamma*z256[k]) * cos(gamma*z256[k]))
+  vref256[k] = ug*(    exp(-gamma*z256[k]) * sin(gamma*z256[k]))
+dz256 = zsize / 256
+
 u32error  = sqrt(dz32 *sum((u32 [-1,:] - uref32 )**2.))
 u64error  = sqrt(dz64 *sum((u64 [-1,:] - uref64 )**2.))
 u128error = sqrt(dz128*sum((u128[-1,:] - uref128)**2.))
+u256error = sqrt(dz256*sum((u256[-1,:] - uref256)**2.))
 v32error  = sqrt(dz32 *sum((v32 [-1,:] - vref32 )**2.))
 v64error  = sqrt(dz64 *sum((v64 [-1,:] - vref64 )**2.))
 v128error = sqrt(dz128*sum((v128[-1,:] - vref128)**2.))
-print("L2 errors u = ", u32error, u64error, u128error)
-print("L2 errors v = ", v32error, v64error, v128error)
-print("Convergence in u = ", (log(u128error) - log(u32error)) / (log(zsize/128.) - log(zsize/32.)))
-print("Convergence in v = ", (log(v128error) - log(v32error)) / (log(zsize/128.) - log(zsize/32.)))
+v256error = sqrt(dz256*sum((v256[-1,:] - vref256)**2.))
+print("L2 errors u = ", u32error, u64error, u128error, u256error)
+print("L2 errors v = ", v32error, v64error, v128error, v256error)
+print("Convergence in u = ", (log(u256error) - log(u32error)) / (log(dz256) - log(dz32)))
+print("Convergence in v = ", (log(v256error) - log(v32error)) / (log(dz256) - log(dz32)))
 
 close('all')
 # ekman spiral
@@ -68,6 +80,7 @@ figure()
 plot(u32 [-1,:], v32 [-1,:])
 plot(u64 [-1,:], v64 [-1,:])
 plot(u128[-1,:], v128[-1,:])
+plot(u256[-1,:], v256[-1,:])
 
 # vertical profiles
 figure()
@@ -75,10 +88,12 @@ subplot(121)
 plot(u32 [-1,:], z32 , 'b-')
 plot(u64 [-1,:], z64 , 'g-')
 plot(u128[-1,:], z128, 'r-')
+plot(u256[-1,:], z256, 'c-')
 subplot(122)
 plot(v32 [-1,:], z32 , 'b-')
 plot(v64 [-1,:], z64 , 'g-')
 plot(v128[-1,:], z128, 'r-')
+plot(v256[-1,:], z256, 'c-')
 
 # errors
 figure()
@@ -86,8 +101,10 @@ subplot(121)
 plot(u32 [-1,:] - uref32 , z32 , 'b-')
 plot(u64 [-1,:] - uref64 , z64 , 'g-')
 plot(u128[-1,:] - uref128, z128, 'r-')
+plot(u256[-1,:] - uref256, z256, 'c-')
 subplot(122)
 plot(v32 [-1,:] - vref32 , z32 , 'b-')
 plot(v64 [-1,:] - vref64 , z64 , 'g-')
 plot(v128[-1,:] - vref128, z128, 'r-')
+plot(v256[-1,:] - vref256, z256, 'c-')
 
--- cases/ekman/run.sh
+++ cases/ekman/run.sh
@@ -21,3 +21,10 @@ rm -f *.0* ekman.out
 ../microhh run ekman
 cd ..
 
+cd ekman256
+python ekmanprof.py
+rm -f *.0* ekman.out
+../microhh init ekman
+../microhh run ekman
+cd ..
+
