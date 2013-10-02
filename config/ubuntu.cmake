86997bffb2a9b63e5d109a41ac68f9a6cce5dae3
Thijs Heus <thijsheus@gmail.com>
2013-10-02 15:10:41 +0200

    Add config for UniKoeln local Ubuntu build

---
+++ config/ubuntu.cmake
@@ -0,0 +1,19 @@
+# ARCH Linux
+set(CMAKE_C_COMPILER   "/usr/bin/cc")
+set(CMAKE_CXX_COMPILER "/usr/bin/c++")
+set(CXX_COMPILER_WRAPPER mpicxx)
+set(C_COMPILER_WRAPPER mpicc)
+
+set(USER_CXX_FLAGS "")
+set(USER_CXX_FLAGS_RELEASE "-O3 -ffast-math -mtune=native -march=native")
+set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(FFTW_INCLUDE_DIR   "/usr/include")
+set(FFTW_LIB           "/usr/lib/libfftw3.a")
+set(NETCDF_INCLUDE_DIR "/usr/include")
+set(NETCDF_LIB_C       "/usr/lib/libnetcdf.a")
+set(NETCDF_LIB_CPP     "/usr/lib/libnetcdf_c++.a")
+set(HDF5_LIB_1         "/usr/lib/libhdf5.a")
+set(HDF5_LIB_2         "/usr/lib/libhdf5_hl.a")
+set(SZIP_LIB           "")
+set(LIBS ${FFTW_LIB} ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
