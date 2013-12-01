b2cd578d3d686b3161210f41b318018433e8c227
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2013-12-01 21:12:33 +0100

    Cleaned up ekman case

--- cases/ekman/ekmanstats.py
+++ cases/ekman/ekmanstats.py
@@ -1,22 +1,23 @@
 from pylab import *
 import netCDF4
 
-zsize = 5.
-
-# analytical solution
-ug = 1.
-visc = 0.1
-fc = 1.
-
-gamma = (fc / (2.*visc))**.5
-ustar = (ug*(visc*fc)**.5)**.5
-h     = pi / gamma
-
+# load the data
 stats = netCDF4.Dataset("ekman.0000000.nc","r")
 t = stats.variables["t"][:]
 z = stats.variables["z"][:]
 u = stats.variables["u"][:,:]
 v = stats.variables["v"][:,:]
+
+# analytical solution
+zsize = 5.
+ug    = 1.
+visc  = 0.1
+fc    = 1.
+
+gamma = (fc / (2.*visc))**.5
+ustar = (ug*(visc*fc)**.5)**.5
+h     = pi / gamma
+
 uref = zeros(z.size)
 vref = zeros(z.size)
 for k in range(z.size):
@@ -32,13 +33,19 @@ close('all')
 figure()
 plot(u[-1,:], v[-1,:])
 plot(uref, vref, 'k--')
+xlabel('u [m/s]')
+ylabel('v [m/s]')
 
 # vertical profiles
 figure()
 subplot(121)
 plot(u[-1,:], z)
 plot(uref[:], z, 'k--')
+xlabel('u [m/s]')
+ylabel('z [m]')
 subplot(122)
 plot(v[-1,:], z)
 plot(vref[:], z, 'k--')
+xlabel('v [m/s]')
+ylabel('z [m]')
 
