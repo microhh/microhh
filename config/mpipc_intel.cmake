6acdccb9c579e22bf33ec58b6a2e481ed00c0be6
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-01-13 15:18:29 +0100

    Added config file for intel compiler on mpipc

---
+++ config/mpipc_intel.cmake
@@ -0,0 +1,19 @@
+# MPI-PC
+# set(ENV{CXX} g++) # compiler for serial build
+set(ENV{CXX} mpicxx) # compiler for parallel build
+
+set(USER_CXX_FLAGS "-restrict")
+set(USER_CXX_FLAGS_RELEASE "-xAVX -O3")
+set(USER_CXX_FLAGS_DEBUG "-traceback -check=conversions,stack,uninit -check-pointers=rw -check-pointers-dangling=all-check-pointers-undimensioned -fp-stack-check -fp-trap=common -fp-trap-all=common")
+
+set(FFTW_INCLUDE_DIR   "/sw/squeeze-x64/numerics/fftw-3.3-openmp-gccsys/include")
+set(FFTW_LIB           "/sw/squeeze-x64/numerics/fftw-3.3-openmp-gccsys/lib/libfftw3.a")
+set(NETCDF_INCLUDE_DIR "/sw/squeeze-x64/netcdf-latest-static-intel13/include")
+set(NETCDF_LIB_C       "/sw/squeeze-x64/netcdf-latest-static-intel13/lib/libnetcdf.a")
+set(NETCDF_LIB_CPP     "/sw/squeeze-x64/netcdf-latest-static-intel13/lib/libnetcdf_c++.a")
+set(HDF5_LIB_1         "/sw/squeeze-x64/hdf5-latest-static/lib/libhdf5.a")
+set(HDF5_LIB_2         "/sw/squeeze-x64/hdf5-latest-static/lib/libhdf5_hl.a")
+set(SZIP_LIB           "/sw/squeeze-x64/szip-latest-static/lib/libsz.a")
+set(LIBS ${FFTW_LIB} ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
+
+add_definitions(-DRESTRICTKEYWORD=restrict)
