05e8c16ad2b29d84a7407227fa531fdef65f2229
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-02-11 21:04:10 +0100

    Removed unused variables from the dry_thermo

--- src/thermo_dry.cxx
+++ src/thermo_dry.cxx
@@ -79,7 +79,6 @@ int cthermo_dry::getbuoyancysurf(cfield3d *bfield)
 int cthermo_dry::calcbuoyancy(double * restrict b, double * restrict s)
 {
   int ijk,jj,kk;
-  double ql;
   jj = grid->icells;
   kk = grid->icells*grid->jcells;
 
@@ -122,7 +121,7 @@ int cthermo_dry::calcbuoyancybot(double * restrict b , double * restrict bbot,
 
 int cthermo_dry::calcbuoyancyfluxbot(double * restrict bfluxbot, double * restrict sfluxbot)
 {
-  int ij,jj,kk;
+  int ij,jj;
   jj = grid->icells;
 
   double gravitybeta = this->gravitybeta;
