a5bafc735a79776d0fe2d12fcc26882f74b16975
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-02-22 18:12:40 +0100

    Updated the drycblles case

--- cases/drycblles/drycbllesprof.py
+++ cases/drycblles/drycbllesprof.py
@@ -8,19 +8,19 @@ dz = zsize / kmax
 dthetadz = 0.003
 
 # set the height
-z   = numpy.linspace(0.5*dz, zsize-0.5*dz, kmax)
-s   = numpy.zeros(numpy.size(z))
-sls = numpy.zeros(numpy.size(z))
-wls = numpy.zeros(numpy.size(z))
+z    = numpy.linspace(0.5*dz, zsize-0.5*dz, kmax)
+th   = numpy.zeros(numpy.size(z))
+thls = numpy.zeros(numpy.size(z))
+wls  = numpy.zeros(numpy.size(z))
 
 for k in range(kmax):
-  s  [k] = dthetadz*z[k]
-  sls[k] = 2.*(z[k]/zsize - 0.5) / 3600.
-  wls[k] = -0.01*(z[k]/zsize)
+  th  [k] = 300. + dthetadz*z[k]
+  thls[k] = 2.*(z[k]/zsize - 0.5) / 3600.
+  wls [k] = -0.01*(z[k]/zsize)
 
 # write the data to a file
 proffile = open('drycblles.prof','w')
-proffile.write('{0:^20s} {1:^20s} {2:^20s} {3:^20s}\n'.format('z','s','sls', 'wls'))
+proffile.write('{0:^20s} {1:^20s} {2:^20s} {3:^20s}\n'.format('z','th','thls', 'wls'))
 for k in range(kmax):
-  proffile.write('{0:1.14E} {1:1.14E} {2:1.14E} {3:1.14E}\n'.format(z[k], s[k], sls[k], wls[k]))
+  proffile.write('{0:1.14E} {1:1.14E} {2:1.14E} {3:1.14E}\n'.format(z[k], th[k], thls[k], wls[k]))
 proffile.close()
