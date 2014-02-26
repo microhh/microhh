dee55f6c6290ec719d618437d36d6ca4d67b1482
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-02-26 18:59:46 +0100

    Fixed all the restart tests

--- cases/restart_parallel/run.sh
+++ cases/restart_parallel/run.sh
@@ -6,7 +6,7 @@ mpiexec -n 4 ./microhh run drycbl_flow
 mv u.0000002 u.0000002ref
 mv v.0000002 v.0000002ref
 mv w.0000002 w.0000002ref
-mv s.0000002 s.0000002ref
+mv b.0000002 b.0000002ref
 mpiexec -n 4 ./microhh run drycbl_flow_restart
 cmp u.0000002 u.0000002ref
 diffu=$?
@@ -14,7 +14,7 @@ cmp v.0000002 v.0000002ref
 diffv=$?
 cmp w.0000002 w.0000002ref
 diffw=$?
-cmp s.0000002 s.0000002ref
+cmp b.0000002 b.0000002ref
 diffs=$?
 error=$(($diffu + $diffv + $diffw + $diffs))
 if [ $error = 0 ]; then
