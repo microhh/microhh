fcd6eca5c080b7edcde4ab89755d55fd1624ab21
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-02-26 18:52:26 +0100

    Fixed the restart LES cases

--- cases/restart_serial_les/run.sh
+++ cases/restart_serial_les/run.sh
@@ -7,7 +7,7 @@ rm -f *.out
 mv u.0003600 u.0003600ref
 mv v.0003600 v.0003600ref
 mv w.0003600 w.0003600ref
-mv s.0003600 s.0003600ref
+mv th.0003600 th.0003600ref
 mv obuk.0003600 obuk.0003600ref
 ./microhh run drycblles_restart
 cmp u.0003600 u.0003600ref
@@ -16,7 +16,7 @@ cmp v.0003600 v.0003600ref
 diffv=$?
 cmp w.0003600 w.0003600ref
 diffw=$?
-cmp s.0003600 s.0003600ref
+cmp th.0003600 th.0003600ref
 diffs=$?
 cmp obuk.0003600 obuk.0003600ref
 diffobuk=$?
