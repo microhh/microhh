0c3e13d1881cbe6d6cc3a3aa50f6f791e575ee4f
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-04-14 23:58:27 +0200

    Add the APE budget to the plotting script of drycbl

--- cases/drycbl/ebudget.py
+++ cases/drycbl/ebudget.py
@@ -31,6 +31,13 @@ bpe_visc = average(stats.variables["bpe_visc"][start:end,:], 0)
 bpe_diss = average(stats.variables["bpe_diss"][start:end,:], 0)
 bpe_sum  = bpe_turb + bpe_visc + bpe_diss
 
+ape_turb = pe_turb - bpe_turb
+ape_visc = pe_visc - bpe_visc
+ape_diss = -bpe_diss
+ape_bous = pe_bous
+ape_buoy = pe_buoy
+ape_sum  = ape_turb + ape_visc + ape_diss + ape_bous + ape_buoy
+
 # enable LaTeX plotting
 rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
@@ -51,7 +58,7 @@ ylim(0,0.5)
 figure()
 plot(pe_turb, z, 'b-', label='turb')
 plot(pe_visc, z, 'g-', label='visc')
-plot(pe_bous, z, 'r-', label='bous')
+plot(pe_bous, z, 'm-', label='bous')
 plot(pe_buoy, z, 'c-', label='buoy')
 plot(pe_sum , z, 'k:', label='sum' )
 xlabel(r'de$_p$/dt [m$^2$~s$^{-3}$]')
@@ -64,6 +71,18 @@ plot(bpe_turb, z, 'b-', label='turb')
 plot(bpe_visc, z, 'g-', label='visc')
 plot(bpe_diss, z, 'r-', label='diss')
 plot(bpe_sum , z, 'k:', label='sum' )
+xlabel(r'de$_p$/dt [m$^2$~s$^{-3}$]')
+ylabel(r'z [m]')
+legend(loc=0, frameon=False)
+ylim(0,0.5)
+
+figure()
+plot(ape_turb, z, 'b-', label='turb')
+plot(ape_visc, z, 'g-', label='visc')
+plot(ape_bous, z, 'm-', label='bous')
+plot(ape_buoy, z, 'c-', label='buoy')
+plot(ape_diss, z, 'r-', label='diss')
+plot(ape_sum , z, 'k:', label='sum' )
 xlabel(r'de$_p$/dt [m$^2$~s$^{-3}$]')
 ylabel(r'z [m]')
 legend(loc=0, frameon=False)
