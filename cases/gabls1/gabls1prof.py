aac136449cbc4ca70117d5cf1bac744b3521a29e
Bart van Stratum <bart@vanstratum.com>
2014-06-04 11:34:43 +0200

    Read kmax and zsize for profile generation from .ini file

--- cases/gabls1/gabls1prof.py
+++ cases/gabls1/gabls1prof.py
@@ -1,8 +1,14 @@
 import numpy
 
+# Get number of vertical levels from .ini file
+with open('gabls1.ini') as f:
+  for line in f:
+    if(line.split('=')[0]=='ktot'):
+      kmax = int(line.split('=')[1])
+    if(line.split('=')[0]=='zsize'):
+      zsize = float(line.split('=')[1])
+
 # set the height
-kmax  = 64
-zsize = 400.
 dz = zsize / kmax
 
 dthetadz = 0.01
