61cc489c5b78a497b22c6d660ce0ef36f6d0f89e
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-08-11 16:34:26 +0200

    Added a few plotting scripts

--- cases/rayleightaylor/makeanimframes2.py
+++ cases/rayleightaylor/makeanimframes2.py
@@ -1,30 +1,46 @@
+# use the Agg backend to have fast processing without X11
+import matplotlib
+matplotlib.use('Agg')
 import netCDF4
 from pylab import *
 from scipy.interpolate import RectBivariateSpline
 
 # parameters
 kmin = 0
-kmax = 900
+kmax = 1024
 
-smin = -8.
-smax = 13.
+smin = -6.5
+smax = 11.25
 
-iterstart = 100
-iterstep  = 100
-iterend   = 60000
+iterstart = 0
+iterstep  = 1
+iterend   = 1000
 
 noborder = True
 
 niter = (iterend-iterstart) / iterstep + 1
-ioff()
 
 # transition data
-stats = netCDF4.Dataset("slngrad.xzcross.nc","r")
+stats = netCDF4.Dataset("slngrad.xz.00000.nc","r")
 z     = stats.variables["z"][kmin:kmax]
 x     = stats.variables["x"][:]
 s     = stats.variables["slngrad"][-1,kmin:kmax,:]
 
-# color scheme black/blue/yellow/red
+# Black/Red/Yellow/White color scheme
+# create color map
+cdict = {'red':   (( 0.00, 0.00, 0.00),
+                   ( 0.70, 0.50, 0.50),
+                   ( 0.94, 1.00, 1.00),
+                   ( 1.00, 1.00, 1.00)),
+         'green': (( 0.00, 0.00, 0.00),
+                   ( 0.70, 0.00, 0.00),
+                   ( 0.94, 1.00, 1.00),
+                   ( 1.00, 1.00, 1.00)),
+         'blue':  (( 0.00, 0.00, 0.00),
+                   ( 0.70, 0.00, 0.00),
+                   ( 0.94, 0.40, 0.40),
+                   ( 1.00, 1.00, 1.00))}
+"""
 cdict = {'red':   (( 0.00, 0    , 0    ),
                    ( 0.36, 0    , 0    ),
                    ( 0.59, 0    , 0    ),
@@ -43,6 +59,7 @@ cdict = {'red':   (( 0.00, 0    , 0    ),
                    ( 0.75, 0    , 0    ),
                    ( 0.96, 0    , 0    ),
                    ( 1.00, 0    , 0    ))}
+"""
 my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',cdict,256)
 
 # enable LaTeX plotting
@@ -51,31 +68,22 @@ my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap',cdict,256)
 rc('font',**{'family':'serif','serif':['Palatino']})
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
+ioff()
 
-# create interpolation function
-fint = RectBivariateSpline(z,x,s)
-del(s)
-# create interpolated value
-zint = linspace(z.min(), z.max(), 1680)
-sint = fint(zint,x)
-
-ext = [x.min(), x.max(), zint.min(), zint.max()]
+ext = [x.min(), x.max(), z.min(), z.max()]
 
 for i in range(niter):
   s = stats.variables["slngrad"][i,kmin:kmax,:]
   iter = iterstart + i*iterstep
   print("Processing iteration: ", iter)
-  fint = RectBivariateSpline(z,x,s)
-  sint = fint(zint,x)
-  del(s)
 
   close('all')
-  fig1 = figure(figsize=(19.2,19.2*(zint.max()-zint.min())/(x.max()-x.min())))
+  fig1 = figure(figsize=(20.48,10.24))
   if(noborder):
     fig1.subplots_adjust(0.000,0.000,1.0,1.0,0.,0.)
   else:
     fig1.subplots_adjust(0.05,0.05,0.97,0.97,0,0)
-  plt = imshow(sint, origin='lower', extent=ext, cmap=my_cmap, vmin=smin, vmax=smax)
+  plt = imshow(s, origin='lower', extent=ext, cmap=my_cmap, vmin=smin, vmax=smax)
   #colorbar(pad=0.02)
   xlabel(r'$x$')
   ylabel(r'$z$')
@@ -86,5 +94,5 @@ for i in range(niter):
     plt.axes.get_yaxis().set_visible(False)
     plt.axes.get_yaxis().set_visible(False)
   savefig('figs/slngrad.{:07d}.png'.format(iter), facecolor='black', edgecolor='none', dpi=100)
-  del(fint, sint)
+  del(s)
 
