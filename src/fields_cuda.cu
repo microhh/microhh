0f25191a86789a84b65bd75cbc3e5ecd31915e77
Chiel <chielvanheerwaarden@gmail.com>
2014-08-13 18:11:20 +0200

    Testing CUDA aware MPI

--- src/fields_cuda.cu
+++ src/fields_cuda.cu
@@ -3,6 +3,15 @@
 
 int cfields::prepareGPU()
 {
+  double *a;
+
   const int nmemsize = grid->ncells*sizeof(double);
+
+  cudaMalloc((void**)&a, nmemsize);
+  cudaMemcpy(a, sp["th"]->data, nmemsize, cudaMemcpyHostToDevice);
+
+  // test CUDA-aware MPI
+  grid->boundary_cyclic(a);
+
   return 0;
 }
