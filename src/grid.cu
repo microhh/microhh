166011013dbd872a468c71caf58ab7ca9be1dd80
Bart van Stratum <bart@vanstratum.com>
2014-08-30 14:27:26 +0200

    Test with manual (and slow) cyclic boundaries on GPU to bypass MPI call

--- src/grid.cu
+++ src/grid.cu
@@ -1,5 +1,53 @@
 #include "grid.h"
 
+__global__ void grid_cyclic(double * __restrict__ data, 
+                            const int icells, const int jcells, const int kcells,
+                            const int istart, const int jstart,
+                            const int iend,   const int jend, 
+                            const int igc,    const int jgc)
+{
+  const int i = blockIdx.x*blockDim.x + threadIdx.x;
+  const int j = blockIdx.y*blockDim.y + threadIdx.y;
+  const int k = blockIdx.z;
+
+  const int jj = icells;
+  const int kk = icells*jcells;
+
+  int ijk0 = i          + j*jj + k*kk;
+  int ijk1 = iend-igc+i + j*jj + k*kk;
+
+  // East-west
+  if(k < kcells && j < jcells && i < igc)
+  {
+    ijk0 = i          + j*jj + k*kk;
+    ijk1 = iend-igc+i + j*jj + k*kk;
+    data[ijk0] = data[ijk1];
+  }
+
+  if(k < kcells && j < jcells && i < igc)
+  {
+    ijk0 = i+iend   + j*jj + k*kk;
+    ijk1 = i+istart + j*jj + k*kk;
+    data[ijk0] = data[ijk1];
+  }
+
+  // North-east
+  if(k < kcells && j < jgc && i < icells)
+  {
+    ijk0 = i + j           *jj + k*kk;
+    ijk1 = i + (jend-jgc+j)*jj + k*kk;
+    data[ijk0] = data[ijk1];
+  }
+
+  if(k < kcells && j < jgc && i < icells)
+  {
+    ijk0 = i + (j+jend  )*jj + k*kk;
+    ijk1 = i + (j+jstart)*jj + k*kk;
+    data[ijk0] = data[ijk1];
+  }
+
+}
+
 int cgrid::prepareGPU()
 {
   const int kmemsize = kcells*sizeof(double);
@@ -18,5 +66,23 @@ int cgrid::prepareGPU()
   cudaMemcpy(dzi4_g , dzi4 , kmemsize, cudaMemcpyHostToDevice);
   cudaMemcpy(dzhi4_g, dzhi4, kmemsize, cudaMemcpyHostToDevice);
 
+  return 0;
+}
+
+int cgrid::boundary_cyclic_gpu(double * data)
+{
+  const int blocki  = 128;
+  const int blockj  = 2;
+  const int gridi   = icells/blocki + (icells%blocki > 0);
+  const int gridj   = jcells/blockj + (jcells%blockj > 0);
+
+  dim3 gridGPU (gridi, gridj, kcells);
+  dim3 blockGPU(blocki, blockj, 1);
+
+  grid_cyclic<<<gridGPU,blockGPU>>>(data, icells, jcells, kcells,
+                                    istart, jstart,
+                                    iend,   jend,
+                                    igc,    jgc);
+ 
   return 0;
 }
