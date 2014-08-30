16d5fdbda887f8db05615cff6f9556199dd0f1db
Bart van Stratum <bart@vanstratum.com>
2014-08-30 22:05:28 +0200

    update HALO config

--- config/halo.cmake
+++ config/halo.cmake
@@ -22,8 +22,9 @@ set(SZIP_LIB           "/sw/centos58-x64/szip-latest-static/lib/libsz.a")
 set(LIBS ${FFTW_LIB} ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
 
 if(USECUDA)
-  #set(CUDA_PROPAGATE_HOST_FLAGS OFF)
-  set(CUDA_NVCC_FLAGS "-arch=sm_20")
+  set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+  set(LIBS ${LIBS} -rdynamic /usr/local/cuda/lib64/libcufft.so)
+  set(USER_CUDA_NVCC_FLAGS "-arch=sm_20")
 endif()
 
 add_definitions(-DRESTRICTKEYWORD=__restrict__)
