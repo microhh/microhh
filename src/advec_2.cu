08c6fc13676ee8abdbbcb2bd6c1c6da34cd7e8d8
Bart van Stratum <bart@vanstratum.com>
2014-09-02 17:03:54 +0200

    Minor cleanup of the shared memory version. I hoped that this would work better with more continuous reads from global memory (answer is no)

--- src/advec_2.cu
+++ src/advec_2.cu
@@ -136,113 +136,85 @@ __global__ void advec_2_advecuvw(double * __restrict__ ut, double * __restrict__
 }
 
 // !!!!!!!!!! at the moment (for testing) this only works if itot & jtot are multiple of blockdims !!!!!!!! 
-__global__ void advec_2_advecuvw_smem(double * __restrict__ ut, double * __restrict__ vt, double * __restrict__ wt, 
-                                      double * __restrict__ u,  double * __restrict__ v,  double * __restrict__ w,
-                                      double * __restrict__ dzi, double * __restrict__ dzhi, double dxi, double dyi, 
-                                      int jj, int kk, int istart, int jstart, int kstart,
-                                      int iend, int jend, int kend, int igc, int jgc)
-{
-  extern __shared__ double shared[];
-
-  int i = blockIdx.x*blockDim.x + threadIdx.x + istart;
-  int j = blockIdx.y*blockDim.y + threadIdx.y + jstart;
-  int k = blockIdx.z + kstart;
-
-  if(i < iend && j < jend && k < kend)
-  {
-    const int of = (blockDim.x + 2 * igc) * (blockDim.y + 2 * jgc); // Stride per variable in __shared__
-    const int ii = 1; // Stride distance x in __global__ and __shared__
-    const int jjs = blockDim.x + 2 * igc; // Stride distance y in __shared__
-    const int ijk = i + j*jj + k*kk; // Index in __global__ array
-    const int ijs = threadIdx.x + igc + (blockDim.x + 2 * igc) * (threadIdx.y + jgc); // Index in __shared__ array
-
-    double* us = (double*)shared; 
-    double* vs = (double*)&shared[1*of];
-    double* ws = (double*)&shared[2*of];
-
-    // Each thread read variable into shared array
-    us[ijs] = u[ijk];
-    vs[ijs] = v[ijk];
-    ws[ijs] = w[ijk];
-
-    // Some threads read ghost cells (how efficent is this?)
-    if(threadIdx.x == 0 && threadIdx.y == 0) {
-      us[ijs-ii-jjs] = u[ijk-ii-jj]; 
-      vs[ijs-ii-jjs] = v[ijk-ii-jj]; 
-      ws[ijs-ii-jjs] = w[ijk-ii-jj]; 
-    }
-    if(threadIdx.x == 0 && threadIdx.y == blockDim.y-1) {
-      us[ijs-ii+jjs] = u[ijk-ii+jj]; 
-      vs[ijs-ii+jjs] = v[ijk-ii+jj]; 
-      ws[ijs-ii+jjs] = w[ijk-ii+jj]; 
-    }
-    if(threadIdx.x == blockDim.x-1 && threadIdx.y == 0) {
-      us[ijs+ii-jjs] = u[ijk+ii-jj]; 
-      vs[ijs+ii-jjs] = v[ijk+ii-jj]; 
-      ws[ijs+ii-jjs] = w[ijk+ii-jj]; 
-    }
-    if(threadIdx.x == blockDim.x-1 && threadIdx.y == blockDim.y-1) {
-      us[ijs+ii+jjs] = u[ijk+ii+jj]; 
-      vs[ijs+ii+jjs] = v[ijk+ii+jj]; 
-      ws[ijs+ii+jjs] = w[ijk+ii+jj]; 
-    }
-    if(threadIdx.x == 0) {
-      us[ijs-ii]  = u[ijk-ii];
-      vs[ijs-ii]  = v[ijk-ii];
-      ws[ijs-ii]  = w[ijk-ii];
-    } 
-    if(threadIdx.x == blockDim.x-1) {
-      us[ijs+ii]  = u[ijk+ii]; 
-      vs[ijs+ii]  = v[ijk+ii]; 
-      ws[ijs+ii]  = w[ijk+ii]; 
-    }
-    if(threadIdx.y == 0) {
-      us[ijs-jjs] = u[ijk-jj]; 
-      vs[ijs-jjs] = v[ijk-jj]; 
-      ws[ijs-jjs] = w[ijk-jj]; 
-    }
-    if(threadIdx.y == blockDim.y-1) {
-      us[ijs+jjs] = u[ijk+jj]; 
-      vs[ijs+jjs] = v[ijk+jj]; 
-      ws[ijs+jjs] = w[ijk+jj]; 
-    }
-
-    __syncthreads(); 
-
-    ut[ijk] += 
-          - (  interp2(us[ijs       ], us[ijs+ii ]) * interp2(us[ijs    ], us[ijs+ii ])
-             - interp2(us[ijs-ii    ], us[ijs    ]) * interp2(us[ijs-ii ], us[ijs    ]) ) * dxi
-
-          - (  interp2(vs[ijs-ii+jjs], vs[ijs+jjs]) * interp2(us[ijs    ], us[ijs+jjs])
-             - interp2(vs[ijs-ii    ], vs[ijs    ]) * interp2(us[ijs-jjs], us[ijs    ]) ) * dyi 
-
-          - (  interp2(w[ijk-ii+kk  ], w[ijk+kk  ]) * interp2(us[ijs    ], u[ijk+kk  ])
-             - interp2(ws[ijs-ii    ], ws[ijs    ]) * interp2(u[ijk-kk  ], us[ijs    ]) ) * dzi[k];
-
-    vt[ijk] += 
-          - (  interp2(us[ijs+ii-jjs], us[ijs+ii ]) * interp2(vs[ijs    ], vs[ijs+ii ])
-             - interp2(us[ijs-jjs   ], us[ijs    ]) * interp2(vs[ijs-ii ], vs[ijs    ]) ) * dxi
-
-          - (  interp2(vs[ijs       ], vs[ijs+jjs]) * interp2(vs[ijs    ], vs[ijs+jjs])
-             - interp2(vs[ijs-jjs   ], vs[ijs    ]) * interp2(vs[ijs-jjs], vs[ijs    ]) ) * dyi
-
-          - (  interp2(w[ijk-jj+kk  ], w[ijk+kk  ]) * interp2(vs[ijs    ], v[ijk+kk  ])
-             - interp2(ws[ijs-jjs   ], ws[ijs    ]) * interp2(v[ijk-kk  ], vs[ijs    ]) ) * dzi[k];
-
-    if(k>kstart)
-    {
-      wt[ijk] += 
-            - (  interp2(u[ijk+ii-kk], us[ijs+ii ]) * interp2(ws[ijs    ], ws[ijs+ii ])
-               - interp2(u[ijk   -kk], us[ijs    ]) * interp2(ws[ijs-ii ], ws[ijs    ]) ) * dxi
-
-            - (  interp2(v[ijk+jj-kk], vs[ijs+jjs]) * interp2(ws[ijs    ], ws[ijs+jjs])
-               - interp2(v[ijk   -kk], vs[ijs    ]) * interp2(ws[ijs-jjs], ws[ijs    ]) ) * dyi
-
-            - (  interp2(ws[ijs     ], w[ijk+kk  ]) * interp2(ws[ijs    ], w[ijk+kk  ])
-               - interp2(w[ijk-kk   ], ws[ijs    ]) * interp2(w[ijk-kk  ], ws[ijs    ]) ) * dzhi[k];
-    }
-  }
-}
+//__global__ void advec_2_advecuvw_smem(double * __restrict__ ut, double * __restrict__ vt, double * __restrict__ wt, 
+//                                      double * __restrict__ u,  double * __restrict__ v,  double * __restrict__ w,
+//                                      double * __restrict__ dzi, double * __restrict__ dzhi, double dxi, double dyi, 
+//                                      int jj, int kk, int istart, int jstart, int kstart,
+//                                      int iend, int jend, int kend, int igc, int jgc, 
+//                                      int nloads)
+//{
+//  extern __shared__ double shared[];
+//
+//  int i = blockIdx.x*blockDim.x + threadIdx.x + istart;
+//  int j = blockIdx.y*blockDim.y + threadIdx.y + jstart;
+//  int k = blockIdx.z + kstart;
+//
+//  if(i < iend && j < jend && k < kend)
+//  {
+//    const int ijk    = i + j*jj + k*kk; // Index in __global__ array
+//    const int ijs    = threadIdx.x + igc + (blockDim.x + 2 * igc) * (threadIdx.y + jgc); // Location ijk in __shared__ array
+//    const int bdimxp = blockDim.x + 2 * igc; // Block size X with padding
+//    const int bdimyp = blockDim.y + 2 * jgc; // Block size X with padding
+//    const int of = (blockDim.x + 2 * igc) * (blockDim.y + 2 * jgc); // Stride per variable in __shared__
+//    const int ii = 1; // Stride distance x in __global__ and __shared__
+//    const int jjs = blockDim.x + 2 * igc; // Stride distance y in __shared__
+//
+//    double* us = (double*)shared; 
+//    double* vs = (double*)&shared[1*of];
+//    double* ws = (double*)&shared[2*of];
+//
+//    // More cells than threads, so some threads have to read twice (or more..)
+//    int ijsp, i2, j2, ijk2;
+//    for (int n=0; n<nloads; ++n)
+//    {
+//      ijsp     = threadIdx.x + threadIdx.y * blockDim.y + n * blockDim.x * blockDim.y;
+//      if(ijsp < bdimxp * bdimyp)
+//      { 
+//        i2       = ijsp % bdimxp + blockIdx.x * blockDim.x; 
+//        j2       = floor((double)ijsp / bdimxp) + blockIdx.y * blockDim.y;
+//        ijk2     = i2 + j2*jj + k*kk;
+//        us[ijsp] = u[ijk2];
+//        vs[ijsp] = v[ijk2];
+//        ws[ijsp] = w[ijk2];
+//      }
+//    }
+//
+//    __syncthreads(); 
+//
+//    ut[ijk] += 
+//          - (  interp2(us[ijs       ], us[ijs+ii ]) * interp2(us[ijs    ], us[ijs+ii ])
+//             - interp2(us[ijs-ii    ], us[ijs    ]) * interp2(us[ijs-ii ], us[ijs    ]) ) * dxi
+//
+//          - (  interp2(vs[ijs-ii+jjs], vs[ijs+jjs]) * interp2(us[ijs    ], us[ijs+jjs])
+//             - interp2(vs[ijs-ii    ], vs[ijs    ]) * interp2(us[ijs-jjs], us[ijs    ]) ) * dyi 
+//
+//          - (  interp2(w[ijk-ii+kk  ], w[ijk+kk  ]) * interp2(us[ijs    ], u[ijk+kk  ])
+//             - interp2(ws[ijs-ii    ], ws[ijs    ]) * interp2(u[ijk-kk  ], us[ijs    ]) ) * dzi[k];
+//
+//    vt[ijk] += 
+//          - (  interp2(us[ijs+ii-jjs], us[ijs+ii ]) * interp2(vs[ijs    ], vs[ijs+ii ])
+//             - interp2(us[ijs-jjs   ], us[ijs    ]) * interp2(vs[ijs-ii ], vs[ijs    ]) ) * dxi
+//
+//          - (  interp2(vs[ijs       ], vs[ijs+jjs]) * interp2(vs[ijs    ], vs[ijs+jjs])
+//             - interp2(vs[ijs-jjs   ], vs[ijs    ]) * interp2(vs[ijs-jjs], vs[ijs    ]) ) * dyi
+//
+//          - (  interp2(w[ijk-jj+kk  ], w[ijk+kk  ]) * interp2(vs[ijs    ], v[ijk+kk  ])
+//             - interp2(ws[ijs-jjs   ], ws[ijs    ]) * interp2(v[ijk-kk  ], vs[ijs    ]) ) * dzi[k];
+//
+//    if(k>kstart)
+//    {
+//      wt[ijk] += 
+//            - (  interp2(u[ijk+ii-kk], us[ijs+ii ]) * interp2(ws[ijs    ], ws[ijs+ii ])
+//               - interp2(u[ijk   -kk], us[ijs    ]) * interp2(ws[ijs-ii ], ws[ijs    ]) ) * dxi
+//
+//            - (  interp2(v[ijk+jj-kk], vs[ijs+jjs]) * interp2(ws[ijs    ], ws[ijs+jjs])
+//               - interp2(v[ijk   -kk], vs[ijs    ]) * interp2(ws[ijs-jjs], ws[ijs    ]) ) * dyi
+//
+//            - (  interp2(ws[ijs     ], w[ijk+kk  ]) * interp2(ws[ijs    ], w[ijk+kk  ])
+//               - interp2(w[ijk-kk   ], ws[ijs    ]) * interp2(w[ijk-kk  ], ws[ijs    ]) ) * dzhi[k];
+//    }
+//  }
+//}
 
 __global__ void advec_2_advecs(double * __restrict__ st, double * __restrict__ s, 
                                double * __restrict__ u, double * __restrict__ v, double * __restrict__ w,
@@ -306,8 +278,7 @@ int cadvec_2::exec()
   const double dxi = 1./grid->dx;
   const double dyi = 1./grid->dy;
   
-  const int nsmem = (blocki + 2 * grid->igc) * (blockj + 2 * grid->jgc);  
-
+  
   //advec_2_advecu<<<gridGPU, blockGPU>>>(fields->ut->data_g, fields->u->data_g, fields->v->data_g, 
   //                                      fields->w->data_g, grid->dzi_g, dxi, dyi,
   //                                      grid->icells, grid->ijcells,
@@ -332,7 +303,9 @@ int cadvec_2::exec()
                                           grid->icells, grid->ijcells,
                                           grid->istart, grid->jstart, grid->kstart,
                                           grid->iend,   grid->jend, grid->kend);
-
+  
+  //const int nsmem = (blocki + 2 * grid->igc) * (blockj + 2 * grid->jgc);
+  //const int nloads = nsmem / (blocki * blockj) + ((nsmem % (blocki * blockj)) > 0);
   //advec_2_advecuvw_smem<<<gridGPU, blockGPU, 3*nsmem*sizeof(double)>>>
   //                                        (fields->ut->data_g, fields->vt->data_g, fields->wt->data_g, 
   //                                         fields->u->data_g,  fields->v->data_g,  fields->w->data_g, 
@@ -340,7 +313,7 @@ int cadvec_2::exec()
   //                                         grid->icells, grid->ijcells,
   //                                         grid->istart, grid->jstart, grid->kstart,
   //                                         grid->iend,   grid->jend, grid->kend,
-  //                                         grid->igc,    grid->jgc);
+  //                                         grid->igc,    grid->jgc, nloads);
 
   for(fieldmap::iterator it = fields->st.begin(); it!=fields->st.end(); it++)
     advec_2_advecs<<<gridGPU, blockGPU>>>((*it->second).data_g, (*fields->s[it->first]).data_g, 
