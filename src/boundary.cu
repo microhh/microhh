7df435a5a73a36fde384069d1ef454f318d56daa
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-09-21 12:22:31 +0200

    Added the 4th order boundary conditions

--- src/boundary.cu
+++ src/boundary.cu
@@ -31,6 +31,7 @@
 #include "defines.h"
 #include "model.h"
 #include "timeloop.h"
+#include "fd.h"
 
 #define NO_OFFSET 0.
 #define NO_VELOCITY 0.
@@ -40,9 +41,12 @@
 #define BC_FLUX 2
 #define BC_USTAR 3
 
+using namespace fd::o4;
+
 __global__ void boundary_setgcbot_2nd(double * __restrict__ a, double * __restrict__ dzh, int sw, 
                                       double * __restrict__ abot, double * __restrict__ agradbot,
-                                      const unsigned int icells, const unsigned int icellsp, const unsigned int jcells, const unsigned int kstart)
+                                      const int icells, const int icellsp,
+                                      const int jcells, const int kstart)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   int j = blockIdx.y*blockDim.y + threadIdx.y;
@@ -61,9 +65,10 @@ __global__ void boundary_setgcbot_2nd(double * __restrict__ a, double * __restri
   }
 } 
 
-__global__ void boundary_setgctop_2nd(double * __restrict__ a, double * __restrict__ dzh, int sw, 
+__global__ void boundary_setgctop_2nd(double * __restrict__ a, double * __restrict__ dzh, int sw,
                                       double * __restrict__ atop, double * __restrict__ agradtop,
-                                      const unsigned int icells, const unsigned int icellsp, const unsigned int jcells, const unsigned int kend)
+                                      const int icells, const int icellsp,
+                                      const int jcells, const int kend)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   int j = blockIdx.y*blockDim.y + threadIdx.y;
@@ -80,14 +85,116 @@ __global__ void boundary_setgctop_2nd(double * __restrict__ a, double * __restri
     else if(sw == BC_NEUMANN || sw == BC_FLUX)
       a[ijk+kk] = agradtop[ij]*dzh[kend] + a[ijk];
   }
+}
+
+__device__ double fd_grad4x(const double a, const double b, const double c, const double d)
+{
+  return (-(d-a) + 27.*(c-b));
+}
+
+__global__ void boundary_setgcbot_4th(double * __restrict__ a, const int sw,
+                                      double * __restrict__ abot, double * __restrict__ agradbot,
+                                      double * __restrict z,
+                                      const int icells, const int icellsp,
+                                      const int jcells, const int kstart)
+{
+  const int i = blockIdx.x*blockDim.x + threadIdx.x;
+  const int j = blockIdx.y*blockDim.y + threadIdx.y;
+
+  const int kk1 = 1*icellsp*jcells;
+  const int kk2 = 2*icellsp*jcells;
+
+  const int ij  = i + j*icellsp;
+  const int ijk = i + j*icellsp + kstart*kk1;
+
+  if(i < icells && j < jcells)
+  {
+    if(sw == BC_DIRICHLET)
+    {
+      a[ijk-kk1] = (8./3.)*abot[ij] - 2.*a[ijk] + (1./3.)*a[ijk+kk1];
+      a[ijk-kk2] = 8.*abot[ij] - 9.*a[ijk] + 2.*a[ijk+kk1];
+    }
+
+    else if(sw == BC_NEUMANN || sw == BC_FLUX)
+    {
+      a[ijk-kk1] = -(1./24.)*fd_grad4x(z[kstart-2], z[kstart-1], z[kstart], z[kstart+1])*agradbot[ij] + a[ijk    ];
+      a[ijk-kk2] = -(1./ 8.)*fd_grad4x(z[kstart-2], z[kstart-1], z[kstart], z[kstart+1])*agradbot[ij] + a[ijk+kk1];
+    }
+  }
 } 
 
+__global__ void boundary_setgctop_4th(double * __restrict__ a, const int sw,
+                                      double * __restrict__ atop, double * __restrict__ agradtop,
+                                      double * __restrict__ z,
+                                      const int icells, const int icellsp,
+                                      const int jcells, const int kend)
+{
+  const int i = blockIdx.x*blockDim.x + threadIdx.x;
+  const int j = blockIdx.y*blockDim.y + threadIdx.y;
+
+  const int kk1 = 1*icellsp*jcells;
+  const int kk2 = 2*icellsp*jcells;
+
+  const int ij  = i + j*icellsp;
+  const int ijk = i + j*icellsp + (kend-1)*kk1;
+
+  if(i < icells && j < jcells)
+  {
+    if(sw == BC_DIRICHLET)
+    {
+      a[ijk+kk1] = (8./3.)*atop[ij] - 2.*a[ijk] + (1./3.)*a[ijk-kk1];
+      a[ijk+kk2] = 8.*atop[ij] - 9.*a[ijk] + 2.*a[ijk-kk1];
+    }
+
+    else if(sw == BC_NEUMANN || sw == BC_FLUX)
+    {
+      a[ijk+kk1] = (1./24.)*fd_grad4x(z[kend-2], z[kend-1], z[kend], z[kend+1])*agradtop[ij] + a[ijk    ];
+      a[ijk+kk2] = (1./ 8.)*fd_grad4x(z[kend-2], z[kend-1], z[kend], z[kend+1])*agradtop[ij] + a[ijk-kk1];
+    }
+  }
+} 
+
+__global__ void boundary_setgcbotw_4th(double * __restrict__ w,
+                                       const int icells, const int icellsp,
+                                       const int jcells, const int kstart)
+{
+  const int i = blockIdx.x*blockDim.x + threadIdx.x;
+  const int j = blockIdx.y*blockDim.y + threadIdx.y;
+
+  const int kk1 = 1*icellsp*jcells;
+  const int kk2 = 2*icellsp*jcells;
+
+  const int ijk = i + j*icellsp + kstart*kk1;
+
+  if(i < icells && j < jcells)
+  {
+    w[ijk-kk1] = -w[ijk+kk1];
+    w[ijk-kk2] = -w[ijk+kk2];
+  }
+}
+
+__global__ void boundary_setgctopw_4th(double * __restrict__ w,
+                                       const int icells, const int icellsp,
+                                       const int jcells, const int kend)
+{
+  const int i = blockIdx.x*blockDim.x + threadIdx.x;
+  const int j = blockIdx.y*blockDim.y + threadIdx.y;
+
+  const int kk1 = 1*icellsp*jcells;
+  const int kk2 = 2*icellsp*jcells;
+
+  const int ijk = i + j*icellsp + (kend-1)*kk1;
+
+  if(i < icells && j < jcells)
+  {
+    w[ijk+kk1] = -w[ijk-kk1];
+    w[ijk+kk2] = -w[ijk-kk2];
+  }
+}
 
 #ifdef USECUDA
 int cboundary::exec()
 {
-  //fields->forwardGPU();
-
   const int blocki = 128;
   const int blockj = 2;
   const int gridi  = grid->icells/blocki + (grid->icells%blocki > 0);
@@ -117,48 +224,82 @@ int cboundary::exec()
   {
     boundary_setgcbot_2nd<<<grid2dGPU, block2dGPU>>>(&fields->u->data_g[offs], grid->dzh_g, mbcbot, 
                                                      &fields->u->databot_g[offs], &fields->u->datagradbot_g[offs],
-                                                     grid->icells, grid->icellsp, grid->jcells, grid->kstart);
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kstart);
     boundary_setgctop_2nd<<<grid2dGPU, block2dGPU>>>(&fields->u->data_g[offs], grid->dzh_g, mbctop, 
                                                      &fields->u->datatop_g[offs], &fields->u->datagradtop_g[offs],
-                                                     grid->icells, grid->icellsp, grid->jcells, grid->kend);
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kend);
 
     boundary_setgcbot_2nd<<<grid2dGPU, block2dGPU>>>(&fields->v->data_g[offs], grid->dzh_g, mbcbot, 
                                                      &fields->v->databot_g[offs], &fields->v->datagradbot_g[offs],
-                                                     grid->icells, grid->icellsp, grid->jcells, grid->kstart);
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kstart);
     boundary_setgctop_2nd<<<grid2dGPU, block2dGPU>>>(&fields->v->data_g[offs], grid->dzh_g, mbctop, 
                                                      &fields->v->datatop_g[offs], &fields->v->datagradtop_g[offs],
-                                                     grid->icells, grid->icellsp, grid->jcells, grid->kend);
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kend);
 
     for(fieldmap::const_iterator it=fields->sp.begin(); it!=fields->sp.end(); ++it)
     {
       boundary_setgcbot_2nd<<<grid2dGPU, block2dGPU>>>(&it->second->data_g[offs], grid->dzh_g, sbc[it->first]->bcbot, 
                                                        &it->second->databot_g[offs], &it->second->datagradbot_g[offs],
-                                                       grid->icells, grid->icellsp, grid->jcells, grid->kstart);
+                                                       grid->icells, grid->icellsp,
+                                                       grid->jcells, grid->kstart);
       boundary_setgctop_2nd<<<grid2dGPU, block2dGPU>>>(&it->second->data_g[offs], grid->dzh_g, sbc[it->first]->bctop, 
                                                        &it->second->datatop_g[offs], &it->second->datagradtop_g[offs],
-                                                       grid->icells, grid->icellsp, grid->jcells, grid->kend);
+                                                       grid->icells, grid->icellsp,
+                                                       grid->jcells, grid->kend);
     }
   }
-  //else if(grid->swspatialorder == "4")
-  //{
-  //  setgcbot_4th(fields->u->data, grid->z, mbcbot, fields->u->databot, fields->u->datagradbot);
-  //  setgctop_4th(fields->u->data, grid->z, mbctop, fields->u->datatop, fields->u->datagradtop);
+  else if(grid->swspatialorder == "4")
+  {
+    fields->forwardGPU();
 
-  //  setgcbot_4th(fields->v->data, grid->z, mbcbot, fields->v->databot, fields->v->datagradbot);
-  //  setgctop_4th(fields->v->data, grid->z, mbctop, fields->v->datatop, fields->v->datagradtop);
+    boundary_setgcbot_4th<<<grid2dGPU, block2dGPU>>>(&fields->u->data_g[offs], mbcbot, 
+                                                     &fields->u->databot_g[offs], &fields->u->datagradbot_g[offs],
+                                                     grid->z_g,
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kstart);
+    boundary_setgctop_4th<<<grid2dGPU, block2dGPU>>>(&fields->u->data_g[offs], mbctop, 
+                                                     &fields->u->datatop_g[offs], &fields->u->datagradtop_g[offs],
+                                                     grid->z_g,
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kend);
 
-  //  setgcbotw_4th(fields->w->data);
-  //  setgctopw_4th(fields->w->data);
+    boundary_setgcbot_4th<<<grid2dGPU, block2dGPU>>>(&fields->v->data_g[offs], mbcbot, 
+                                                     &fields->v->databot_g[offs], &fields->v->datagradbot_g[offs],
+                                                     grid->z_g,
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kstart);
+    boundary_setgctop_4th<<<grid2dGPU, block2dGPU>>>(&fields->v->data_g[offs], mbctop, 
+                                                     &fields->v->datatop_g[offs], &fields->v->datagradtop_g[offs],
+                                                     grid->z_g,
+                                                     grid->icells, grid->icellsp,
+                                                     grid->jcells, grid->kend);
 
-  //  for(fieldmap::const_iterator it=fields->sp.begin(); it!=fields->sp.end(); ++it)
-  //  {
-  //    setgcbot_4th(it->second->data, grid->z, sbc[it->first]->bcbot, it->second->databot, it->second->datagradbot);
-  //    setgctop_4th(it->second->data, grid->z, sbc[it->first]->bctop, it->second->datatop, it->second->datagradtop);
-  //  }
-  //}
+    boundary_setgcbotw_4th<<<grid2dGPU, block2dGPU>>>(&fields->w->data_g[offs],
+                                                      grid->icells, grid->icellsp,
+                                                      grid->jcells, grid->kstart);
+    boundary_setgctopw_4th<<<grid2dGPU, block2dGPU>>>(&fields->w->data_g[offs],
+                                                      grid->icells, grid->icellsp,
+                                                      grid->jcells, grid->kend);
 
-
-  //fields->backwardGPU();
+    for(fieldmap::const_iterator it=fields->sp.begin(); it!=fields->sp.end(); ++it)
+    {
+      boundary_setgcbot_4th<<<grid2dGPU, block2dGPU>>>(&it->second->data_g[offs], sbc[it->first]->bcbot,
+                                                       &it->second->databot_g[offs], &it->second->datagradbot_g[offs],
+                                                       grid->z_g,
+                                                       grid->icells, grid->icellsp,
+                                                       grid->jcells, grid->kstart);
+      boundary_setgctop_4th<<<grid2dGPU, block2dGPU>>>(&it->second->data_g[offs], sbc[it->first]->bctop, 
+                                                       &it->second->datatop_g[offs], &it->second->datagradtop_g[offs],
+                                                       grid->z_g,
+                                                       grid->icells, grid->icellsp,
+                                                       grid->jcells, grid->kend);
+    }
+    fields->backwardGPU();
+  }
 
   return 0;
 }
