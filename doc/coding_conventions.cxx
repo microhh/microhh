d02c0079b577e92a83d35950683cd5b81538ebd9
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-10-06 10:12:34 +0200

    Some alterations to the coding conventions

--- doc/coding_conventions.cxx
+++ doc/coding_conventions.cxx
@@ -1,25 +1,34 @@
-// 1. All classes, structs, typedefs and enums go PascalCase:
+// 1.1 All classes, structs, typedefs and enums go PascalCase:
 class ThermoMoist;
 struct StatsVar;
 typedef std::map<std::string, Field3d *> FieldMap;
 enum BoundaryType {DirichletType, NeumannType, FluxType};
 
 
-// 2. All functions go camelCase, function names should be informative:
+// 2.1 All functions go camelCase, function names should be informative:
 void execStats();
 void initPrognosticVariable;
 
-// 2.1 All accessors and mutators contain the name of the variable without changing the case:
-void get_calcProfs();
-void set_calcProfs(true);
+// 2.2 All accessors and mutators contain the name of the variable without changing the case:
+bool get_calcMeanProfs();
+void set_calcMeanProfs(true);
 void get_gridData();
 
-// 2.2 Functions that distinguish multiple versions for different spatial orders use underscore suffix:
+// 2.3 Functions that distinguish multiple versions for different spatial orders use underscore suffix:
 void interpolate_4th();
 void calcFlux_2nd();
 
 
-// 3. Variable names go the the wish of the user:
+// 3.1 Variable names go at the wish of the user:
 bool calcProfs = true;
 int icells;
 
+
+// 4.1 GPU versions of existing functions and variables are written with a suffix _g:
+void boundaryCyclic_g();
+
+// 4.2 GPU kernels are stored in namespaces with the name of the class and a suffix _g:
+namespace ThermoMoist_g
+{
+  calcBuoyancy();
+}
