b2d5a1fd89114f889e0e97d8289492f88e9ab5ae
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-03-01 14:32:36 +0100

    Brackets in sign function

--- src/boundary_surface.cxx
+++ src/boundary_surface.cxx
@@ -41,7 +41,7 @@ namespace
 {
   // Size of the lookup table.
   const int nzL = 10000; // Size of the lookup table for MO iterations.
-  inline double sign(double n) { return n > 0 ? 1 : (n < 0 ? -1 : 0); }
+  inline double sign(const double n) { return (n > 0) ? 1 : ( (n < 0) ? -1 : 0); }
 }
 
 BoundarySurface::BoundarySurface(Model *modelin, Input *inputin) : Boundary(modelin, inputin)
