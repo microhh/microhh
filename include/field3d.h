728cf3c725dbad5dc756b55852c7dcc03c0ccf37
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-03-04 10:16:51 +0100

    Cleanup field3d

--- include/field3d.h
+++ include/field3d.h
@@ -30,44 +30,44 @@ class Grid;
 
 class Field3d
 {
-  public:
-    // functions
-    Field3d(Grid *, Master *, std::string, std::string, std::string);
-    ~Field3d();
+    public:
+        // functions
+        Field3d(Grid*, Master*, std::string, std::string, std::string);
+        ~Field3d();
 
-    int init();
-    // int checkfornan();
+        int init();
+        // int checkfornan();
 
-    // variables at CPU
-    double *data;
-    double *databot;
-    double *datatop;
-    double *datamean;
-    double *datagradbot;
-    double *datagradtop;
-    double *datafluxbot;
-    double *datafluxtop;
-    std::string name;
-    std::string unit;
-    std::string longname;
-    double visc;
+        // variables at CPU
+        double* data;
+        double* databot;
+        double* datatop;
+        double* datamean;
+        double* datagradbot;
+        double* datagradtop;
+        double* datafluxbot;
+        double* datafluxtop;
+        std::string name;
+        std::string unit;
+        std::string longname;
+        double visc;
 
-    // Device functions and variables
-    void initDevice();  ///< Allocate Field3D fields at device 
-    void clear_device(); ///< Deallocate Field3D fields at device 
+        // Device functions and variables
+        void init_device();  ///< Allocate Field3D fields at device 
+        void clear_device(); ///< Deallocate Field3D fields at device 
 
-    double *data_g;
-    double *databot_g;
-    double *datatop_g;
-    double *datamean_g;
-    double *datagradbot_g;
-    double *datagradtop_g;
-    double *datafluxbot_g;
-    double *datafluxtop_g;
+        double* data_g;
+        double* databot_g;
+        double* datatop_g;
+        double* datamean_g;
+        double* datagradbot_g;
+        double* datagradtop_g;
+        double* datafluxbot_g;
+        double* datafluxtop_g;
 
-  private:
-    Grid   *grid;
-    Master *master;
+    private:
+        Grid* grid;
+        Master* master;
 };
 #endif
 
