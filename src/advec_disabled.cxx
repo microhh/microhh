cd0da663400e9bd6b55dfef571e96f39f7b1e939
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-03-06 16:49:09 +0100

    Added empty advection class

---
+++ src/advec_disabled.cxx
@@ -0,0 +1,55 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2015 Chiel van Heerwaarden
+ * Copyright (c) 2011-2015 Thijs Heus
+ * Copyright (c) 2014-2015 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include "grid.h"
+#include "fields.h"
+#include "defines.h"
+#include "constants.h"
+#include "master.h"
+#include "model.h"
+#include "advec_disabled.h"
+
+Advec_disabled::Advec_disabled(Model* modelin, Input* inputin) : Advec(modelin, inputin)
+{
+}
+
+Advec_disabled::~Advec_disabled()
+{
+}
+
+unsigned long Advec_disabled::get_time_limit(unsigned long idt, const double dt)
+{
+    unsigned long idtlim = (unsigned long) Constants::dbig;
+    return idtlim;
+}
+
+double Advec_disabled::get_cfl(const double dt)
+{
+    return cflmin;
+}
+
+void Advec_disabled::exec()
+{
+}
