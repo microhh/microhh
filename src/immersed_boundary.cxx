00eb163dafb884c2917ba8b3aa7c1c34b400ae13
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-05-14 19:41:34 +0200

    First working test case

--- src/immersed_boundary.cxx
+++ src/immersed_boundary.cxx
@@ -20,9 +20,11 @@
  * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <cstdio>
 #include "master.h"
 #include "grid.h"
 #include "fields.h"
+#include "defines.h"
 #include "immersed_boundary.h"
 
 Immersed_boundary::Immersed_boundary(Master& masterin, Grid& gridin) : 
@@ -35,6 +37,48 @@ Immersed_boundary::~Immersed_boundary()
 {
 }
 
+namespace
+{
+    void ib(double* const restrict u, double* const restrict w,
+            const int istart, const int iend,
+            const int jstart, const int jend,
+            const int kstart, const int kend,
+            const int icells, const int ijcells)
+    {
+        const int ii = 1;
+        const int jj = icells;
+        const int kk = ijcells;
+
+        const int ibc_istart = istart + (iend-istart) / 4;
+        const int ibc_iend   = istart + 3 * (iend-istart) / 4;
+        const int ibc_kstart = kstart;
+        const int ibc_kend   = kstart + (kstart-kend) / 4;
+
+        // Set the u ghost cells
+        for (int k=ibc_kstart; k<ibc_kend; ++k)
+            for (int j=jstart; j<jend; ++j)
+            {
+                const int ijk_left  = ibc_istart + j*jj + k*kk;
+                const int ijk_right = ibc_iend   + j*jj + k*kk;
+                u[ijk_left ] = 0.;
+                u[ijk_right] = 0.;
+            }
+
+        // Set the w ghost cells
+        for (int j=jstart; j<jend; ++j)
+            for (int i=ibc_istart; i<ibc_iend; ++i)
+            {
+                const int ijk_top = i + j*jj + ibc_kend*kk;
+                w[ijk_top] = 0.;
+            }
+    }
+}
+
 void Immersed_boundary::set_ghost_cells(Fields& fields)
 {
+    ib(fields.u->data, fields.w->data,
+       grid.istart, grid.iend,
+       grid.jstart, grid.jend,
+       grid.kstart, grid.kend,
+       grid.icells, grid.ijcells);
 }
