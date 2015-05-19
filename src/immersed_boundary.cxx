c881c940d594068b1cea19d0714f3d83e8dcf526
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-05-20 00:22:50 +0200

    Build in no slip bcs on faces

--- src/immersed_boundary.cxx
+++ src/immersed_boundary.cxx
@@ -25,6 +25,7 @@
 #include "grid.h"
 #include "fields.h"
 #include "defines.h"
+#include "finite_difference.h"
 #include "immersed_boundary.h"
 
 Immersed_boundary::Immersed_boundary(Master& masterin, Grid& gridin) : 
@@ -39,11 +40,12 @@ Immersed_boundary::~Immersed_boundary()
 
 namespace
 {
-    void ib(double* const restrict u, double* const restrict w,
-            const int istart, const int iend,
-            const int jstart, const int jend,
-            const int kstart, const int kend,
-            const int icells, const int ijcells)
+    void set_no_penetration(double* const restrict ut, double* const restrict wt,
+                            double* const restrict u, double* const restrict w,
+                            const int istart, const int iend,
+                            const int jstart, const int jend,
+                            const int kstart, const int kend,
+                            const int icells, const int ijcells)
     {
         const int ii = 1;
         const int jj = icells;
@@ -62,8 +64,10 @@ namespace
             {
                 const int ijk_istart  = ibc_istart + j*jj + k*kk;
                 const int ijk_iend    = ibc_iend+1 + j*jj + k*kk;
-                u[ijk_istart] = 0.;
-                u[ijk_iend  ] = 0.;
+                u [ijk_istart] = 0.;
+                u [ijk_iend  ] = 0.;
+                ut[ijk_istart] = 0.;
+                ut[ijk_iend  ] = 0.;
             }
 
         // Set the w ghost cells, no flow in the block.
@@ -72,17 +76,92 @@ namespace
             {
                 const int ijk_kstart = i + j*jj + ibc_kstart  *kk;
                 const int ijk_kend   = i + j*jj + (ibc_kend+1)*kk;
-                w[ijk_kstart] = 0.;
-                w[ijk_kend  ] = 0.;
+                w [ijk_kstart] = 0.;
+                w [ijk_kend  ] = 0.;
+                wt[ijk_kstart] = 0.;
+                wt[ijk_kend  ] = 0.;
             }
     }
+
+    void set_no_slip(double* const restrict ut, double* const restrict wt,
+                     const double* const restrict u, const double* const restrict w,
+                     const double* const restrict rhoref, const double* const restrict rhorefh,
+                     const double* const restrict dzi, const double* const restrict dzhi,
+                     const double dxi,
+                     const double visc,
+                     const int istart, const int iend,
+                     const int jstart, const int jend,
+                     const int kstart, const int kend,
+                     const int icells, const int ijcells)
+    {
+        using namespace Finite_difference::O2;
+
+        const int ii = 1;
+        const int jj = icells;
+        const int kk = ijcells;
+
+        const double dxidxi = dxi*dxi;
+
+        // Put an solid block at 1/2 of the horizontal dimension in the
+        // middle of the channel.
+        const int ibc_istart = istart + 7 * (iend-istart) / 16;
+        const int ibc_iend   = istart + 9 * (iend-istart) / 16;
+        const int ibc_kstart = kstart + 3 * (kend-kstart) / 8;
+        const int ibc_kend   = kstart + 5 * (kend-kstart) / 8;
+
+        // Set the w no slip at the vertical walls, by reverting the advection 
+        // and diffusion towards the wall.
+        for (int k=ibc_kstart; k<ibc_kend+1; ++k)
+            for (int j=jstart; j<jend; ++j)
+            {
+                int ijk = ibc_istart-1 + j*jj + k*kk;
+                wt[ijk] +=
+                        + ( interp2(u[ijk+ii-kk], u[ijk+ii]) * interp2(w[ijk], w[ijk+ii]) ) * dxi
+                        - visc * ( (w[ijk+ii] - w[ijk]) ) * dxidxi;
+
+                ijk = ibc_iend + j*jj + k*kk;
+                wt[ijk] +=
+                        - ( interp2(u[ijk-kk], u[ijk]) * interp2(w[ijk-ii], w[ijk]) ) * dxi
+                        + visc * ( (w[ijk] - w[ijk-ii]) ) * dxidxi;
+            }
+
+        // Set the u no slip at the horizontal walls
+        for (int j=jstart; j<jend; ++j)
+            for (int i=ibc_istart; i<ibc_iend+1; ++i)
+            {
+                int k = ibc_kstart;
+                int ijk = i + j*jj + k*kk;
+                ut[ijk] +=
+                        + ( rhorefh[k+1] * interp2(w[ijk-ii+kk], w[ijk+kk]) * interp2(u[ijk   ], u[ijk+kk]) ) / rhoref[k] * dzi[k];
+                        - visc * ( (u[ijk+kk] - u[ijk   ]) * dzhi[k+1]) * dzi[k];
+
+                k = ibc_kend+1;
+                ijk = i + j*jj + k*kk;
+                ut[ijk] +=
+                        - ( rhorefh[k] * interp2(w[ijk-ii], w[ijk]) * interp2(u[ijk-kk], u[ijk]) ) / rhoref[k] * dzi[k];
+                        + visc * ( (u[ijk] - u[ijk-kk]) * dzhi[k] ) * dzi[k];
+            }
+    }
+
 }
 
-void Immersed_boundary::set_ghost_cells(Fields& fields)
+void Immersed_boundary::exec(Fields& fields)
 {
-    ib(fields.u->data, fields.w->data,
-       grid.istart, grid.iend,
-       grid.jstart, grid.jend,
-       grid.kstart, grid.kend,
-       grid.icells, grid.ijcells);
+    set_no_penetration(fields.ut->data, fields.wt->data,
+                       fields.u->data, fields.w->data,
+                       grid.istart, grid.iend,
+                       grid.jstart, grid.jend,
+                       grid.kstart, grid.kend,
+                       grid.icells, grid.ijcells);
+
+    set_no_slip(fields.ut->data, fields.wt->data,
+                fields.u->data, fields.w->data,
+                fields.rhoref, fields.rhorefh,
+                grid.dzi, grid.dzhi,
+                grid.dxi,
+                fields.visc,
+                grid.istart, grid.iend,
+                grid.jstart, grid.jend,
+                grid.kstart, grid.kend,
+                grid.icells, grid.ijcells);
 }
