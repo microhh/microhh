efa35452413bd481a7e4c6bea7c1062564aad70c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-05-19 21:18:59 +0200

    Reduce case to single block

--- src/immersed_boundary.cxx
+++ src/immersed_boundary.cxx
@@ -49,80 +49,12 @@ namespace
         const int jj = icells;
         const int kk = ijcells;
 
-        // Put an solid block at 1/4 of the horizontal dimension in the
+        // Put an solid block at 1/2 of the horizontal dimension in the
         // middle of the channel.
-        int ibc_istart = istart + 15 * (iend-istart) / 64;
-        int ibc_iend   = istart + 17 * (iend-istart) / 64;
-        int ibc_kstart = kstart + 7 * (kend-kstart) / 16;
-        int ibc_kend   = kstart + 9 * (kend-kstart) / 16;
-
-        // Set the u ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend+1; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    u[ijk] = 0.;
-                }
-
-        // Set the w ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend+1; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    w[ijk] = 0.;
-                }
-
-        // Put an solid block at 3/4 of the horizontal dimension in the
-        // middle of the channel.
-        ibc_istart = istart + 47 * (iend-istart) / 64;
-        ibc_iend   = istart + 49 * (iend-istart) / 64;
-
-        // Set the u ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend+1; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    u[ijk] = 0.;
-                }
-
-        // Set the w ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend+1; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    w[ijk] = 0.;
-                }
-
-        // Put two solid blocks at 1/2 of the horizontal dimension
-        ibc_istart = istart + 31 * (iend-istart) / 64;
-        ibc_iend   = istart + 33 * (iend-istart) / 64;
-        ibc_kstart = kstart + 3 * (kend-kstart) / 16;
-        ibc_kend   = kstart + 5 * (kend-kstart) / 16;
-
-        // Set the u ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend+1; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    u[ijk] = 0.;
-                }
-
-        // Set the w ghost cells, no flow in the block.
-        for (int k=ibc_kstart; k<ibc_kend+1; ++k)
-            for (int j=jstart; j<jend; ++j)
-                for (int i=ibc_istart; i<ibc_iend; ++i)
-                {
-                    const int ijk  = i + j*jj + k*kk;
-                    w[ijk] = 0.;
-                }
-
-        ibc_kstart = kstart + 11 * (kend-kstart) / 16;
-        ibc_kend   = kstart + 13 * (kend-kstart) / 16;
+        const int ibc_istart = istart + 7 * (iend-istart) / 16;
+        const int ibc_iend   = istart + 9 * (iend-istart) / 16;
+        const int ibc_kstart = kstart + 3 * (kend-kstart) / 8;
+        const int ibc_kend   = kstart + 5 * (kend-kstart) / 8;
 
         // Set the u ghost cells, no flow in the block.
         for (int k=ibc_kstart; k<ibc_kend; ++k)
