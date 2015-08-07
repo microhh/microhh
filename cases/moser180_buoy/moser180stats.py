bba4086d8f0d066e9ef6f27c0f6e260ab1af5c0b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-08-07 09:11:31 +0200

    Updated configuration settings and improved moser stats script for heated channel flow

--- cases/moser180_buoy/moser180stats.py
+++ cases/moser180_buoy/moser180stats.py
@@ -4,54 +4,9 @@ import netCDF4
 
 from pylab import *
 
-start = 25
-end   = 35
-plotens = False
-
-# read Moser's data
-Mosermean = numpy.loadtxt("../moser180/chan180.means", skiprows=25)
-Moserrey  = numpy.loadtxt("../moser180/chan180.reystress", skiprows=25)
-Moseru2   = numpy.loadtxt("../moser180/chan180.uubal", skiprows=25)
-Moserv2   = numpy.loadtxt("../moser180/chan180.wwbal", skiprows=25)
-Moserw2   = numpy.loadtxt("../moser180/chan180.vvbal", skiprows=25)
-Mosertke  = numpy.loadtxt("../moser180/chan180.kbal" , skiprows=25)
-Moseruw   = numpy.loadtxt("../moser180/chan180.uvbal", skiprows=25)
-
-yplusMoser = Mosermean[:,1]
-uavgMoser  = Mosermean[:,2]
-uvarMoser  = Moserrey[:,2]
-vvarMoser  = Moserrey[:,3]
-wvarMoser  = Moserrey[:,4]
-
-u2_shearMoser = Moseru2[:,3]
-u2_turbMoser  = Moseru2[:,6]
-u2_viscMoser  = Moseru2[:,7]
-u2_dissMoser  = Moseru2[:,2]
-u2_rdstrMoser = Moseru2[:,4]
-
-v2_turbMoser  = Moserv2[:,6]
-v2_viscMoser  = Moserv2[:,7]
-v2_dissMoser  = Moserv2[:,2]
-v2_rdstrMoser = Moserv2[:,4]
-
-w2_turbMoser  = Moserw2[:,6]
-w2_viscMoser  = Moserw2[:,7]
-w2_dissMoser  = Moserw2[:,2]
-w2_presMoser  = Moserw2[:,5]
-w2_rdstrMoser = Moserw2[:,4]
-
-tke_shearMoser = Mosertke[:,3]
-tke_turbMoser  = Mosertke[:,6]
-tke_viscMoser  = Mosertke[:,7]
-tke_dissMoser  = Mosertke[:,2]
-tke_presMoser  = Mosertke[:,5]
-
-uw_shearMoser = Moseruw[:,3]
-uw_presMoser  = Moseruw[:,5]
-uw_turbMoser  = Moseruw[:,6]
-uw_viscMoser  = Moseruw[:,7]
-uw_dissMoser  = Moseruw[:,2]
-uw_rdstrMoser = Moseruw[:,4]
+start = 0
+end   = 10
+plotens = True
 
 stats = netCDF4.Dataset("moser180.default.0000000.nc","r")
 t  = stats.variables["t"] [start:end]
@@ -62,6 +17,7 @@ vavgt = stats.variables["v"] [start:end,:]
 uvart = stats.variables["u2"][start:end,:]
 vvart = stats.variables["v2"][start:end,:]
 wvart = stats.variables["w2"][start:end,:]
+bvart = stats.variables["b2"][start:end,:]
 
 uwt    = stats.variables["uw"]   [start:end,:]
 udifft = stats.variables["udiff"][start:end,:]
@@ -83,6 +39,7 @@ v2_visct  = stats.variables["v2_visc"] [start:end,:]
 v2_disst  = stats.variables["v2_diss"] [start:end,:]
 v2_rdstrt = stats.variables["v2_rdstr"][start:end,:]
 
+w2_buoyt  = stats.variables["w2_buoy"] [start:end,:]
 w2_turbt  = stats.variables["w2_turb"] [start:end,:]
 w2_visct  = stats.variables["w2_visc"] [start:end,:]
 w2_disst  = stats.variables["w2_diss"] [start:end,:]
@@ -91,6 +48,7 @@ w2_rdstrt = stats.variables["w2_rdstr"][start:end,:]
 
 # tke budget
 tke_sheart = stats.variables["tke_shear"][start:end,:]
+tke_buoyt  = stats.variables["tke_buoy"] [start:end,:]
 tke_turbt  = stats.variables["tke_turb"] [start:end,:]
 tke_visct  = stats.variables["tke_visc"] [start:end,:]
 tke_disst  = stats.variables["tke_diss"] [start:end,:]
@@ -102,6 +60,7 @@ uw_visct  = stats.variables["uw_visc"] [start:end,:]
 uw_turbt  = stats.variables["uw_turb"] [start:end,:]
 uw_disst  = stats.variables["uw_diss"] [start:end,:]
 uw_prest  = stats.variables["uw_pres"] [start:end,:]
+uw_buoyt  = stats.variables["uw_buoy"] [start:end,:]
 uw_rdstrt = stats.variables["uw_rdstr"][start:end,:]
 
 # buoyancy varianc budget
@@ -120,6 +79,7 @@ vavg = numpy.mean(vavgt,0)
 uvar = numpy.mean(uvart,0)
 vvar = numpy.mean(vvart,0)
 wvar = numpy.mean(wvart,0)
+bvar = numpy.mean(bvart,0)
 
 uw    = numpy.mean(uwt,0)
 udiff = numpy.mean(udifft,0)
@@ -143,33 +103,35 @@ v2_rdstr = numpy.mean(v2_rdstrt,0)
 v2_resid = v2_turb + v2_visc + v2_diss + v2_rdstr
 
 w2_turb  = numpy.mean(w2_turbt ,0)
+w2_buoy  = numpy.mean(w2_buoyt ,0)
 w2_visc  = numpy.mean(w2_visct ,0)
 w2_diss  = numpy.mean(w2_disst ,0)
 w2_pres  = numpy.mean(w2_prest ,0)
 w2_rdstr = numpy.mean(w2_rdstrt,0)
-w2_resid = w2_turb + w2_visc + w2_diss + w2_pres + w2_rdstr
+w2_resid = w2_turb + w2_visc + w2_diss + w2_pres + w2_rdstr + w2_buoy
 
 tke_shear = numpy.mean(tke_sheart,0)
+tke_buoy  = numpy.mean(tke_buoyt ,0)
 tke_turb  = numpy.mean(tke_turbt ,0)
 tke_visc  = numpy.mean(tke_visct ,0)
 tke_diss  = numpy.mean(tke_disst ,0)
 tke_pres  = numpy.mean(tke_prest ,0)
-tke_resid = tke_shear + tke_turb + tke_visc + tke_diss + tke_pres
-
+tke_resid = tke_shear + tke_turb + tke_visc + tke_diss + tke_pres + tke_buoy
+ 
 uw_shear = numpy.mean(uw_sheart,0)
 uw_turb  = numpy.mean(uw_turbt ,0)
+uw_buoy  = numpy.mean(uw_buoyt ,0)
 uw_visc  = numpy.mean(uw_visct ,0)
 uw_diss  = numpy.mean(uw_disst ,0)
 uw_pres  = numpy.mean(uw_prest ,0)
 uw_rdstr = numpy.mean(uw_rdstrt,0)
-uw_resid = uw_shear + uw_turb + uw_visc + uw_diss + uw_pres + uw_rdstr
+uw_resid = uw_shear + uw_turb + uw_buoy + uw_visc + uw_diss + uw_pres + uw_rdstr
 
 b2_shear = numpy.mean(b2_sheart,0)
 b2_turb  = numpy.mean(b2_turbt ,0)
 b2_visc  = numpy.mean(b2_visct ,0)
 b2_diss  = numpy.mean(b2_disst ,0)
 b2_resid = b2_shear + b2_turb + b2_visc + b2_diss
-
 
 utotavg = numpy.mean(utotavgt,0)
 ustar   = numpy.mean(ustart)
@@ -194,14 +156,13 @@ if(plotens):
  for n in range(end-start):
    semilogx(yplus[starty:endy], utotavgt[n,starty:endy] / ustar, color='#cccccc')
 semilogx(yplus[starty:endy], utotavg[starty:endy] / ustar, 'b-', label='u')
-semilogx(yplusMoser, uavgMoser, 'k--', label="Moser")
 semilogx(ypluslin, ulin, 'k:')
 semilogx(ypluslog, ulog, 'k:')
 xlabel('y+')
 ylabel('u+')
 legend(loc=2, frameon=False)
 grid()
-axis([0.1, 200, 0, 20])
+xlim(0, 200)
 
 figure()
 if(plotens):
@@ -209,17 +170,16 @@ if(plotens):
     plot(yplus [starty:endy], (uvart[n,starty:endy] / ustar**2.)**0.5, color='#cccccc')
     plot(yplus [starty:endy], (vvart[n,starty:endy] / ustar**2.)**0.5, color='#cccccc')
     plot(yplush[starty:endy], (wvart[n,starty:endy] / ustar**2.)**0.5, color='#cccccc')
+    plot(yplus [starty:endy], (bvart[n,starty:endy] / (bflux[0]/ustar)**2.)**0.5, color='#cccccc')
 plot(yplus [starty:endy], (uvar[starty:endy] / ustar**2.)**0.5, 'b-', label='u')
 plot(yplus [starty:endy], (vvar[starty:endy] / ustar**2.)**0.5, 'g-', label='v')
 plot(yplush[starty:endy], (wvar[starty:endy] / ustar**2.)**0.5, 'r-', label='w')
-plot(yplusMoser, sqrt(uvarMoser), 'k--', label="Moser")
-plot(yplusMoser, sqrt(vvarMoser), 'k--')
-plot(yplusMoser, sqrt(wvarMoser), 'k--')
+plot(yplus [starty:endy], (bvar[starty:endy] / (bflux[0]/ustar)**2.)**.5, 'm-', label='b')
 xlabel('y+')
 ylabel('rms')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, 0, 3.5])
+xlim(0, 200)
 
 figure()
 if(plotens):
@@ -234,17 +194,12 @@ plot(yplus[starty:endy], u2_turb [starty:endy] * visc / ustar**4., 'g-', label='
 plot(yplus[starty:endy], u2_visc [starty:endy] * visc / ustar**4., 'c-', label='Tv')
 plot(yplus[starty:endy], u2_diss [starty:endy] * visc / ustar**4., 'r-', label='D')
 plot(yplus[starty:endy], u2_rdstr[starty:endy] * visc / ustar**4., 'm-', label='P')
-plot(yplus[starty:endy], u2_resid[starty:endy] * visc / ustar**4., 'k-', label='resid')
-plot(yplusMoser, u2_shearMoser, 'k--', label="Moser")
-plot(yplusMoser, u2_turbMoser , 'k--')
-plot(yplusMoser, u2_viscMoser , 'k--')
-plot(yplusMoser, u2_dissMoser , 'k--')
-plot(yplusMoser, u2_rdstrMoser, 'k--')
+plot(yplus[starty:endy], u2_resid[starty:endy] * visc / ustar**4., 'k--', label='resid')
 xlabel('y+')
 ylabel('Rxx')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, -0.6, 0.6])
+xlim(0, 200)
 
 figure()
 if(plotens):
@@ -257,16 +212,12 @@ plot(yplus[starty:endy], v2_turb [starty:endy] * visc / ustar**4., 'g-', label='
 plot(yplus[starty:endy], v2_visc [starty:endy] * visc / ustar**4., 'c-', label='Tv')
 plot(yplus[starty:endy], v2_diss [starty:endy] * visc / ustar**4., 'r-', label='D')
 plot(yplus[starty:endy], v2_rdstr[starty:endy] * visc / ustar**4., 'm-', label='P')
-plot(yplus[starty:endy], v2_resid[starty:endy] * visc / ustar**4., 'k-', label='resid')
-plot(yplusMoser, v2_turbMoser , 'k--', label="Moser")
-plot(yplusMoser, v2_viscMoser , 'k--')
-plot(yplusMoser, v2_dissMoser , 'k--')
-plot(yplusMoser, v2_rdstrMoser, 'k--')
+plot(yplus[starty:endy], v2_resid[starty:endy] * visc / ustar**4., 'k--', label='resid')
 xlabel('y+')
 ylabel('Ryy')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, -0.06, 0.06])
+xlim(0, 200)
 
 figure()
 if(plotens):
@@ -276,28 +227,26 @@ if(plotens):
     plot(yplush[starty:endy], w2_disst [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplush[starty:endy], w2_prest [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplush[starty:endy], w2_rdstrt[n,starty:endy] * visc / ustar**4., color='#cccccc')
+    plot(yplush[starty:endy], w2_buoyt [n,starty:endy] * visc / ustar**4., color='#cccccc')
 plot(yplush[starty:endy], w2_turb [starty:endy] * visc / ustar**4., 'g-', label='Tt')
 plot(yplush[starty:endy], w2_visc [starty:endy] * visc / ustar**4., 'c-', label='Tv')
 plot(yplush[starty:endy], w2_diss [starty:endy] * visc / ustar**4., 'r-', label='D')
 plot(yplush[starty:endy], w2_pres [starty:endy] * visc / ustar**4., 'y-', label='Tp')
 plot(yplush[starty:endy], w2_rdstr[starty:endy] * visc / ustar**4., 'm-', label='P')
-plot(yplush[starty:endy], w2_resid[starty:endy] * visc / ustar**4., 'k-', label='resid')
-plot(yplusMoser, w2_turbMoser , 'k--', label="Moser")
-plot(yplusMoser, w2_viscMoser , 'k--')
-plot(yplusMoser, w2_dissMoser , 'k--')
-plot(yplusMoser, w2_presMoser , 'k--')
-plot(yplusMoser, w2_rdstrMoser, 'k--')
+plot(yplush[starty:endy], w2_buoy [starty:endy] * visc / ustar**4., 'k-', label='B')
+plot(yplush[starty:endy], w2_resid[starty:endy] * visc / ustar**4., 'k--', label='resid')
 xlabel('y+')
 ylabel('Rzz')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, -0.06, 0.06])
+xlim(0, 200)
 
 figure()
 if(plotens):
   for n in range(end-start):
     plot(yplus[starty:endy], tke_sheart[n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], tke_turbt [n,starty:endy] * visc / ustar**4., color='#cccccc')
+    plot(yplus[starty:endy], tke_buoyt [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], tke_visct [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], tke_disst [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], tke_prest [n,starty:endy] * visc / ustar**4., color='#cccccc')
@@ -306,17 +255,13 @@ plot(yplus[starty:endy], tke_turb [starty:endy] * visc / ustar**4., 'g-', label=
 plot(yplus[starty:endy], tke_visc [starty:endy] * visc / ustar**4., 'c-', label='Tv')
 plot(yplus[starty:endy], tke_diss [starty:endy] * visc / ustar**4., 'r-', label='D')
 plot(yplus[starty:endy], tke_pres [starty:endy] * visc / ustar**4., 'y-', label='Tp')
-plot(yplus[starty:endy], tke_resid[starty:endy] * visc / ustar**4., 'k-', label='resid')
-plot(yplusMoser, tke_shearMoser, 'k--', label="Moser")
-plot(yplusMoser, tke_turbMoser , 'k--')
-plot(yplusMoser, tke_viscMoser , 'k--')
-plot(yplusMoser, tke_dissMoser , 'k--')
-plot(yplusMoser, tke_presMoser , 'k--')
+plot(yplus[starty:endy], tke_buoy [starty:endy] * visc / ustar**4., 'k-', label='B')
+plot(yplus[starty:endy], tke_resid[starty:endy] * visc / ustar**4., 'k--', label='resid')
 xlabel('y+')
 ylabel('tke')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, -0.3, 0.3])
+xlim(0, 200)
 
 figure()
 if(plotens):
@@ -327,42 +272,37 @@ if(plotens):
     plot(yplus[starty:endy], uw_disst [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], uw_prest [n,starty:endy] * visc / ustar**4., color='#cccccc')
     plot(yplus[starty:endy], uw_rdstrt[n,starty:endy] * visc / ustar**4., color='#cccccc')
+    plot(yplus[starty:endy], uw_buoyt [n,starty:endy] * visc / ustar**4., color='#cccccc')
 plot(yplus[starty:endy], uw_shear[starty:endy] * visc / ustar**4., 'b-', label='S')
 plot(yplus[starty:endy], uw_turb [starty:endy] * visc / ustar**4., 'g-', label='Tt')
 plot(yplus[starty:endy], uw_visc [starty:endy] * visc / ustar**4., 'c-', label='Tv')
 plot(yplus[starty:endy], uw_diss [starty:endy] * visc / ustar**4., 'r-', label='D')
 plot(yplus[starty:endy], uw_pres [starty:endy] * visc / ustar**4., 'y-', label='Tp')
 plot(yplus[starty:endy], uw_rdstr[starty:endy] * visc / ustar**4., 'm-', label='P')
-plot(yplus[starty:endy], uw_resid[starty:endy] * visc / ustar**4., 'k-', label='resid')
-plot(yplusMoser, uw_shearMoser, 'k--', label="Moser")
-plot(yplusMoser, uw_turbMoser , 'k--')
-plot(yplusMoser, uw_viscMoser , 'k--')
-plot(yplusMoser, uw_dissMoser , 'k--')
-plot(yplusMoser, uw_presMoser , 'k--')
-plot(yplusMoser, uw_rdstrMoser, 'k--')
+plot(yplus[starty:endy], uw_buoy [starty:endy] * visc / ustar**4., 'k-', label='B')
+plot(yplus[starty:endy], uw_resid[starty:endy] * visc / ustar**4., 'k--', label='resid')
 xlabel('y+')
 ylabel('Rxz')
 legend(loc=0, frameon=False)
 grid()
-axis([0, 200, -0.1, 0.1])
+xlim(0, 200)
 
 figure()
 if(plotens):
   for n in range(end-start):
-    plot(yplus[starty:endy], b2_sheart[n,starty:endy], color='#cccccc')
-    plot(yplus[starty:endy], b2_turbt [n,starty:endy], color='#cccccc')
-    plot(yplus[starty:endy], b2_visct [n,starty:endy], color='#cccccc')
-    plot(yplus[starty:endy], b2_disst [n,starty:endy], color='#cccccc')
-plot(yplus[starty:endy], b2_shear[starty:endy], 'b-', label='S')
-plot(yplus[starty:endy], b2_turb [starty:endy], 'g-', label='Tt')
-plot(yplus[starty:endy], b2_visc [starty:endy], 'c-', label='Tv')
-plot(yplus[starty:endy], b2_diss [starty:endy], 'r-', label='D')
-plot(yplus[starty:endy], b2_resid[starty:endy], 'k-', label='resid')
+    plot(yplus[starty:endy], b2_sheart[n,starty:endy] / (bflux[0]/ustar)**2, color='#cccccc')
+    plot(yplus[starty:endy], b2_turbt [n,starty:endy] / (bflux[0]/ustar)**2, color='#cccccc')
+    plot(yplus[starty:endy], b2_visct [n,starty:endy] / (bflux[0]/ustar)**2, color='#cccccc')
+    plot(yplus[starty:endy], b2_disst [n,starty:endy] / (bflux[0]/ustar)**2, color='#cccccc')
+plot(yplus[starty:endy], b2_shear[starty:endy] / (bflux[0]/ustar)**2, 'b-', label='S')
+plot(yplus[starty:endy], b2_turb [starty:endy] / (bflux[0]/ustar)**2, 'g-', label='Tt')
+plot(yplus[starty:endy], b2_visc [starty:endy] / (bflux[0]/ustar)**2, 'c-', label='Tv')
+plot(yplus[starty:endy], b2_diss [starty:endy] / (bflux[0]/ustar)**2, 'r-', label='D')
+plot(yplus[starty:endy], b2_resid[starty:endy] / (bflux[0]/ustar)**2, 'k--', label='resid')
 xlabel('y+')
 ylabel('B2')
 legend(loc=0, frameon=False)
 grid()
-#axis([0, 100, -0.6, 0.6])
 xlim(0, 200)
 
 
