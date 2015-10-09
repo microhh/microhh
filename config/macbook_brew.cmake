19490850184f7ad47c537c053e7485d9db6310f3
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-10-09 23:34:32 +0200

    Added config file for homebrew on OSX

---
+++ config/macbook_brew.cmake
@@ -0,0 +1,32 @@
+# MacBook
+if(USEMPI) 
+  set(ENV{CC}  mpicc ) # C compiler for parallel build
+  set(ENV{CXX} mpicxx) # C++ compiler for parallel build
+else()
+  set(ENV{CC}  clang  ) # C compiler for serial build
+  set(ENV{CXX} clang++) # C++ compiler for serial build
+endif()
+
+set(GNU_SED "gsed")
+
+set(USER_CXX_FLAGS "")
+set(USER_CXX_FLAGS_RELEASE "-Ofast -march=native")
+set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(FFTW_INCLUDE_DIR   "/usr/local/include")
+set(FFTW_LIB           "/usr/local/lib/libfftw3.a")
+set(NETCDF_INCLUDE_DIR "/usr/local/include")
+set(NETCDF_LIB_C       "/usr/local/lib/libnetcdf.a")
+set(NETCDF_LIB_CPP     "/usr/local/lib/libnetcdf_c++.a")
+set(HDF5_LIB_1         "/usr/local/lib/libhdf5.a")
+set(HDF5_LIB_2         "/usr/local/lib/libhdf5_hl.a")
+set(SZIP_LIB           "/usr/local/lib/libsz.a")
+set(LIBS ${FFTW_LIB} ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
+
+if(USECUDA)
+  set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+  set(LIBS ${LIBS} -rdynamic /usr/local/cuda/lib64/libcufft.so)
+  set(USER_CUDA_NVCC_FLAGS "-arch=sm_20")
+endif()
+
+add_definitions(-DRESTRICTKEYWORD=__restrict__)
