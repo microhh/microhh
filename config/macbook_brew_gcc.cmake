5c97e295d68af17917936d8c4754c498026f006e
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-10-11 14:44:55 +0200

    Added a config file for gcc using homebrew on OSX

---
+++ config/macbook_brew_gcc.cmake
@@ -0,0 +1,32 @@
+# MacBook
+if(USEMPI) 
+  set(ENV{CC}  mpicc ) # C compiler for parallel build
+  set(ENV{CXX} mpicxx) # C++ compiler for parallel build
+else()
+  set(ENV{CC}  gcc-5) # C compiler for serial build
+  set(ENV{CXX} g++-5) # C++ compiler for serial build
+endif()
+
+set(GNU_SED "gsed")
+
+set(USER_CXX_FLAGS "")
+set(USER_CXX_FLAGS_RELEASE "-Ofast -march=native")
+set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(FFTW_INCLUDE_DIR   "/usr/local/include")
+set(FFTW_LIB           "/usr/local/lib/libfftw3.dylib")
+set(NETCDF_INCLUDE_DIR "/usr/local/include")
+set(NETCDF_LIB_C       "/usr/local/lib/libnetcdf.dylib")
+set(NETCDF_LIB_CPP     "/usr/local/lib/libnetcdf_c++.dylib")
+set(HDF5_LIB_1         "/usr/local/lib/libhdf5.dylib")
+set(HDF5_LIB_2         "/usr/local/lib/libhdf5_hl.dylib")
+set(SZIP_LIB           "/usr/local/lib/libsz.dylib")
+set(LIBS ${FFTW_LIB} ${NETCDF_LIB_CPP} ${NETCDF_LIB_C} ${HDF5_LIB_2} ${HDF5_LIB_1} ${SZIP_LIB} m z curl)
+
+if(USECUDA)
+  set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+  set(LIBS ${LIBS} -rdynamic /usr/local/cuda/lib64/libcufft.so)
+  set(USER_CUDA_NVCC_FLAGS "-arch=sm_20")
+endif()
+
+add_definitions(-DRESTRICTKEYWORD=__restrict__)
