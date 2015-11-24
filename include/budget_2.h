eefef5f0fbb76fbc596062ae5365b71121724748
Bart van Stratum <bart@vanstratum.com>
2015-11-24 11:31:43 +0100

    Added empty class for 2nd order budget statistics

---
+++ include/budget_2.h
@@ -0,0 +1,50 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2015 Chiel van Heerwaarden
+ * Copyright (c) 2011-2015 Thijs Heus
+ * Copyright (c) 2014-2015 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BUDGET_2
+#define BUDGET_2
+
+class Input;
+class Master;
+class Stats;
+class Grid;
+class Fields;
+class Thermo;
+struct Mask;
+
+#include "budget.h"
+
+class Budget_2 : public Budget
+{
+    public:
+        Budget_2(Input*, Master*, Grid*, Fields*, Thermo*, Stats*);
+        ~Budget_2();
+
+        void init();
+        void create();
+        void exec_stats(Mask*);
+
+    private:
+        double* umodel;
+        double* vmodel;
+};
+#endif
---
+++ src/budget_2.cxx
@@ -0,0 +1,108 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2015 Chiel van Heerwaarden
+ * Copyright (c) 2011-2015 Thijs Heus
+ * Copyright (c) 2014-2015 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdio>
+#include <cmath>
+#include "master.h"
+#include "grid.h"
+#include "fields.h"
+#include "defines.h"
+#include "finite_difference.h"
+#include "model.h"
+#include "thermo.h"
+#include "stats.h"
+#include <netcdfcpp.h>
+
+#include "budget.h"
+#include "budget_2.h"
+
+Budget_2::Budget_2(Input* inputin, Master* masterin, Grid* gridin, Fields* fieldsin, Thermo* thermoin, Stats* statsin) :
+    Budget(inputin, masterin, gridin, fieldsin, thermoin, statsin)
+{
+    umodel = 0;
+    vmodel = 0;
+}
+
+Budget_2::~Budget_2()
+{
+    delete[] umodel;
+    delete[] vmodel;
+}
+
+void Budget_2::init()
+{
+    umodel = new double[grid.kcells];
+    vmodel = new double[grid.kcells];
+
+    for (int k=0; k<grid.kcells; ++k)
+    {
+        umodel[k] = 0.;
+        vmodel[k] = 0.;
+    }
+}
+
+void Budget_2::create()
+{
+    // add the profiles for the kinetic energy to the statistics
+    stats.add_prof("ke" , "Kinetic energy" , "m2 s-2", "z");
+    stats.add_prof("tke", "Turbulent kinetic energy" , "m2 s-2", "z");
+
+    // add the profiles for the kinetic energy budget to the statistics
+    stats.add_prof("u2_shear" , "Shear production term in U2 budget" , "m2 s-3", "z" );
+    stats.add_prof("v2_shear" , "Shear production term in V2 budget" , "m2 s-3", "z" );
+    stats.add_prof("tke_shear", "Shear production term in TKE budget", "m2 s-3", "z" );
+
+    stats.add_prof("u2_turb" , "Turbulent transport term in U2 budget" , "m2 s-3", "z" );
+    stats.add_prof("v2_turb" , "Turbulent transport term in V2 budget" , "m2 s-3", "z" );
+    stats.add_prof("w2_turb" , "Turbulent transport term in W2 budget" , "m2 s-3", "zh");
+    stats.add_prof("tke_turb", "Turbulent transport term in TKE budget", "m2 s-3", "z" );
+
+    stats.add_prof("u2_visc" , "Viscous transport term in U2 budget" , "m2 s-3", "z" );
+    stats.add_prof("v2_visc" , "Viscous transport term in V2 budget" , "m2 s-3", "z" );
+    stats.add_prof("w2_visc" , "Viscous transport term in W2 budget" , "m2 s-3", "zh");
+    stats.add_prof("tke_visc", "Viscous transport term in TKE budget", "m2 s-3", "z" );
+
+    stats.add_prof("u2_diss" , "Dissipation term in U2 budget" , "m2 s-3", "z" );
+    stats.add_prof("v2_diss" , "Dissipation term in V2 budget" , "m2 s-3", "z" );
+    stats.add_prof("w2_diss" , "Dissipation term in W2 budget" , "m2 s-3", "zh");
+    stats.add_prof("tke_diss", "Dissipation term in TKE budget", "m2 s-3", "z" );
+
+    stats.add_prof("w2_pres" , "Pressure transport term in W2 budget" , "m2 s-3", "zh");
+    stats.add_prof("tke_pres", "Pressure transport term in TKE budget", "m2 s-3", "z" );
+
+    stats.add_prof("u2_rdstr", "Pressure redistribution term in U2 budget", "m2 s-3", "z" );
+    stats.add_prof("v2_rdstr", "Pressure redistribution term in V2 budget", "m2 s-3", "z" );
+    stats.add_prof("w2_rdstr", "Pressure redistribution term in W2 budget", "m2 s-3", "zh");
+
+    if (thermo.get_switch() != "0")
+    {
+        stats.add_prof("w2_buoy" , "Buoyancy production/destruction term in W2 budget" , "m2 s-3", "zh");
+        stats.add_prof("tke_buoy", "Buoyancy production/destruction term in TKE budget", "m2 s-3", "z" );
+    }
+}
+
+void Budget_2::exec_stats(Mask* m)
+{
+    // calculate the mean of the fields
+    grid.calc_mean(umodel, fields.u->data, grid.kcells);
+    grid.calc_mean(vmodel, fields.v->data, grid.kcells);
+}
