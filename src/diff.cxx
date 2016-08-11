499fa0d0ec7175bd914a9a753e955b6603af660b
Bart van Stratum <bart@vanstratum.com>
2016-08-11 16:32:50 +0200

    Re-added the check for a MO type of surface for Smagorinsky diffusion

--- src/diff.cxx
+++ src/diff.cxx
@@ -78,12 +78,13 @@ Diff* Diff::factory(Master* masterin, Input* inputin, Model* modelin, const std:
     else if (swdiff == "smag2")
     {
         // the subgrid model requires a surface model because of the MO matching at first level
-        //if (swboundary != "surface")
-        //{
-        //    masterin->print_error("swdiff == \"smag2\" requires swboundary == \"surface\"\n");
-        //    throw 1;
-        //}
-        return new Diff_smag_2(modelin, inputin);
+        if ((swboundary == "surface") || (swboundary == "surface_patch"))
+            return new Diff_smag_2(modelin, inputin);
+        else
+        {
+            masterin->print_error("swdiff == \"smag2\" requires swboundary == \"surface\" or \"surface_patch\"\n");
+            throw 1;
+        }
     }
     else
     {
