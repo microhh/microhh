2f53e1169b023835f1359ee8d8a3416d3b3a610c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2017-09-03 20:58:00 +0200

    Proper randomize definition

--- include/fields.h
+++ include/fields.h
@@ -154,7 +154,7 @@ class Fields
 
         void add_mean_profs(Data_block&);
         // int add_mean_prof(Input*, std::string, double*, double);
-        // int randomize    (Input*, std::string, double*);
+        void randomize(Input&, std::string, TF* const restrict);
         // int add_vortex_pair(Input*);
 
         // statistics
