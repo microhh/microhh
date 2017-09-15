0bc6c60cd06b04bb843210fd90b71ea46cb22c54
Bart van Stratum <bart@vanstratum.com>
2017-09-15 12:34:17 +0200

    Added missing classes/functions to init script MUST

--- cases/must/init_must.py
+++ cases/must/init_must.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pylab as pl
 from scipy.special import erf
 
-from microhh_tools import Stretched_grid, write_output, replace_namelist_value
+from microhh_tools import replace_namelist_value
 
 def esat(T):
     return 0.611e3 * np.exp(17.2694 * (T - 273.16) / (T - 35.86))
@@ -11,10 +11,59 @@ def qsat(T,p):
     return 0.622 * esat(T) / p
 
 # Create stretched grid
-#gr = Stretched_grid(96, 40, 10, 2.0, 4.0)
-gr = Stretched_grid(64, 40, 10, 5.0, 10.0)
+class Stretched_grid:
+    def __init__(self, kmax, nloc1, nbuf1, dz1, dz2):
+        dn         = 1./kmax
+        n          = np.linspace(dn, 1.-dn, kmax)
+        nloc1     *= dn
+        nbuf1     *= dn
+        dzdn1      = dz1/dn
+        dzdn2      = dz2/dn
 
-#gr.plot()
+        dzdn       = dzdn1 + 0.5*(dzdn2-dzdn1)*(1. + np.tanh((n-nloc1)/nbuf1))
+        self.dz    = dzdn*dn
+
+        self.kmax  = kmax
+        self.z     = np.zeros(self.dz.size)
+        stretch    = np.zeros(self.dz.size)
+
+        self.z[0]  = 0.5*self.dz[0]
+        stretch[0] = 1.
+
+        for k in range(1, self.kmax):
+              self.z [k] = self.z[k-1] + 0.5*(self.dz[k-1]+self.dz[k])
+              stretch[k] = self.dz[k]/self.dz[k-1]
+
+        self.zsize = self.z[kmax-1] + 0.5*self.dz[kmax-1]
+        print('Grid: kmax=%i, zsize=%f'%(kmax,self.zsize))
+        
+    def plot(self):
+        pl.figure()
+        pl.plot(self.dz, self.z, '-x')
+        pl.xlabel('dz (m)')
+        pl.ylabel('z (m)')      
+
+def write_output(file_name, variables, n):
+    """ Write input files for MicroHH; e.g. initial vertical profiles or time series """
+    f = open(file_name, 'w')
+
+    for var in variables.keys():
+        f.write('{0:^21s} '.format(var))
+    f.write('\n')
+
+    for k in range(n):
+        for var in variables.keys():
+            f.write('{0:+1.14E} '.format(variables[var][k]))
+        f.write('\n')
+
+    f.close()
+
+# Create stretched grid
+#gr = Stretched_grid(96, 40, 10, 2.0, 4.0)
+#gr = Stretched_grid(64, 40, 10, 5.0, 10.0)
+gr = Stretched_grid(72, 50, 10, 3.0, 20.0)
+
+gr.plot()
 
 # Write back vertical extent domain
 replace_namelist_value('zsize', gr.zsize)
