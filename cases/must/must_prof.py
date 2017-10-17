d8ff775f120259d7a9c10007509d14d2b5435b1d
Bart van Stratum <bart@vanstratum.com>
2017-10-17 11:22:50 +0200

    Update MUST case

--- cases/must/must_prof.py
+++ cases/must/must_prof.py
@@ -35,15 +35,29 @@ class Stretched_grid:
 
         self.zsize = self.z[kmax-1] + 0.5*self.dz[kmax-1]
         print('Grid: kmax=%i, zsize=%f'%(kmax,self.zsize))
-
+        
     def plot(self):
         pl.figure()
         pl.plot(self.dz, self.z, '-x')
         pl.xlabel('dz (m)')
-        pl.ylabel('z (m)')
+        pl.ylabel('z (m)')      
 
+def write_output(file_name, variables, n):
+    """ Write input files for MicroHH; e.g. initial vertical profiles or time series """
+    f = open(file_name, 'w')
 
-if __name__ == '__main__':
+    for var in variables.keys():
+        f.write('{0:^21s} '.format(var))
+    f.write('\n')
+
+    for k in range(n):
+        for var in variables.keys():
+            f.write('{0:+1.14E} '.format(variables[var][k]))
+        f.write('\n')
+
+    f.close()
+
+if __name__ == "__main__":
 
     import microhh_tools as mht
 
@@ -52,44 +66,48 @@ if __name__ == '__main__':
     #gr = Stretched_grid(64, 40, 10, 5.0, 10.0)
     #gr = Stretched_grid(72,  50, 10, 3.0, 20.0)
     #gr = Stretched_grid(144, 120, 20, 1.0, 15.0)
-    gr = Stretched_grid(192, 180, 10, 0.5, 15.0)
+    #gr = Stretched_grid(192, 180, 10, 0.5, 15.0)
+    gr = Stretched_grid(384, 370, 20, 0.25, 15.0)
     gr.plot()
-
+    
     # Write back vertical extent domain
     mht.replace_namelist_value('zsize', gr.zsize)
     mht.replace_namelist_value('ktot', gr.z.size)
-
+    
     # Create initial vertical profiles
     thl = np.ones(gr.z.size)*318
     qt  = 6.4 - gr.z * 0.0030
     qt /= 1000
-
-    u   = np.ones(gr.z.size)*6.25
+    
+    #u   = np.ones(gr.z.size)*6.25
     ug  = np.ones(gr.z.size)*3
-
-    v   = np.ones(gr.z.size)*3.6
+    
+    #v   = np.ones(gr.z.size)*3.6
     vg  = np.ones(gr.z.size)*7
-
+    
+    u = 1.4*np.log(gr.z+10) - 0.006*gr.z
+    v = 0.6*np.log(gr.z+5) #- 0.001*z
+    
     # Large scale tendencies thl and qt
     thlls = -1.395 + 0.00089 * gr.z
     qtls  = 0.0015 + 0.0002  * gr.z
-
+    
     # To MicroHH units (hours -> sec, g kg-1 -> kg kg-1
     thlls /= 3600.
     qtls  /= (3600. * 1000.)
-
+    
     # Remove tendency in/near SBL
     zt   = 100       # Height of transition
     dz   = 100       # Depth of transition layer
     fac  = 0.5 + 0.5*erf((gr.z-zt)/(0.25*dz))
-
+    
     thlls2 = thlls #* fac
     qtls2  = qtls  #* fac
-
+    
     # Write to .prof file as input for MicroHH
     variables = {'z':gr.z, 'thl':thl, 'qt':qt, 'u':u, 'ug':ug, 'v':v, 'vg':vg, 'thlls':thlls2, 'qtls':qtls2}
     mht.write_output('must.prof', variables, gr.z.size)
-
+    
     # Create surface time dependent input
     time = np.arange(0.5,5.7501,0.25)       # h UTC
     # Surface potential temperature fitted to observations
@@ -97,14 +115,14 @@ if __name__ == '__main__':
     th1cm_fit = ths_func[3] + ths_func[2]*time + ths_func[1]*time**2 + ths_func[0]*time**3
     time -= 0.5                             # h since start of experiment
     time *= 3600                            # s   "    "           "
-
+    
     pbot = 87000
     exnbot = (pbot / 1e5)**(287.04/1005.)
     Tbot = th1cm_fit * exnbot
     qsbot = qsat(Tbot, pbot)
-
+    
     RHbot = (0.18 + 0.07 * (time/time.max()))
     qbot = RHbot * qsbot
-
+    
     variables = {'t':time, 'sbot[thl]':th1cm_fit, 'sbot[qt]':qbot}
     mht.write_output('must.time', variables, time.size)
