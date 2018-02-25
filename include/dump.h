fb24e57e77e94ca0da2e33c8bbd656c3adfa7ef5
Thijs Heus <cls0160@owens-rw01.ten.osc.edu>
2018-02-24 19:56:19 -0500

    Cross, Column, Dump. Compiles, not yet tested

---
+++ include/dump.h
@@ -0,0 +1,59 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2017 Chiel van Heerwaarden
+ * Copyright (c) 2011-2017 Thijs Heus
+ * Copyright (c) 2014-2017 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DUMP
+#define DUMP
+
+class Master;
+class Input;
+template<typename> class Grid;
+template<typename> class Fields;
+
+template<typename TF>
+class Dump
+{
+    public:
+        Dump(Master&, Grid<TF>&, Fields<TF>&, Input&);
+        ~Dump();
+
+        void init(double);
+        void create();
+
+        unsigned long get_time_limit(unsigned long);
+        bool get_switch() { return swdump; }
+        std::vector<std::string>* get_dumplist();
+
+        bool do_dump(unsigned long);
+        void save_dump(TF*, std::string, int);
+
+    private:
+    protected:
+        Grid<TF>& grid;
+        Fields<TF>& fields;
+        Master& master;
+
+        std::vector<std::string> dumplist; ///< List with all dumps from the ini file.
+        bool swdump;           ///< Statistics on/off switch
+        double sampletime;
+        unsigned long isampletime;
+};
+#endif
