06b16f080a8b1537391119fc9d6709fa850d5e67
julietbravo <bart@vanstratum.com>
2018-06-11 19:30:34 +0200

    Added microphysics autoconversion

--- src/microphys_2mom_warm.cxx
+++ src/microphys_2mom_warm.cxx
@@ -33,6 +33,8 @@
 #include "microphys.h"
 #include "microphys_2mom_warm.h"
 
+using namespace Constants;
+
 namespace
 {
     template<typename TF>
@@ -77,34 +79,34 @@ namespace mp3d
                         const int istart, const int jstart, const int kstart,
                         const int iend,   const int jend,   const int kend,
                         const int jj, const int kk,
-                        Micro_2mom_warm_constants<TF> micro_constans)
+                        Micro_2mom_warm_constants<TF> constants)
     {
         const TF x_star = 2.6e-10;       // SB06, list of symbols, same as UCLA-LES
         const TF k_cc   = 9.44e9;        // UCLA-LES (Long, 1974), 4.44e9 in SB06, p48
         const TF nu_c   = 1;             // SB06, Table 1., same as UCLA-LES
-        //const TF kccxs  = k_cc / (TF(20.) * x_star) * (nu_c+2)*(nu_c+4) / pow(nu_c+1, 2);
+        const TF kccxs  = k_cc / (TF(20.) * x_star) * (nu_c+2)*(nu_c+4) / pow(nu_c+1, 2);
 
-        //for (int k=kstart; k<kend; k++)
-        //    for (int j=jstart; j<jend; j++)
-        //        #pragma ivdep
-        //        for (int i=istart; i<iend; i++)
-        //        {
-        //            const int ijk = i + j*jj + k*kk;
-        //            if(ql[ijk] > ql_min<TF>)
-        //            {
-        //                const TF xc      = rho[k] * ql[ijk] / Nc0<TF>; // Mean mass of cloud drops [kg]
-        //                const TF tau     = TF(1.) - ql[ijk] / (ql[ijk] + qr[ijk] + dsmall); // SB06, Eq 5
-        //                const TF phi_au  = TF(600.) * pow(tau, TF(0.68)) * pow(TF(1.) - pow(tau, TF(0.68)), 3); // UCLA-LES
-        //                //const TF phi_au  = 400. * pow(tau, 0.7) * pow(1. - pow(tau, 0.7), 3); // SB06, Eq 6
-        //                const TF au_tend = rho[k] * kccxs * pow(ql[ijk], 2) * pow(xc, 2) *
-        //                                       (TF(1.) + phi_au / pow(TF(1.)-tau, 2)); // SB06, eq 4
+        for (int k=kstart; k<kend; k++)
+            for (int j=jstart; j<jend; j++)
+                #pragma ivdep
+                for (int i=istart; i<iend; i++)
+                {
+                    const int ijk = i + j*jj + k*kk;
+                    if(ql[ijk] > constants.ql_min)
+                    {
+                        const TF xc      = rho[k] * ql[ijk] / constants.Nc0;    // Mean mass of cloud drops [kg]
+                        const TF tau     = TF(1.) - ql[ijk] / (ql[ijk] + qr[ijk] + dsmall);    // SB06, Eq 5
+                        const TF phi_au  = TF(600.) * pow(tau, TF(0.68)) * pow(TF(1.) - pow(tau, TF(0.68)), 3);    // UCLA-LES
+                        //const TF phi_au  = 400. * pow(tau, 0.7) * pow(1. - pow(tau, 0.7), 3);    // SB06, Eq 6
+                        const TF au_tend = rho[k] * kccxs * pow(ql[ijk], 2) * pow(xc, 2) *
+                                               (TF(1.) + phi_au / pow(TF(1.)-tau, 2)); // SB06, eq 4
 
-        //                qrt[ijk]  += au_tend;
-        //                nrt[ijk]  += au_tend * rho[k] / x_star;
-        //                qtt[ijk]  -= au_tend;
-        //                thlt[ijk] += Lv / (cp * exner[k]) * au_tend;
-        //            }
-        //        }
+                        qrt[ijk]  += au_tend;
+                        nrt[ijk]  += au_tend * rho[k] / x_star;
+                        qtt[ijk]  -= au_tend;
+                        thlt[ijk] += Lv / (cp * exner[k]) * au_tend;
+                    }
+                }
     }
 }
 
@@ -154,6 +156,10 @@ void Microphys_2mom_warm<TF>::exec(Thermo<TF>& thermo)
     auto ql = fields.get_tmp();
     thermo.get_thermo_field(*ql, "ql", false, false);
 
+    // Get pressure and exner function from thermodynamics
+    std::vector<TF> p     = thermo.get_p_vector();
+    std::vector<TF> exner = thermo.get_exner_vector();
+
     // Microphysics is handled in XZ slices, to
     // (1) limit the required number of tmp fields
     // (2) re-use some expensive calculations used in multiple microphysics routines.
@@ -192,19 +198,12 @@ void Microphys_2mom_warm<TF>::exec(Thermo<TF>& thermo)
     // ---------------------------------
 
     // Autoconversion; formation of rain drop by coagulating cloud droplets
-    //autoconversion(fields.st.at("qr")->fld.data(), fields.st.at("nr")->fld.data(), fields.st.at("qt")->fld.data(), fields.st.at("thl")->fld.data(),
-    //               fields.sp.at("qr")->fld.data(), ql->fld.data(), fields.rhoref.data(), bs.exnref.data(),
-    //               gd.istart, gd.jstart, gd.kstart,
-    //               gd.iend,   gd.jend,   gd.kend,
-    //               gd.icells, gd.ijcells,
-    //               micro_constants);
-
-    // Accretion; growth of raindrops collecting cloud droplets
-    //accretion(fields.st.at("qr")->fld.data(), fields.st.at("qt")->fld.data(), fields.st.at("thl")->fld.data(),
-    //          fields.sp.at("qr")->fld.data(), ql->fld.data(), fields.rhoref.data(), bs.exnref.data(),
-    //          gd.istart, gd.jstart, gd.kstart,
-    //          gd.iend,   gd.jend,   gd.kend,
-    //          gd.icells, gd.ijcells);
+    mp3d::autoconversion(fields.st.at("qr")->fld.data(), fields.st.at("nr")->fld.data(), fields.st.at("qt")->fld.data(), fields.st.at("thl")->fld.data(),
+                         fields.sp.at("qr")->fld.data(), ql->fld.data(), fields.rhoref.data(), exner.data(),
+                         gd.istart, gd.jstart, gd.kstart,
+                         gd.iend,   gd.jend,   gd.kend,
+                         gd.icells, gd.ijcells,
+                         micro_constants);
 
 
     // Release all local tmp fields in use
