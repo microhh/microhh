d25ab2621d1858c1830a8646bf5d20d5c7575ab7
Bart van Stratum <bart@vanstratum.com>
2018-06-21 14:07:00 +0200

    Added empty .cu versions of the 4th order advec/diff/pres to fix CUDA compilation

---
+++ src/advec_4.cu
@@ -0,0 +1,52 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2017 Chiel van Heerwaarden
+ * Copyright (c) 2011-2017 Thijs Heus
+ * Copyright (c) 2014-2017 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "advec_4.h"
+#include "grid.h"
+#include "fields.h"
+#include "tools.h"
+#include "constants.h"
+#include "finite_difference.h"
+#include "field3d_operators.h"
+
+#ifdef USECUDA
+template<typename TF>
+unsigned long Advec_4<TF>::get_time_limit(unsigned long idt, double dt)
+{
+    throw std::runtime_error("Advec_4 not yet implemented on GPU\n");
+}
+
+template<typename TF>
+double Advec_4<TF>::get_cfl(const double dt)
+{
+    throw std::runtime_error("Advec_4 not yet implemented on GPU\n");
+}
+
+template<typename TF>
+void Advec_4<TF>::exec()
+{
+    throw std::runtime_error("Advec_4 not yet implemented on GPU\n");
+}
+#endif
+
+template class Advec_4<double>;
+template class Advec_4<float>;
