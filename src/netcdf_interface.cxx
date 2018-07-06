087f39808a24b6cdc94e7a04596b9bc88c7373e8
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2018-07-06 16:34:06 +0200

    Netcdf reader working in parallel compilation.

--- src/netcdf_interface.cxx
+++ src/netcdf_interface.cxx
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <vector>
 #include <netcdf.h>
 
@@ -14,8 +15,9 @@ namespace
         throw std::runtime_error(error);
     }
 
-    void nc_check(const int return_value)
+    void nc_check(Master& master, int return_value)
     {
+        master.broadcast(&return_value, 1);
         if (return_value != NC_NOERR)
             nc_throw(return_value);
     }
@@ -24,102 +26,124 @@ namespace
 Netcdf_file::Netcdf_file(Master& master, const std::string& name, Netcdf_mode mode) :
     Netcdf_handle(master)
 {
-    int nc_check_output = NC_NOERR;
+    int nc_check_code;
 
     if (master.get_mpiid() == 0)
     {
         if (mode == Netcdf_mode::Create)
-            nc_check_output = nc_create(name.c_str(), NC_NOCLOBBER | NC_NETCDF4, &ncid);
+            nc_check_code = nc_create(name.c_str(), NC_NOCLOBBER | NC_NETCDF4, &ncid);
         else if (mode == Netcdf_mode::Write)
-            nc_check_output = nc_open(name.c_str(), NC_WRITE | NC_NETCDF4, &ncid);
+            nc_check_code = nc_open(name.c_str(), NC_WRITE | NC_NETCDF4, &ncid);
         else if (mode == Netcdf_mode::Read)
-            nc_check_output = nc_open(name.c_str(), NC_NOWRITE | NC_NETCDF4, &ncid);
+            nc_check_code = nc_open(name.c_str(), NC_NOWRITE | NC_NETCDF4, &ncid);
     }
 
-    master.broadcast(&nc_check_output, 1);
-    nc_check( nc_check_output );
+    nc_check(master, nc_check_code);
 
     root_ncid = ncid;
 
     if (master.get_mpiid() == 0)
     {
         if (mode == Netcdf_mode::Create)
-            nc_check_output =  nc_enddef(root_ncid);
+            nc_check_code =  nc_enddef(root_ncid);
     }
 
-    master.broadcast(&nc_check_output, 1);
-    nc_check( nc_check_output );
+    nc_check(master, nc_check_code);
 }
 
 Netcdf_file::~Netcdf_file()
 {
-    int nc_check_output = NC_NOERR;
+    int nc_check_code;
 
     if (master.get_mpiid() == 0)
-        nc_check_output = nc_close(ncid);
-
-    master.broadcast(&nc_check_output, 1);
-    nc_check( nc_check_output );
+        nc_check_code = nc_close(ncid);
+    nc_check(master, nc_check_code);
 }
 
 void Netcdf_handle::add_dimension(const std::string& dim_name, const size_t dim_size)
 {
+    int nc_check_code;
+
     if (master.get_mpiid() == 0)
-    {
-        nc_check( nc_redef(root_ncid) );
+        nc_check_code = nc_redef(root_ncid);
+    nc_check(master, nc_check_code);
 
-        int dim_id;
-        int def_out = nc_def_dim(ncid, dim_name.c_str(), dim_size, &dim_id);
+    int dim_id;
+    int def_out;
 
-        // Dimension is written or already exists.
-        if (def_out == NC_NOERR)
-            dims.emplace(dim_name, dim_id);
-        else if (def_out == NC_ENAMEINUSE)
-        {}
-        // Error.
-        else
-            nc_throw(def_out);
+    if (master.get_mpiid() == 0)
+        def_out = nc_def_dim(ncid, dim_name.c_str(), dim_size, &dim_id);
 
-        nc_check( nc_enddef(root_ncid) );
-    }
+    master.broadcast(&def_out, 1);
+
+    // Dimension is written or already exists.
+    if (def_out == NC_NOERR)
+        dims.emplace(dim_name, dim_id);
+    else if (def_out == NC_ENAMEINUSE)
+    {}
+    // Error.
+    else
+        nc_throw(def_out);
+
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_enddef(root_ncid);
+
+    nc_check(master, nc_check_code);
 }
 
 Netcdf_variable Netcdf_handle::add_variable(
         const std::string& var_name,
         const std::vector<std::string> dim_names)
 {
+    int nc_check_code;
+
     int var_id = -1;
     std::vector<size_t> dim_sizes;
 
     if (master.get_mpiid() == 0)
+        nc_check_code = nc_redef(root_ncid);
+    nc_check(master, nc_check_code);
+
+    int ndims = dim_names.size();
+    std::vector<int> dim_ids;
+
+    for (const std::string& dim_name : dim_names)
+        dim_ids.push_back(dims.at(dim_name));
+
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_def_var(ncid, var_name.c_str(), NC_DOUBLE, ndims, dim_ids.data(), &var_id);
+    nc_check(master, nc_check_code);
+
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_enddef(root_ncid);
+    nc_check(master, nc_check_code);
+
+    // Broadcast the dim_ids size of the main process to run the for loop on all processes.
+    int dim_ids_size = dim_ids.size();
+    master.broadcast(&dim_ids_size, 1);
+
+    for (int i=0; i<dim_ids_size; ++i)
     {
-        nc_check ( nc_redef(root_ncid) );
+        const int dim_id = dim_ids[i];
 
-        int ndims = dim_names.size();
-        std::vector<int> dim_ids;
-        for (const std::string& dim_name : dim_names)
-            dim_ids.push_back(dims.at(dim_name));
+        size_t dim_len;
 
-        nc_check( nc_def_var(ncid, var_name.c_str(), NC_DOUBLE, ndims, dim_ids.data(), &var_id) );
-        nc_check( nc_enddef(root_ncid) );
+        if (master.get_mpiid() == 0)
+            nc_check_code = nc_inq_dimlen(ncid, dim_id, &dim_len);
+        nc_check(master, nc_check_code);
 
-        for (size_t i=0; i<dim_ids.size(); ++i)
+        int dim_len_int = static_cast<int>(dim_len);
+        master.broadcast(&dim_len_int, 1);
+
+        if (dim_len_int == NC_UNLIMITED)
         {
-            const int dim_id = dim_ids[i];
-
-            size_t dim_len;
-            nc_check( nc_inq_dimlen(ncid, dim_id, &dim_len) );
-
-            if (dim_len == NC_UNLIMITED)
-            {
-                if (i == 0)
-                    dim_sizes.push_back(1);
-                else
-                    throw std::runtime_error("Only the outermost dimension is allowed to be an NC_UNLIMITED dimension");
-            }
+            if (i == 0)
+                dim_sizes.push_back(1);
             else
-                dim_sizes.push_back(dim_len);
+                throw std::runtime_error("Only the leftmost dimension is allowed to be an NC_UNLIMITED dimension");
         }
+        else
+            dim_sizes.push_back(dim_len);
     }
 
     return Netcdf_variable(master, *this, var_id, dim_sizes);
@@ -135,11 +159,12 @@ void Netcdf_handle::insert(
         const std::vector<size_t>& i_start,
         const std::vector<size_t>& i_count)
 {
+    int nc_check_code;
+
+    // CvH: Add proper size checking.
     if (master.get_mpiid() == 0)
-    {
-        // CvH: Add proper size checking.
-        nc_check( nc_put_vara_double(ncid, var_id, i_start.data(), i_count.data(), values.data()) );
-    }
+        nc_check_code = nc_put_vara_double(ncid, var_id, i_start.data(), i_count.data(), values.data());
+    nc_check(master, nc_check_code);
 }
 
 void Netcdf_handle::insert(
@@ -148,23 +173,30 @@ void Netcdf_handle::insert(
         const std::vector<size_t>& i_start,
         const std::vector<size_t>& i_count)
 {
+    int nc_check_code;
+
+    // CvH: Add proper size checking.
     if (master.get_mpiid() == 0)
-    {
-        // CvH: Add proper size checking.
-        nc_check( nc_put_vara_double(ncid, var_id, i_start.data(), i_count.data(), &value) );
-    }
+        nc_check_code = nc_put_vara_double(ncid, var_id, i_start.data(), i_count.data(), &value);
+    nc_check(master, nc_check_code);
 }
 
 Netcdf_group Netcdf_handle::add_group(const std::string& name)
 {
     int group_ncid = -1;
+    int nc_check_code;
 
     if (master.get_mpiid() == 0)
-    {
-        nc_check( nc_redef(root_ncid) );
-        nc_check( nc_def_grp(ncid, name.c_str(), &group_ncid) );
-        nc_check( nc_enddef(root_ncid) );
-    }
+        nc_check_code = nc_redef(root_ncid);
+    nc_check(master, nc_check_code);
+
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_def_grp(ncid, name.c_str(), &group_ncid);
+    nc_check(master, nc_check_code);
+
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_enddef(root_ncid);
+    nc_check(master, nc_check_code);
 
     return Netcdf_group(master, group_ncid, root_ncid);
 }
@@ -172,9 +204,11 @@ Netcdf_group Netcdf_handle::add_group(const std::string& name)
 Netcdf_group Netcdf_handle::get_group(const std::string& name)
 {
     int group_ncid = -1;
+    int nc_check_code;
 
     if (master.get_mpiid() == 0)
-        nc_check( nc_inq_ncid(ncid, name.c_str(), &group_ncid) );
+        nc_check_code = nc_inq_ncid(ncid, name.c_str(), &group_ncid);
+    nc_check(master, nc_check_code);
 
     return Netcdf_group(master, group_ncid, root_ncid);
 }
@@ -195,14 +229,22 @@ void Netcdf_handle::get_variable(
         const std::vector<size_t>& i_start,
         const std::vector<size_t>& i_count)
 {
-    if (master.get_mpiid() == 0)
-    {
-        int var_id;
-        nc_check( nc_inq_varid(ncid, name.c_str(), &var_id) );
+    int nc_check_code;
+    int var_id;
 
-        // CvH: Add check if the vector is large enough.
-        nc_check( nc_get_vara_double(ncid, var_id, i_start.data(), i_count.data(), values.data()) );
-    }
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_inq_varid(ncid, name.c_str(), &var_id);
+    nc_check(master, nc_check_code);
+
+    // CvH: Add check if the vector is large enough.
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_get_vara_double(ncid, var_id, i_start.data(), i_count.data(), values.data());
+    nc_check(master, nc_check_code);
+
+    // If the vector is long enough, it can be copied. We assume that this routine does NOT resize vectors.
+    int total_count = std::accumulate(i_count.begin(), i_count.end(), 1, std::multiplies<>());
+    master.broadcast(&total_count, 1);
+    master.broadcast(values.data(), total_count);
 }
 
 template<>
@@ -212,30 +254,36 @@ void Netcdf_handle::get_variable(
         const std::vector<size_t>& i_start,
         const std::vector<size_t>& i_count)
 {
+    int nc_check_code;
+
+    int var_id;
+    if (master.get_mpiid() == 0)
+        nc_check_code = nc_inq_varid(ncid, name.c_str(), &var_id);
+    nc_check(master, nc_check_code);
+
+    // CvH: This needs to be removed once the stats is properly templated.
+    std::vector<double>values_double(values.size());
+
+    // CvH: Add check if the vector is large enough.
     if (master.get_mpiid() == 0)
     {
-        int var_id;
-        nc_check( nc_inq_varid(ncid, name.c_str(), &var_id) );
-
-        // CvH: This needs to be removed once the stats is properly templated.
         std::vector<double>values_double(values.size());
-
-        // CvH: Add check if the vector is large enough.
-        nc_check( nc_get_vara_double(ncid, var_id, i_start.data(), i_count.data(), values_double.data()) );
-
-        std::copy(values_double.begin(), values_double.end(), values.begin());
+        nc_check_code = nc_get_vara_double(ncid, var_id, i_start.data(), i_count.data(), values_double.data());
     }
+    nc_check(master, nc_check_code);
+
+    if (master.get_mpiid() == 0)
+        std::copy(values_double.begin(), values_double.end(), values.begin());
 }
 
-
+// Variable does not communicate with NetCDF library directly.
 Netcdf_variable::Netcdf_variable(Master& master, Netcdf_handle& nc_file, const int var_id, const std::vector<size_t>& dim_sizes) :
     master(master), nc_file(nc_file), var_id(var_id), dim_sizes(dim_sizes)
 {}
 
 void Netcdf_variable::insert(const std::vector<double>& values, const std::vector<size_t> i_start)
 {
-    if (master.get_mpiid() == 0)
-        nc_file.insert(values, var_id, i_start, dim_sizes);
+    nc_file.insert(values, var_id, i_start, dim_sizes);
 }
 
 void Netcdf_variable::insert(
@@ -243,14 +291,12 @@ void Netcdf_variable::insert(
         const std::vector<size_t> i_start,
         const std::vector<size_t> i_count)
 {
-    if (master.get_mpiid() == 0)
-        nc_file.insert(values, var_id, i_start, i_count);
+    nc_file.insert(values, var_id, i_start, i_count);
 }
 
 void Netcdf_variable::insert(const double value, const std::vector<size_t> i_start)
 {
-    if (master.get_mpiid() == 0)
-        nc_file.insert(value, var_id, i_start, dim_sizes);
+    nc_file.insert(value, var_id, i_start, dim_sizes);
 }
 
 // template void Netcdf_handle::get_variable<double>(std::vector<double>&, const std::string&, const std::vector<size_t>&, const std::vector<size_t>&);
