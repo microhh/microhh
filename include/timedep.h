6c9771833e5e5653d6a6be3ecba3e4e580b4b8e2
julietbravo <bart@vanstratum.com>
2019-03-16 18:01:50 +0100

    Account for the Galilean transformation in the nudging profiles

--- include/timedep.h
+++ include/timedep.h
@@ -42,7 +42,7 @@ class Timedep
         void create_timedep();
         void update_time_dependent(TF&, Timeloop<TF>&);
 
-        void create_timedep_prof();
+        void create_timedep_prof(TF);
         void update_time_dependent_prof(std::vector<TF>&, Timeloop<TF>&);
 
         #ifdef USECUDA
