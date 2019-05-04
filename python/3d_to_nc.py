dc57bb8ba6c3e740db5297451df8798cf534eec5
Thijs Heus <6053388+thijsheus@users.noreply.github.com>
2019-05-04 14:56:07 -0400

    Make 3d_to_nc script compatible with 8-byte floats

--- python/3d_to_nc.py
+++ python/3d_to_nc.py
@@ -1,20 +1,32 @@
 import numpy   as np
 import struct  as st
 import netCDF4 as nc4
+import os
+import sys
+import microhh_tools as mht     # available in microhh/python directory
+
+if (len(sys.argv)>1):
+  os.chdir(sys.argv[1])
+
+
+
+# Read the namelist settings
+nl = mht.Read_namelist()
 
 # Settings -------
-variable   = 'w'
-nx         = 1024
-ny         = 1024
-nz         = 256
-starttime  = 0
-sampletime = 600
-endtime    = 10800
-iotimeprec = -2
+variables  = nl['dump']['dumplist']
+nx         = nl['grid']['itot']
+ny         = nl['grid']['jtot']
+nz         = nl['grid']['ktot']
+starttime  = nl['time']['starttime']
+endtime    = nl['time']['endtime']
+sampletime = nl['dump']['sampletime']
+iotimeprec = 0
 nxsave     = nx
 nysave     = ny
 nzsave     = nz
 endian     = 'little'
+loadtype   = 'float'
 savetype   = 'float'
 # End settings ---
 
@@ -25,6 +37,18 @@ elif (endian == 'big'):
     en = '>'
 else:
     raise RuntimeError("Endianness has to be little or big")
+
+# Set the correct string for the loadtype
+if (loadtype == 'double'):
+    TF = 8
+    ra = 'd'
+elif (loadtype == 'float'):
+    TF = 4
+    ra = 'f'
+else:
+    raise RuntimeError("The savetype has to be float or double")
+
+fstring = '{0}{1}'+ra
 
 # Set the correct string for the savetype
 if (savetype == 'double'):
@@ -40,63 +64,70 @@ nt = int((endtime - starttime) / sampletime + 1)
 # Read grid properties from grid.0000000
 n   = nx*ny*nz
 fin = open("grid.{:07d}".format(0),"rb")
-raw = fin.read(nx*8)
-x   = np.array(st.unpack('{0}{1}d'.format(en, nx), raw))
-raw = fin.read(nx*8)
-xh  = np.array(st.unpack('{0}{1}d'.format(en, nx), raw))
-raw = fin.read(ny*8)
-#y   = np.array(st.unpack('{0}{1}d'.format(en, ny), raw))
-raw = fin.read(ny*8)
-#yh  = np.array(st.unpack('{0}{1}d'.format(en, ny), raw))
-raw = fin.read(nz*8)
-#z   = np.array(st.unpack('{0}{1}d'.format(en, nz), raw))
-raw = fin.read(nz*8)
-#zh  = np.array(st.unpack('{0}{1}d'.format(en, nz), raw))
+raw = fin.read(nx*TF)
+x   = np.array(st.unpack(fstring.format(en, nx), raw))
+raw = fin.read(nx*TF)
+xh  = np.array(st.unpack(fstring.format(en, nx), raw))
+raw = fin.read(ny*TF)
+y   = np.array(st.unpack(fstring.format(en, ny), raw))
+raw = fin.read(ny*TF)
+yh  = np.array(st.unpack(fstring.format(en, ny), raw))
+raw = fin.read(nz*TF)
+z   = np.array(st.unpack(fstring.format(en, nz), raw))
+raw = fin.read(nz*TF)
+zh  = np.array(st.unpack(fstring.format(en, nz), raw))
 fin.close()
 
-# Create netCDF file
-ncfile = nc4.Dataset("%s.nc"%variable, "w")
 
-if  (variable=='u'): loc = [1,0,0]
-elif(variable=='v'): loc = [0,1,0]
-elif(variable=='w'): loc = [0,0,1]
-else:                loc = [0,0,0]
+# Loop over the different variables
+for variable in variables:
+  # Create netCDF file
+  try:
+    ncfile = nc4.Dataset("%s.nc"%variable, "w")
 
-locx = 'x' if loc[0] == 0 else 'xh'
-locy = 'y' if loc[1] == 0 else 'yh'
-locz = 'z' if loc[2] == 0 else 'zh'
+    if  (variable=='u'): loc = [1,0,0]
+    elif(variable=='v'): loc = [0,1,0]
+    elif(variable=='w'): loc = [0,0,1]
+    else:                loc = [0,0,0]
 
-dim_x  = ncfile.createDimension(locx,  nxsave)
-dim_y  = ncfile.createDimension(locy,  nysave)
-dim_z  = ncfile.createDimension(locz,  nzsave)
-dim_t  = ncfile.createDimension('time', nt)
+    locx = 'x' if loc[0] == 0 else 'xh'
+    locy = 'y' if loc[1] == 0 else 'yh'
+    locz = 'z' if loc[2] == 0 else 'zh'
 
-var_x  = ncfile.createVariable(locx, sa, (locx,))
-var_y  = ncfile.createVariable(locy, sa, (locy,))
-var_z  = ncfile.createVariable(locz, sa, (locz,))
-var_t  = ncfile.createVariable('time', 'i4', ('time',))
-var_3d = ncfile.createVariable(variable, sa, ('time',locz, locy, locx,))
+    dim_x  = ncfile.createDimension(locx,  nxsave)
+    dim_y  = ncfile.createDimension(locy,  nysave)
+    dim_z  = ncfile.createDimension(locz,  nzsave)
+    dim_t  = ncfile.createDimension('time', nt)
 
-var_t.units = "time units since start"
+    var_x  = ncfile.createVariable(locx, sa, (locx,))
+    var_y  = ncfile.createVariable(locy, sa, (locy,))
+    var_z  = ncfile.createVariable(locz, sa, (locz,))
+    var_t  = ncfile.createVariable('time', 'i4', ('time',))
+    var_3d = ncfile.createVariable(variable, sa, ('time',locz, locy, locx,),zlib=True)
 
-# Write grid properties to netCDF
-var_x[:] = x[:nxsave] if locx=='x' else xh[:nxsave]
-#var_y[:] = y[:nysave] if locy=='y' else yh[:nysave]
-#var_z[:] = z[:nzsave] if locz=='z' else zh[:nzsave]
+    var_t.units = "time units since start"
 
-# Loop through the files and read 3d field
-for t in range(nt):
-    time = int((starttime + t*sampletime) / 10**iotimeprec)
-    print("Processing t={:07d}".format(time))
+    # Write grid properties to netCDF
+    var_x[:] = x[:nxsave] if locx=='x' else xh[:nxsave]
+    var_y[:] = y[:nysave] if locy=='y' else yh[:nysave]
+    var_z[:] = z[:nzsave] if locz=='z' else zh[:nzsave]
 
-    var_t[t] = time * 10**iotimeprec
+    # Loop through the files and read 3d field
+    for t in range(nt):
+        time = int((starttime + t*sampletime) / 10**iotimeprec)
+        print("Processing t={:07d}".format(time))
 
-    fin = open("%s.%07i"%(variable, time),"rb")
-    for k in range(nzsave):
-        raw = fin.read(nx*ny*8)
-        tmp = np.array(st.unpack('{0}{1}d'.format(en, nx*ny), raw))
-        var_3d[t,k,:,:] = tmp.reshape((ny, nx))[:nysave,:nxsave]
-    fin.close()
-    ncfile.sync()
+        var_t[t] = time * 10**iotimeprec
 
-ncfile.close()
+        fin = open("%s.%07i"%(variable, time),"rb")
+        for k in range(nzsave):
+            raw = fin.read(nx*ny*TF)
+            tmp = np.array(st.unpack(fstring.format(en, nx*ny), raw))
+            var_3d[t,k,:,:] = tmp.reshape((ny, nx))[:nysave,:nxsave]
+        fin.close()
+        ncfile.sync()
+
+    ncfile.close()
+  except:
+    print("Failed to create %s.nc"%variable)
+    ncfile.close()
