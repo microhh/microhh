35a8ffbe62590ae9f3ace5cd8328463d68168718
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2019-06-07 21:27:27 +0200

    Added headers to the RRTMGP code with copyright and licence.

--- include_rrtmgp/Source_functions.h
+++ include_rrtmgp/Source_functions.h
@@ -1,3 +1,27 @@
+/*
+ * This file is part of a C++ interface to the Radiative Transfer for Energetics (RTE)
+ * and Rapid Radiative Transfer Model for GCM applications Parallel (RRTMGP).
+ *
+ * The original code is found at https://github.com/RobertPincus/rte-rrtmgp.
+ *
+ * Contacts: Robert Pincus and Eli Mlawer
+ * email: rrtmgp@aer.com
+ *
+ * Copyright 2015-2019,  Atmospheric and Environmental Research and
+ * Regents of the University of Colorado.  All right reserved.
+ *
+ * This C++ interface can be downloaded from https://github.com/microhh/rrtmgp_cpp
+ *
+ * Contact: Chiel van Heerwaarden
+ * email: chiel.vanheerwaarden@wur.nl
+ *
+ * Copyright 2019, Wageningen University & Research.
+ *
+ * Use and duplication is permitted under the terms of the
+ * BSD 3-clause license, see http://opensource.org/licenses/BSD-3-Clause
+ *
+ */
+
 #ifndef SOURCE_FUNCTIONS_H
 #define SOURCE_FUNCTIONS_H
 
