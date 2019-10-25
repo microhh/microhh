db6c7c23343a66e19ee9b7084afdd18146fd2c46
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2019-10-25 09:51:56 +0200

    Merge taylorgreen test and test_plot script.

--- cases/run_small_set.py
+++ cases/run_small_set.py
@@ -3,12 +3,15 @@ import sys
 sys.path.append('../python/')
 import microhh_tools as mht
 
-import taylorgreen.taylorgreen_test as tg
-import taylorgreen.taylorgreen_test_plot as tgp
+import taylorgreen.taylorgreen_test as taylorgreen
+import drycblles.drycblles_test as drycblles
 
 for prec in ['sp', 'dp']:
     for mode in ['cpu']:
         microhh_exec = 'microhh_{}_{}'.format(prec, mode)
-        tg.run_test(microhh_exec, prec, 'taylorgreen')
-        tgp.plot_test(microhh_exec, prec, 'taylorgreen')
+
+        taylorgreen.run_test(microhh_exec, prec, 'taylorgreen')
+        taylorgreen.plot_test(microhh_exec, prec, 'taylorgreen')
+
+        drycblles.run_test(microhh_exec, prec, 'drycblles')
 
