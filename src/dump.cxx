9946aea3edeb78b04643ad63a348fb36d6776595
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-02-29 17:53:57 +0100

    Remove .xyz.

--- src/dump.cxx
+++ src/dump.cxx
@@ -119,7 +119,7 @@ void Dump<TF>::save_dump(TF* data, const std::string& varname, int iotime)
     const double no_offset = 0.;
     char filename[256];
 
-    std::sprintf(filename, "%s.xyz.%07d", varname.c_str(), iotime);
+    std::sprintf(filename, "%s.%07d", varname.c_str(), iotime);
     std::ifstream infile(filename);
 
     if (infile.good())
