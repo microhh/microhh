6fe6e74a5d31ba417d51a4cd7015cda14ebdd393
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2020-04-18 23:35:53 +0200

    Split plots, adaptive time step switched off.

--- cases/conservation/conservation_test.py
+++ cases/conservation/conservation_test.py
@@ -18,18 +18,27 @@ dict_rk = {
     'rk4': {'time': {'rkorder': 4}}}
 
 dict_dt = {
-    'dt1000': {'time': {'dt': 10.  }},
-    'dt0500': {'time': {'dt':  5.  }},
-    'dt0250': {'time': {'dt':  2.5 }},
-    'dt0125': {'time': {'dt':  1.25}}}
+    'dt1000': {'time': {'dtmax': 10.  }, 'stats': {'sampletime': 10.}},
+    'dt0500': {'time': {'dtmax':  5.  }, 'stats': {'sampletime': 10.}},
+    'dt0250': {'time': {'dtmax':  2.5 }, 'stats': {'sampletime': 10.}},
+    'dt0125': {'time': {'dtmax':  1.25}, 'stats': {'sampletime': 10.}}}
 
 
 def get_data_from_nc(experiment_name):
     data = nc.Dataset('{}/conservation.default.0000000.nc'.format(experiment_name), 'r')
     time = data.variables['time'][:]
-    mom = data.group['default'].variable['u'][:,:].sum(axis=1)
-    tke = data.group['budget'].variable['tke'][:,:].sum(axis=1)
-    mass = data.group['default'].variable['s'][:,:].sum(axis=1)
+    dz = data.variables['zh'][1] - data.variables['zh'][0]
+
+    mom = dz * (data.groups['default'].variables['u'][:,:] + data.groups['default'].variables['v'][:,:]).sum(axis=1)
+    tke = 0.5 * dz * ( data.groups['default'].variables['u_2'][:,:] \
+                     + data.groups['default'].variables['v_2'][:,:] \
+                     + data.groups['default'].variables['w_2'][:,:-1]).sum(axis=1)
+    mass = dz * (data.groups['default'].variables['s'][:,:]).sum(axis=1)
+
+    mom  /= mom [1]
+    tke  /= tke [1]
+    mass /= mass[1]
+
     return time, mom, tke, mass
 
 
@@ -48,51 +57,54 @@ def plot(experiment_name):
 
     with PdfPages(file_name) as pdf:
         plt.figure()
-        plt.subplot(131)
-        plt.plot(time100_3rd[1:100], mom100_3rd [1:100] - mom100_3rd [1], 'b-' , label='rk3: dt=10.0')
-        plt.plot(time100_4th[1:100], mom100_4th [1:100] - mom100_4th [1], 'r-' , label='rk4: dt=10.0')
-        plt.plot(time200_3rd[1:200], mom200_3rd [1:200] - mom200_3rd [1], 'b--', label='rk3: dt=5.0')
-        plt.plot(time200_4th[1:200], mom200_4th [1:200] - mom200_4th [1], 'r--', label='rk4: dt=5.0')
-        plt.plot(time400_3rd[1:400], mom400_3rd [1:400] - mom400_3rd [1], 'b:' , label='rk3: dt=2.5')
-        plt.plot(time400_4th[1:400], mom400_4th [1:400] - mom400_4th [1], 'r:' , label='rk4: dt=2.5')
-        plt.plot(time800_3rd[1:800], mom800_3rd [1:800] - mom800_3rd [1], 'b:' , label='rk3: dt=1.25')
-        plt.plot(time800_4th[1:800], mom800_4th [1:800] - mom800_4th [1], 'r:' , label='rk4: dt=1.25')
+        plt.plot(time100_3rd[1:], mom100_3rd [1:] - mom100_3rd [1], 'b-' , label='rk3: dt=10.0')
+        plt.plot(time100_4th[1:], mom100_4th [1:] - mom100_4th [1], 'r-' , label='rk4: dt=10.0')
+        plt.plot(time200_3rd[1:], mom200_3rd [1:] - mom200_3rd [1], 'b--', label='rk3: dt=5.0')
+        plt.plot(time200_4th[1:], mom200_4th [1:] - mom200_4th [1], 'r--', label='rk4: dt=5.0')
+        plt.plot(time400_3rd[1:], mom400_3rd [1:] - mom400_3rd [1], 'b:' , label='rk3: dt=2.5')
+        plt.plot(time400_4th[1:], mom400_4th [1:] - mom400_4th [1], 'r:' , label='rk4: dt=2.5')
+        plt.plot(time800_3rd[1:], mom800_3rd [1:] - mom800_3rd [1], 'b:' , label='rk3: dt=1.25')
+        plt.plot(time800_4th[1:], mom800_4th [1:] - mom800_4th [1], 'r:' , label='rk4: dt=1.25')
         plt.legend(loc=0, frameon=False)
-        plt.title('momentum conservation')
-        plt.subplot(132)
-        plt.plot(time100_3rd[1:100], tke100_3rd [1:100] - tke100_3rd [1], 'b-' , label='rk3: dt=10.0')
-        plt.plot(time100_4th[1:100], tke100_4th [1:100] - tke100_4th [1], 'r-' , label='rk4: dt=10.0')
-        plt.plot(time200_3rd[1:200], tke200_3rd [1:200] - tke200_3rd [1], 'b--', label='rk3: dt=5.0')
-        plt.plot(time200_4th[1:200], tke200_4th [1:200] - tke200_4th [1], 'r--', label='rk4: dt=5.0')
-        plt.plot(time400_3rd[1:400], tke400_3rd [1:400] - tke400_3rd [1], 'b:' , label='rk3: dt=2.5')
-        plt.plot(time400_4th[1:400], tke400_4th [1:400] - tke400_4th [1], 'r:' , label='rk4: dt=2.5')
-        plt.plot(time800_3rd[1:800], tke800_3rd [1:800] - tke800_3rd [1], 'b:' , label='rk3: dt=1.25')
-        plt.plot(time800_4th[1:800], tke800_4th [1:800] - tke800_4th [1], 'r:' , label='rk4: dt=1.25')
-        plt.legend(loc=0, frameon=False)
-        plt.title('energy conservation')
-        plt.subplot(133)
-        plt.plot(time100_3rd[1:100], mass100_3rd[1:100] - mass100_3rd[1], 'b-' , label='rk3: dt=10.0')
-        plt.plot(time100_4th[1:100], mass100_4th[1:100] - mass100_4th[1], 'r-' , label='rk4: dt=10.0')
-        plt.plot(time200_3rd[1:200], mass200_3rd[1:200] - mass200_3rd[1], 'b--', label='rk3: dt=5.0')
-        plt.plot(time200_4th[1:200], mass200_4th[1:200] - mass200_4th[1], 'r--', label='rk4: dt=5.0')
-        plt.plot(time400_3rd[1:400], mass400_3rd[1:400] - mass400_3rd[1], 'b:' , label='rk3: dt=2.5')
-        plt.plot(time400_4th[1:400], mass400_4th[1:400] - mass400_4th[1], 'r:' , label='rk4: dt=2.5')
-        plt.plot(time800_3rd[1:800], mass800_3rd[1:800] - mass800_3rd[1], 'b:' , label='rk3: dt=1.25')
-        plt.plot(time800_4th[1:800], mass800_4th[1:800] - mass800_4th[1], 'r:' , label='rk4: dt=1.25')
-        plt.legend(loc=0, frameon=False)
-        plt.title('mass conservation')
+        plt.title('momentum')
         pdf.savefig()
 
-        tkeloss100_3rd = tke100_3rd[100] - tke100_3rd[1]
-        tkeloss200_3rd = tke200_3rd[200] - tke100_3rd[1]
-        tkeloss400_3rd = tke400_3rd[400] - tke100_3rd[1]
-        tkeloss800_3rd = tke800_3rd[800] - tke100_3rd[1]
-        tkeloss100_4th = tke100_4th[100] - tke100_4th[1]
-        tkeloss200_4th = tke200_4th[200] - tke100_4th[1]
-        tkeloss400_4th = tke400_4th[400] - tke100_4th[1]
-        tkeloss800_4th = tke800_4th[800] - tke100_4th[1]
+        plt.figure()
+        plt.plot(time100_3rd[1:], tke100_3rd [1:] - tke100_3rd [1], 'b-' , label='rk3: dt=10.0')
+        plt.plot(time100_4th[1:], tke100_4th [1:] - tke100_4th [1], 'r-' , label='rk4: dt=10.0')
+        plt.plot(time200_3rd[1:], tke200_3rd [1:] - tke200_3rd [1], 'b--', label='rk3: dt=5.0')
+        plt.plot(time200_4th[1:], tke200_4th [1:] - tke200_4th [1], 'r--', label='rk4: dt=5.0')
+        plt.plot(time400_3rd[1:], tke400_3rd [1:] - tke400_3rd [1], 'b:' , label='rk3: dt=2.5')
+        plt.plot(time400_4th[1:], tke400_4th [1:] - tke400_4th [1], 'r:' , label='rk4: dt=2.5')
+        plt.plot(time800_3rd[1:], tke800_3rd [1:] - tke800_3rd [1], 'b:' , label='rk3: dt=1.25')
+        plt.plot(time800_4th[1:], tke800_4th [1:] - tke800_4th [1], 'r:' , label='rk4: dt=1.25')
+        plt.legend(loc=0, frameon=False)
+        plt.title('energy')
+        pdf.savefig()
 
-        dts           = np.array([10., 5., 2.5, 1.25])
+        plt.figure()
+        plt.plot(time100_3rd[1:], mass100_3rd[1:] - mass100_3rd[1], 'b-' , label='rk3: dt=10.0')
+        plt.plot(time100_4th[1:], mass100_4th[1:] - mass100_4th[1], 'r-' , label='rk4: dt=10.0')
+        plt.plot(time200_3rd[1:], mass200_3rd[1:] - mass200_3rd[1], 'b--', label='rk3: dt=5.0')
+        plt.plot(time200_4th[1:], mass200_4th[1:] - mass200_4th[1], 'r--', label='rk4: dt=5.0')
+        plt.plot(time400_3rd[1:], mass400_3rd[1:] - mass400_3rd[1], 'b:' , label='rk3: dt=2.5')
+        plt.plot(time400_4th[1:], mass400_4th[1:] - mass400_4th[1], 'r:' , label='rk4: dt=2.5')
+        plt.plot(time800_3rd[1:], mass800_3rd[1:] - mass800_3rd[1], 'b:' , label='rk3: dt=1.25')
+        plt.plot(time800_4th[1:], mass800_4th[1:] - mass800_4th[1], 'r:' , label='rk4: dt=1.25')
+        plt.legend(loc=0, frameon=False)
+        plt.title('mass')
+        pdf.savefig()
+
+        tkeloss100_3rd = tke100_3rd[-1] - tke100_3rd[1]
+        tkeloss200_3rd = tke200_3rd[-1] - tke100_3rd[1]
+        tkeloss400_3rd = tke400_3rd[-1] - tke100_3rd[1]
+        tkeloss800_3rd = tke800_3rd[-1] - tke100_3rd[1]
+        tkeloss100_4th = tke100_4th[-1] - tke100_4th[1]
+        tkeloss200_4th = tke200_4th[-1] - tke100_4th[1]
+        tkeloss400_4th = tke400_4th[-1] - tke100_4th[1]
+        tkeloss800_4th = tke800_4th[-1] - tke100_4th[1]
+
+        dts = np.array([10., 5., 2.5, 1.25])
         tkelosses_3rd = np.array([tkeloss100_3rd, tkeloss200_3rd, tkeloss400_3rd, tkeloss800_3rd])
         tkelosses_4th = np.array([tkeloss100_4th, tkeloss200_4th, tkeloss400_4th, tkeloss800_4th])
 
