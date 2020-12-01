813e3264c6b565e3d27c760d0c2e01d0a4088c42
Bart van Stratum <bart@vanstratum.com>
2020-12-01 14:47:37 +0100

    Added advec_2i6 scheme

--- src/advec.cxx
+++ src/advec.cxx
@@ -36,6 +36,7 @@
 #include "advec_2i4.h"
 #include "advec_2i5.h"
 #include "advec_2i52.h"
+#include "advec_2i6.h"
 #include "advec_4.h"
 #include "advec_4m.h"
 
@@ -71,6 +72,8 @@ std::shared_ptr<Advec<TF>> Advec<TF>::factory(
         return std::make_shared<Advec_2i5<TF>>(masterin, gridin, fieldsin, inputin);
     else if (swadvec == "2i52")
         return std::make_shared<Advec_2i52<TF>>(masterin, gridin, fieldsin, inputin);
+    else if (swadvec == "2i6")
+        return std::make_shared<Advec_2i6<TF>>(masterin, gridin, fieldsin, inputin);
     else if (swadvec == "4")
         return std::make_shared<Advec_4<TF>>(masterin, gridin, fieldsin, inputin);
     else if (swadvec == "4m")
