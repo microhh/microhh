b800f14a79dd1eff9b9720941026437484e0fb86
Bart van Stratum <bart@vanstratum.com>
2022-02-04 12:46:27 +0100

    Start at radiation option with prescribed surface fluxes

---
+++ include/radiation_prescribed.h
@@ -0,0 +1,85 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2020 Chiel van Heerwaarden
+ * Copyright (c) 2011-2020 Thijs Heus
+ * Copyright (c) 2014-2020 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RADIATION_PRESCRIBED_H
+#define RADIATION_PRESCRIBED_H
+
+#include "radiation.h"
+
+class Master;
+class Input;
+template<typename> class Grid;
+template<typename> class Stats;
+template<typename> class Diff;
+template<typename> class Column;
+template<typename> class Dump;
+template<typename> class Cross;
+template<typename> class Field3d;
+template<typename> class Thermo;
+template<typename> class Timeloop;
+
+template<typename TF>
+class Radiation_prescribed : public Radiation<TF>
+{
+	public:
+		Radiation_prescribed(Master&, Grid<TF>&, Fields<TF>&, Input&);
+        virtual ~Radiation_prescribed() {}
+
+        void init(Timeloop<TF>&);
+        void create(
+                Input&, Netcdf_handle&, Thermo<TF>&,
+                Stats<TF>&, Column<TF>&, Cross<TF>&, Dump<TF>&);
+        void exec(Thermo<TF>&, double, Timeloop<TF>&, Stats<TF>&);
+
+        unsigned long get_time_limit(unsigned long);
+        std::vector<TF>& get_surface_radiation(std::string);
+
+        void get_radiation_field(Field3d<TF>&, std::string, Thermo<TF>&, Timeloop<TF>&)
+        { throw std::runtime_error("\"get_radiation_field()\" is not implemented in radiation_rrtmpg"); }
+		bool check_field_exists(std::string name)
+        { throw std::runtime_error("\"check_field_exists()\" is not implemented in radiation_rrtmpg"); }
+
+        // Empty functions which do nothing:
+        void exec_all_stats(
+                Stats<TF>&, Cross<TF>&, Dump<TF>&, Column<TF>&,
+                Thermo<TF>&, Timeloop<TF>&, const unsigned long, const int) {};
+        void exec_column(Column<TF>&, Thermo<TF>&, Timeloop<TF>&) {};
+
+	private:
+		using Radiation<TF>::swradiation;
+		using Radiation<TF>::master;
+		using Radiation<TF>::grid;
+		using Radiation<TF>::fields;
+		using Radiation<TF>::field3d_operators;
+
+        // Input values
+        TF lw_flux_dn;
+        TF lw_flux_up;
+        TF sw_flux_dn;
+        TF sw_flux_up;
+
+        // Surface radiative fluxes
+        std::vector<TF> lw_flux_dn_sfc;
+        std::vector<TF> lw_flux_up_sfc;
+
+        std::vector<TF> sw_flux_dn_sfc;
+        std::vector<TF> sw_flux_up_sfc;
+};
+#endif
---
+++ src/radiation_prescribed.cxx
@@ -0,0 +1,98 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2020 Chiel van Heerwaarden
+ * Copyright (c) 2011-2020 Thijs Heus
+ * Copyright (c) 2014-2020 Bart van Stratum
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdexcept>
+
+#include "radiation_prescribed.h"
+#include "grid.h"
+#include "constants.h"
+#include "input.h"
+
+template<typename TF>
+Radiation_prescribed<TF>::Radiation_prescribed(
+        Master& masterin, Grid<TF>& gridin, Fields<TF>& fieldsin, Input& inputin) :
+        Radiation<TF>(masterin, gridin, fieldsin, inputin)
+{
+    swradiation = "prescribed";
+
+    sw_flux_dn = inputin.get_item<TF>("radiation", "sw_flux_dn", "");
+    sw_flux_up = inputin.get_item<TF>("radiation", "sw_flux_up", "");
+    lw_flux_dn = inputin.get_item<TF>("radiation", "lw_flux_dn", "");
+    lw_flux_up = inputin.get_item<TF>("radiation", "lw_flux_up", "");
+}
+
+template<typename TF>
+void Radiation_prescribed<TF>::init(Timeloop<TF>& timeloop)
+{
+    auto& gd = grid.get_grid_data();
+
+    // Resize surface radiation fields.
+    sw_flux_dn_sfc.resize(gd.ijcells);
+    sw_flux_up_sfc.resize(gd.ijcells);
+    lw_flux_dn_sfc.resize(gd.ijcells);
+    lw_flux_up_sfc.resize(gd.ijcells);
+}
+
+template<typename TF>
+unsigned long Radiation_prescribed<TF>::get_time_limit(unsigned long itime)
+{
+    return Constants::ulhuge;
+}
+
+template<typename TF>
+void Radiation_prescribed<TF>::create(
+        Input& input, Netcdf_handle& input_nc, Thermo<TF>& thermo,
+        Stats<TF>& stats, Column<TF>& column, Cross<TF>& cross, Dump<TF>& dump)
+{
+    // Set surface radiation fields for land-surface model.
+    std::fill(sw_flux_dn_sfc.begin(), sw_flux_dn_sfc.end(), sw_flux_dn);
+    std::fill(sw_flux_up_sfc.begin(), sw_flux_up_sfc.end(), sw_flux_up);
+    std::fill(lw_flux_dn_sfc.begin(), lw_flux_dn_sfc.end(), lw_flux_dn);
+    std::fill(lw_flux_up_sfc.begin(), lw_flux_up_sfc.end(), lw_flux_up);
+}
+
+#ifndef USECUDA
+template<typename TF>
+void Radiation_prescribed<TF>::exec(
+        Thermo<TF>& thermo, const double time, Timeloop<TF>& timeloop, Stats<TF>& stats)
+{
+}
+
+template<typename TF>
+std::vector<TF>& Radiation_prescribed<TF>::get_surface_radiation(std::string name)
+{
+    if (name == "sw_down")
+        return sw_flux_dn_sfc;
+    else if (name == "sw_up")
+        return sw_flux_up_sfc;
+    else if (name == "lw_down")
+        return lw_flux_dn_sfc;
+    else if (name == "lw_up")
+        return lw_flux_up_sfc;
+    else
+    {
+        std::string error = "Variable \"" + name + "\" is not a valid surface radiation field";
+        throw std::runtime_error(error);
+    }
+}
+#endif
+
+template class Radiation_prescribed<double>;
+template class Radiation_prescribed<float>;
