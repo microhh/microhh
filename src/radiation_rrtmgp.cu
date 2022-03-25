cb382dbffbcf39fd9f5e273296ccf0e93c5d48f5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-03-25 16:24:28 +0100

    First step towards GPU radiation.

--- src/radiation_rrtmgp.cu
+++ src/radiation_rrtmgp.cu
@@ -21,11 +21,192 @@
 #include "radiation_rrtmgp.h"
 
 
+namespace
+{
+}
+
+
 #ifdef USECUDA
 template <typename TF>
 void Radiation_rrtmgp<TF>::exec(Thermo<TF>& thermo, double time, Timeloop<TF>& timeloop, Stats<TF>& stats)
 {
-    throw std::runtime_error("Radiation_rrtmgp is not implemented yet on the GPU");
+    auto& gd = grid.get_grid_data();
+
+    const bool do_radiation = ((timeloop.get_itime() % idt_rad == 0) && !timeloop.in_substep()) ;
+
+    if (do_radiation)
+    {
+        // Set the tendency to zero.
+        std::fill(fields.sd.at("thlt_rad")->fld.begin(), fields.sd.at("thlt_rad")->fld.end(), Float(0.));
+
+        auto t_lay = fields.get_tmp_g();
+        auto t_lev = fields.get_tmp_g();
+        auto h2o   = fields.get_tmp_g(); // This is the volume mixing ratio, not the specific humidity of vapor.
+        auto clwp  = fields.get_tmp_g();
+        auto ciwp  = fields.get_tmp_g();
+
+        // Set the input to the radiation on a 3D grid without ghost cells.
+        thermo.get_radiation_fields(*t_lay, *t_lev, *h2o, *clwp, *ciwp);
+
+        const int nmaxh = gd.imax*gd.jmax*(gd.ktot+1);
+        const int ijmax = gd.imax*gd.jmax;
+
+        // CvH these are senseless copies, can we avoid them?
+        Array_gpu<Float,2> t_lay_a(t_lay->fld, {gd.imax*gd.jmax, gd.ktot});
+        Array_gpu<Float,2> t_lev_a(t_lev->fld, {gd.imax*gd.jmax, gd.ktot+1});
+        Array_gpu<Float,1> t_sfc_a(t_lev->fld_bot, {gd.imax*gd.jmax});
+        Array_gpu<Float,2> h2o_a(h2o->fld, {gd.imax*gd.jmax, gd.ktot});
+        Array_gpu<Float,2> clwp_a(clwp->fld, {gd.imax*gd.jmax, gd.ktot});
+        Array_gpu<Float,2> ciwp_a(ciwp->fld, {gd.imax*gd.jmax, gd.ktot});
+
+        // Flux fields.
+        Array_gpu<Float,2> flux_up ({gd.imax*gd.jmax, gd.ktot+1});
+        Array_gpu<Float,2> flux_dn ({gd.imax*gd.jmax, gd.ktot+1});
+        Array_gpu<Float,2> flux_net({gd.imax*gd.jmax, gd.ktot+1});
+
+        const bool compute_clouds = true;
+
+        /*
+        try
+        {
+            if (sw_longwave)
+            {
+                exec_longwave(
+                        thermo, timeloop, stats,
+                        flux_up, flux_dn, flux_net,
+                        t_lay_a, t_lev_a, t_sfc_a, h2o_a, clwp_a, ciwp_a,
+                        compute_clouds);
+
+                calc_tendency(
+                        fields.sd.at("thlt_rad")->fld.data(),
+                        flux_up.ptr(), flux_dn.ptr(),
+                        fields.rhoref.data(), thermo.get_basestate_vector("exner").data(),
+                        gd.dz.data(),
+                        gd.istart, gd.iend, gd.jstart, gd.jend, gd.kstart, gd.kend,
+                        gd.igc, gd.jgc, gd.kgc,
+                        gd.icells, gd.ijcells,
+                        gd.imax, gd.imax*gd.jmax);
+
+                store_surface_fluxes(
+                        lw_flux_up_sfc.data(), lw_flux_dn_sfc.data(),
+                        flux_up.ptr(), flux_dn.ptr(),
+                        gd.istart, gd.iend,
+                        gd.jstart, gd.jend,
+                        gd.igc, gd.jgc,
+                        gd.icells, gd.ijcells,
+                        gd.imax);
+            }
+
+            if (sw_shortwave)
+            {
+                if (!sw_fixed_sza)
+                {
+                    // Update the solar zenith angle, and calculate new shortwave reference column
+                    const int day_of_year = int(timeloop.calc_day_of_year());
+                    const int year = timeloop.get_year();
+                    const Float seconds_after_midnight = Float(timeloop.calc_hour_of_day()*3600);
+                    this->mu0 = calc_cos_zenith_angle(lat, lon, day_of_year, seconds_after_midnight, year);
+
+                    // Calculate correction factor for impact Sun's distance on the solar "constant"
+                    const Float frac_day_of_year = Float(day_of_year) + seconds_after_midnight / Float(86400);
+                    this->tsi_scaling = calc_sun_distance_factor(frac_day_of_year);
+
+                    if (is_day(this->mu0))
+                    {
+                        const int n_bnd = kdist_sw->get_nband();
+
+                        // Set the solar zenith angle and albedo.
+                        Array<Float,2> sfc_alb_dir({n_bnd, n_col});
+                        Array<Float,2> sfc_alb_dif({n_bnd, n_col});
+
+                        for (int ibnd=1; ibnd<=n_bnd; ++ibnd)
+                        {
+                            sfc_alb_dir({ibnd, 1}) = this->sfc_alb_dir;
+                            sfc_alb_dif({ibnd, 1}) = this->sfc_alb_dif;
+                        }
+
+                        Array<Float,1> mu0({n_col});
+                        mu0({1}) = this->mu0;
+
+                        solve_shortwave_column(
+                                optical_props_sw,
+                                sw_flux_up_col, sw_flux_dn_col, sw_flux_dn_dir_col, sw_flux_net_col,
+                                sw_flux_dn_dir_inc, sw_flux_dn_dif_inc, thermo.get_basestate_vector("ph")[gd.kend],
+                                gas_concs_col,
+                                *kdist_sw,
+                                col_dry,
+                                p_lay_col, p_lev_col,
+                                t_lay_col, t_lev_col,
+                                mu0,
+                                sfc_alb_dir, sfc_alb_dif,
+                                tsi_scaling,
+                                n_lay_col);
+                    }
+                }
+
+                if (is_day(this->mu0))
+                {
+                    Array<Float,2> flux_dn_dir({gd.imax*gd.jmax, gd.ktot+1});
+
+                    exec_shortwave(
+                            thermo, timeloop, stats,
+                            flux_up, flux_dn, flux_dn_dir, flux_net,
+                            t_lay_a, t_lev_a, h2o_a, clwp_a, ciwp_a,
+                            compute_clouds);
+
+                    calc_tendency(
+                            fields.sd.at("thlt_rad")->fld.data(),
+                            flux_up.ptr(), flux_dn.ptr(),
+                            fields.rhoref.data(), thermo.get_basestate_vector("exner").data(),
+                            gd.dz.data(),
+                            gd.istart, gd.iend, gd.jstart, gd.jend, gd.kstart, gd.kend,
+                            gd.igc, gd.jgc, gd.kgc,
+                            gd.icells, gd.ijcells,
+                            gd.imax, gd.imax*gd.jmax);
+
+                    store_surface_fluxes(
+                            sw_flux_up_sfc.data(), sw_flux_dn_sfc.data(),
+                            flux_up.ptr(), flux_dn.ptr(),
+                            gd.istart, gd.iend,
+                            gd.jstart, gd.jend,
+                            gd.igc, gd.jgc,
+                            gd.icells, gd.ijcells,
+                            gd.imax);
+                }
+                else
+                {
+                    // Set the surface fluxes to zero, for (e.g.) the land-surface model.
+                    std::fill(sw_flux_up_sfc.begin(), sw_flux_up_sfc.end(), Float(0));
+                    std::fill(sw_flux_dn_sfc.begin(), sw_flux_dn_sfc.end(), Float(0));
+                }
+            }
+        } // End try block.
+        catch (std::exception& e)
+        {
+            #ifdef USEMPI
+            std::cout << "SINGLE PROCESS EXCEPTION: " << e.what() << std::endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+            #else
+            throw;
+            #endif
+        }
+        */
+
+        fields.release_tmp(t_lay);
+        fields.release_tmp(t_lev);
+        fields.release_tmp(h2o);
+        fields.release_tmp(clwp);
+        fields.release_tmp(ciwp);
+    }
+
+    // Always add the tendency.
+    add_tendency(
+            fields.st.at("thl")->fld_g,
+            fields.sd.at("thlt_rad")->fld_g,
+            gd.istart, gd.iend, gd.jstart, gd.jend, gd.kstart, gd.kend,
+            gd.icells, gd.ijcells);
+
+    stats.calc_tend(*fields.st.at("thl"), tend_name);
 }
 
 
