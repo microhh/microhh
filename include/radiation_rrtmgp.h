c3c0fe830c263942b16387ea4a20fca422500b85
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-04-01 19:32:40 +0000

    Fix CMake bug and assign Gas optics on base class pointer.

--- include/radiation_rrtmgp.h
+++ include/radiation_rrtmgp.h
@@ -203,7 +203,7 @@ class Radiation_rrtmgp : public Radiation<TF>
 
         #ifdef __CUDACC__
         std::unique_ptr<Gas_concs_gpu> gas_concs_gpu;
-        std::unique_ptr<Gas_optics_rrtmgp_gpu> kdist_lw_gpu;
+        std::unique_ptr<Gas_optics_gpu> kdist_lw_gpu;
         std::unique_ptr<Cloud_optics_gpu> cloud_lw_gpu;
         #endif
 };
