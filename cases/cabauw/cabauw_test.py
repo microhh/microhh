17a448a5976880ff6ec66a95323da3c1b1fb762c
Bart van Stratum <bart@vanstratum.com>
2022-09-19 16:31:47 +0200

    Exlude some invalid combinations from Cabauw test. All check passing now.

--- cases/cabauw/cabauw_test.py
+++ cases/cabauw/cabauw_test.py
@@ -1,10 +1,15 @@
 import sys, glob, os, shutil
+import subprocess
 import numpy as np
 
 from cabauw_input import create_case_input
 from microhh_tools import execute
 
-def test_cabauw_case(executables):
+#def execute(task):
+#    return subprocess.call(task, shell=True, executable='/bin/bash')
+
+
+def test_cabauw_case(executable):
     """
     Test Cabauw case with all options.
     """
@@ -19,47 +24,56 @@ def test_cabauw_case(executables):
 
     endtime = 10
 
-    use_homogeneous_z0 = True
-    use_homogeneous_ls = True
+    # Don't test ray-tracer for now..
     use_rt = False
+    gpt_set = '128_112'
 
-    # Test all executable for all Cabauw case options.
-    for executable in executables:
-        print('---- Testing {} ----'.format(executable))
-        for use_htessel in [True, False]:
-            for use_rrtmgp in [True, False]:
-                for homogeneous in [True, False]:
-                    for gpt_set in ['128_112', '256_224']:
+    for use_htessel in [True, False]:
+        for use_rrtmgp in [True, False]:
 
-                        print('Testing use_htessel={}, use_rrtmgp={}, gpt_set={}, homogeneous={}'.format(
-                                use_htessel, use_rrtmgp, gpt_set, homogeneous))
+            # Only test heterogeneous mode with LSM:
+            if use_htessel:
+                homogeneous_ls_opts = [True, False]
+                homogeneous_z0_opts = [True, False]
+            else:
+                homogeneous_ls_opts = [True]
+                homogeneous_z0_opts = [True]
 
-                        # Cleanup...
-                        to_rm = glob.glob('*000*')
-                        to_rm += ['cabauw_input.nc', 'stderr.log', 'stdout.log']
-                        for f in to_rm:
-                            if os.path.exists(f):
-                                os.remove(f)
+            for use_homogeneous_z0 in homogeneous_z0_opts:
+                for use_homogeneous_ls in homogeneous_ls_opts:
 
-                        # Yikes...
-                        TF = np.float32 if 'sp' in executable else np.float64
+                    print('Testing use_htessel={}, use_rrtmgp={}, gpt_set={}, homog_z0={}, homog_ls={}'.format(
+                            use_htessel, use_rrtmgp, gpt_set, use_homogeneous_z0, use_homogeneous_ls))
 
-                        # Create input files and .ini file from .ini.base.
-                        create_case_input(
-                                use_htessel,
-                                use_rrtmgp,
-                                use_rt,
-                                homogeneous,    # z0
-                                homogeneous,    # land-surface
-                                gpt_set,
-                                itot, jtot, ktot,
-                                xsize, ysize, zsize,
-                                endtime, TF)
+                    # Cleanup...
+                    to_rm = glob.glob('*000*')
+                    to_rm += ['cabauw.ini', 'cabauw_input.nc', 'stderr.log', 'stdout.log']
+                    for f in to_rm:
+                        if os.path.exists(f):
+                            os.remove(f)
 
-                        # Init and run case.
-                        err = 0
-                        err += execute('{} init cabauw'.format(executable))
-                        err += execute('{} run cabauw'.format(executable))
+                    # Yikes...
+                    TF = np.float32 if 'sp' in executable else np.float64
+
+                    # Create input files and .ini file from .ini.base.
+                    create_case_input(
+                            use_htessel,
+                            use_rrtmgp,
+                            use_rt,
+                            use_homogeneous_z0,
+                            use_homogeneous_ls,
+                            gpt_set,
+                            itot, jtot, ktot,
+                            xsize, ysize, zsize,
+                            endtime, TF)
+
+                    # Init and run case.
+                    ret = 0
+                    ret += execute('{} init cabauw'.format(executable))
+                    ret += execute('{} run cabauw'.format(executable))
+
+                    if ret != 0:
+                        raise Exception('Case failed for {}'.format(executable))
 
     
 if __name__ == '__main__':
@@ -67,12 +81,12 @@ if __name__ == '__main__':
     Test Cabauw case with all its options.
     """
 
-    modes = ['cpu', 'cpumpi', 'gpu']
+    modes = ['gpu', 'cpu', 'cpumpi']
     precs = ['dp', 'sp']
 
     executables = []
     for mode in modes:
         for prec in precs:
-            executables.append('../../build_{}_{}/microhh'.format(prec, mode))
-
-    test_cabauw_case(executables)
+            executable = '../../build_{}_{}/microhh'.format(prec, mode)
+            print('--- Testing {} ----'.format(executable))
+            test_cabauw_case(executable)
