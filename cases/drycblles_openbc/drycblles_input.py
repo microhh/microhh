d16716479a05c34b3d75ddfa207bcf74f181fdf2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2023-03-17 11:42:36 +0100

    Merge Bart's edits.

--- cases/drycblles_openbc/drycblles_input.py
+++ cases/drycblles_openbc/drycblles_input.py
@@ -89,16 +89,9 @@ lbc.s_south[:] = np.cos(4*np.pi*x/xsize)
 #lbc.s_north[:] = np.cos(4*np.pi*y/ysize)
 #lbc.s_east[:] = np.cos(4*np.pi*x/xsize)
 
-<<<<<<<<< Temporary merge branch 1
 lbc.u_west[:, :, :] = 2. * z[None, :, None] / zsize
 lbc.u_east[:, :, :] = 2. * z[None, :, None] / zsize
 lbc.v_south[:, :, :] = 2. * z[None, :, None] / zsize
 lbc.v_north[:, :, :] = 2. * z[None, :, None] / zsize
-=========
-lbc.u_west[:] = 1
-lbc.u_east[:] = 0
-lbc.v_south[:] = 0
-lbc.v_north[:] = 1
->>>>>>>>> Temporary merge branch 2
 
 lbc.to_netcdf('drycblles')
