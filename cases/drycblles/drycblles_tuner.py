de62ea5422469d3b8680239c3a0878de299b476a
Thijs Heus <6053388+thijsheus@users.noreply.github.com>
2023-11-18 20:17:23 -0500

    Making the list_wisdom.py more flexible (and getting rid of the ellipses)

--- cases/drycblles/drycblles_tuner.py
+++ cases/drycblles/drycblles_tuner.py
@@ -31,17 +31,17 @@ perm_opts = {
         }
 
 res_opts = {
+    'res_32x32x32': {'grid': {'itot': 32, 'jtot': 32, 'ktot': 32}},
     'res_64x64x64': {'grid': {'itot': 64, 'jtot': 64, 'ktot': 64}},
 
     'res_128x128x128': {'grid': {'itot': 128, 'jtot': 128, 'ktot': 128}},
     'res_256x256x128': {'grid': {'itot': 256, 'jtot': 256, 'ktot': 128}},
     'res_512x512x128': {'grid': {'itot': 512, 'jtot': 512, 'ktot': 128}},
     'res_1024x1024x128': {'grid': {'itot': 1024, 'jtot': 1024, 'ktot': 128}},
-    #'res_2048x2048x128': {'grid': {'itot': 2048, 'jtot': 2048, 'ktot': 128}}
-
-    #'res_256x256x256': {'grid': {'itot': 256, 'jtot': 256, 'ktot': 256}},
-    #'res_512x512x256': {'grid': {'itot': 512, 'jtot': 512, 'ktot': 256}},
-    #'res_1024x1024x256': {'grid': {'itot': 1024, 'jtot': 1024, 'ktot': 256}},
+    'res_2048x2048x128': {'grid': {'itot': 2048, 'jtot': 2048, 'ktot': 128}},
+    'res_256x256x256': {'grid': {'itot': 256, 'jtot': 256, 'ktot': 256}},
+    'res_512x512x256': {'grid': {'itot': 512, 'jtot': 512, 'ktot': 256}},
+    'res_1024x1024x256': {'grid': {'itot': 1024, 'jtot': 1024, 'ktot': 256}}
 }
 
 
@@ -50,14 +50,16 @@ if __name__ == '__main__':
     case_name = 'drycblles'
     mode='gpu'
     case_dir='.'
-    experiment='tuner'
 
-    #executables = ['microhh_sp_gpu', 'microhh_dp_gpu']
-    executables = ['microhh']
+
+    executables = ['microhh_sp_gpu', 'microhh_dp_gpu']
+    # executables = ['microhh']
 
     os.environ['KERNEL_LAUNCHER_TUNE'] = '*'
 
     for executable in executables:
+        experiment='tuner_'+executable
+
         mht.run_permutations(
             case_name, base_opts, mpi_opts, [perm_opts, res_opts],
             executable=executable, mode=mode, case_dir=case_dir, experiment=experiment)
