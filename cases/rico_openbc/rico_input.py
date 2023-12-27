9e3f231d5b1e856db124b46902540dc87fc5c4d3
Bart van Stratum <bart@vanstratum.com>
2023-12-27 15:31:15 +0100

    Added first version of nested RICO case.

---
+++ cases/rico_openbc/rico_input.py
@@ -0,0 +1,449 @@
+import shutil
+import stat
+import sys
+import os
+
+import netCDF4 as nc
+import xarray as xr
+import numpy as np
+
+# Custom modules, from `microhh_root/python` directory.
+import microhh_lbc_tools as mlt
+import microhh_tools as mht
+
+
+def grid_linear_stretched(dz0, alpha, ktot):
+    """
+    Simple linearly stretched grid where the grid spacing
+    in each level increases with a factor `1+alpha`.
+    """
+    dz = dz0 * (1 + alpha)**np.arange(ktot) 
+    zh = np.zeros(ktot+1)
+    zh[1:]= np.cumsum(dz)
+    z = 0.5 * (zh[1:] + zh[:-1])
+    zsize = zh[-1]
+
+    return z, zh, zsize
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        raise Exception('Provide domain number as argument (0...N)')
+    domain = int(sys.argv[1])
+
+    """
+    Case settings.
+    """
+    # Work directory. Each domain is placed in its own sub-directory.
+    work_path = '.'
+
+    # Outer domain with doubly-periodic BCs.
+    d0 = mlt.Domain(
+            name = 'dom_0',
+            itot = 96,
+            jtot = 56,
+            dx = 360,
+            dy = 360,
+            end_time = 6*3600,
+            work_path = work_path)
+
+    # Inner domains with open BCs.
+    d1 = mlt.Domain(
+            name = 'dom_1',
+            itot = 96,
+            jtot = 56,
+            dx = 120,
+            dy = 120,
+            center_in_parent = True,
+            start_offset = 0, #7200,
+            end_offset = 0,
+            parent = d0,
+            work_path = work_path)
+
+    d2 = mlt.Domain(
+            name = 'dom_2',
+            itot = 96,
+            jtot = 56,
+            dx = 40,
+            dy = 40,
+            center_in_parent = True,
+            start_offset = 0,
+            end_offset = 0,
+            parent = d1,
+            work_path = work_path)
+
+    d0.child = d1
+    d1.child = d2
+
+    float_type = np.float64
+    microhh_path = '/home/bart/meteo/models/microhh'
+    microhh_bin = '/home/bart/meteo/models/microhh/build_dp_cpumpi/microhh'
+    
+    #case = 'gcss'  # Original RICO
+    case = 'ss08' # Moist RICO from Stevens/Seifert & Seifert/Heus
+    #case = 'test' # More moist mixed-layer for testing
+
+    sw_advec = '2i5'
+    sw_sponge = True
+    n_ghost = 3
+    n_sponge = 5 if sw_sponge else 0
+    lbc_freq = 60
+
+    """
+    Generate case input.
+    """
+    # Select correct `Domain` instance. 
+    if domain == 0:
+        domain = d0
+    elif domain == 1:
+        domain = d1
+    elif domain == 2:
+        domain = d2
+    else:
+        raise Exception('Domain number out of range!')
+
+    # Create work directory.
+    if not os.path.exists(domain.work_dir):
+        os.makedirs(domain.work_dir)
+
+    # Read base settings.
+    ini = mht.Read_namelist('rico.ini.base')
+    
+    # Linearly stretched vertical grid.
+    ktot = 96
+    z, zh, zsize = grid_linear_stretched(dz0=25, alpha=0.01, ktot=ktot)
+    
+    # Define fields and vertical profiles.
+    thl   = np.zeros(ktot)
+    qt    = np.zeros(ktot)
+    u     = np.zeros(ktot)
+    ugeo  = np.zeros(ktot)
+    v     = np.zeros(ktot)
+    vgeo  = np.zeros(ktot)
+    wls   = np.zeros(ktot)
+    thlls = np.zeros(ktot)
+    qtls  = np.zeros(ktot)
+    
+    for k in range(ktot):
+        # Liquid water potential temperature: same in GCSS and SS08
+        if(z[k] < 740.):
+            thl[k] = 297.9
+        else:
+            thl[k] = 297.9 + (317.0 - 297.9)/(4000. - 740.) * (z[k] - 740.) 
+    
+        if(case == 'gcss'):
+            if(z[k] < 740.):
+                qt[k] = 16.0 + (13.8 - 16.0) / 740. * z[k]
+            elif(z[k] < 3260.):
+                qt[k] = 13.8 + (2.4 - 13.8) / (3260. - 740.) * (z[k] - 740.) 
+            else:
+                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.) 
+    
+        elif(case == 'ss08'):
+            if(z[k] < 740.):
+                qt[k] = 16.0 + (13.8 - 16.0) / 740. * z[k]
+            elif(z[k] < 3260.):
+                qt[k] = 13.8 + (4.4 - 13.8) / (3260. - 740.) * (z[k] - 740.) 
+            else:
+                qt[k] = 4.4 + (3.6 - 4.4)/(4000. - 3260.) * (z[k] - 3260.) 
+    
+        elif(case == 'test'):
+            q0 = 18.
+            q1 = 15.8
+            if(z[k] < 740.):
+                qt[k] = q0 + (q1 - q0) / 740. * z[k]
+            elif(z[k] < 3260.):
+                qt[k] = q1 + (2.4 - q1) / (3260. - 740.) * (z[k] - 740.) 
+            else:
+                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.) 
+    
+        # Subsidence
+        if(z[k] < 2260):
+            wls[k] = -0.005 * (z[k] / 2260.)
+        else:
+            wls[k] = -0.005
+    
+        # U and V component wind
+        u[k] = -9.9 + 2.0e-3 * z[k]
+        v[k] = -3.8
+        ugeo[k] = u[k]
+        vgeo[k] = v[k]
+    
+        # Advective and radiative tendency thl
+        thlls[k] = -2.5 / 86400.
+    
+        # Advective tendency qt
+        if(z[k] < 2980):
+            qtls[k] = -1.0 / 86400. + (1.3456/ 86400.) * z[k] / 2980.
+        else:
+            qtls[k] = 4e-6
+    
+    # normalize profiles to SI
+    qt  /= 1000.
+    qtls/= 1000.
+
+    """
+    Create NetCDF file.
+    """
+    def add_var(nc_group, name, dims, values):
+        nc = nc_group.createVariable(name, float_type, dims)
+        nc[:] = values
+
+    # write the data to a file
+    nc_file = nc.Dataset(
+            f'{domain.work_dir}/rico_input.nc', mode='w',
+            datamodel='NETCDF4', clobber=True)
+
+    nc_file.createDimension('z', ktot)
+    add_var(nc_file, 'z', ('z'), z)
+
+    nc_group_init = nc_file.createGroup('init');
+    add_var(nc_group_init, 'thl',    ('z'), thl)
+    add_var(nc_group_init, 'qt',     ('z'), qt)
+    add_var(nc_group_init, 'u',      ('z'), u)
+    add_var(nc_group_init, 'u_geo',  ('z'), ugeo)
+    add_var(nc_group_init, 'v',      ('z'), v)
+    add_var(nc_group_init, 'v_geo',  ('z'), vgeo)
+    add_var(nc_group_init, 'w_ls',   ('z'), wls)
+    add_var(nc_group_init, 'thl_ls', ('z'), thlls)
+    add_var(nc_group_init, 'qt_ls',  ('z'), qtls)
+
+    nc_file.close()
+    
+    # Calculate SST
+    ep = 287.04 / 461.5 
+    
+    # Surface settings
+    def esat(T):
+        c0 = 0.6105851e+03; c1 = 0.4440316e+02; c2 = 0.1430341e+01; c3 = 0.2641412e-01 
+        c4 = 0.2995057e-03; c5 = 0.2031998e-05; c6 = 0.6936113e-08; c7 = 0.2564861e-11 
+        c8 = -.3704404e-13 
+        x  = max(-80.,T-273.15)
+        return c0+x*(c1+x*(c2+x*(c3+x*(c4+x*(c5+x*(c6+x*(c7+x*c8)))))))
+    
+    def qsat(p, T):
+        return ep*esat(T)/(p-(1.-ep)*esat(T))
+    
+    ps  = 101540.
+    SST = 299.8 
+    ths = SST / (ps/1.e5)**(287.04/1005.)
+    qs  = qsat(ps, SST) 
+
+    """
+    Create LBCs & initial fields from parent domain.
+    """
+    if domain.parent is not None:
+
+        interpolation_method = 'linear'
+        fields = ['thl', 'qt', 'qr', 'nr', 'u' ,'v', 'w']
+
+        # Offset of current domain in parent domain.
+        xstart = domain.i0_in_parent * domain.parent.dx
+        ystart = domain.j0_in_parent * domain.parent.dy
+
+        """
+        Create LBCs from parent domain.
+        """
+        xz = {}
+        yz = {}
+        for fld in fields:
+            xz[fld] = xr.open_dataset(f'{domain.parent.work_dir}/{fld}.xz.nc', decode_times=False)
+            yz[fld] = xr.open_dataset(f'{domain.parent.work_dir}/{fld}.yz.nc', decode_times=False)
+
+            # Select correct time period.
+            t0 = domain.start_offset
+            t1 = domain.parent.end_time+domain.end_offset
+
+            xz[fld] = xz[fld].sel(time=slice(t0, t1))
+            yz[fld] = yz[fld].sel(time=slice(t0, t1))
+
+        time = xz[list(xz.keys())[0]].time.values
+
+        # Create Xarray Dataset with correct fields/coordinates/.. of LBCs.
+        time_lbc = np.arange(0, domain.end_time+1, lbc_freq)
+
+        lbc_ds = mlt.get_lbc_xr_dataset(
+                fields,
+                domain.xsize,
+                domain.ysize,
+                domain.itot,
+                domain.jtot,
+                z, zh[:-1],
+                time_lbc,
+                n_ghost,
+                n_sponge,
+                dtype = float_type)
+
+        # Add offset of child in parent domain, for easy interpolation with Xarray.
+
+        for v in lbc_ds.variables:
+            if 'x' in v:
+                lbc_ds[v] = lbc_ds[v] + xstart
+            if 'y' in v:
+                lbc_ds[v] = lbc_ds[v] + ystart
+    
+        print('Interpolating LBCs...')
+        for loc in ['north', 'west', 'east', 'south']:
+            for fld in fields:
+
+                # Short cuts.
+                lbc_in = lbc_ds[f'{fld}_{loc}']
+                dims = lbc_in.dims
+    
+                # Dimensions in LBC file.
+                xloc, yloc = dims[3], dims[2]
+    
+                # Dimensions in cross-section.
+                xloc_in = 'xh' if 'xh' in xloc else 'x'
+                yloc_in = 'yh' if 'yh' in yloc else 'y'
+    
+                # Switch between yz and xz crosses.
+                cc = yz if loc in ['west','east'] else xz
+    
+                # Interpolate!
+                ip = cc[fld].interp({yloc_in: lbc_ds[yloc], xloc_in: lbc_ds[xloc]}, method=interpolation_method)
+    
+                # Check if interpolation was success.
+                if np.any(np.isnan(ip[fld].values)):
+                    raise Exception('Interpolated BCs contain NaNs!')
+    
+                lbc_in[:] = ip[fld].values
+
+        # DEBUG:
+        lbc_ds.to_netcdf('test.nc')
+
+        # Write binary input files for MicroHH.
+        mlt.write_dataset_as_binaries(lbc_ds, float_type, output_dir=domain.work_dir)
+
+        """
+        Interpolate initial fields from parent domain.
+        """
+        print('Interpolating initial fields...')
+
+        # Lookup table with LES dimensions:
+        dims = {
+                'x':  np.arange(domain.dx/2, domain.xsize, domain.dx) + xstart,
+                'xh': np.arange(0, domain.xsize, domain.dx) + xstart,
+                'y':  np.arange(domain.dy/2, domain.ysize, domain.dy) + ystart,
+                'yh': np.arange(0, domain.ysize, domain.dy) + ystart}
+
+        for fld in fields:
+            ds = xr.open_dataset(f'{domain.parent.work_dir}/{fld}.nc', decode_times=False)
+
+            # Select start time.
+            ds = ds.sel(time=domain.start_offset)
+
+            # Interpolate to correct coordinates
+            dim_y = ds[fld].dims[1]  # x or xh
+            dim_x = ds[fld].dims[2]  # y or yh
+
+            ds.interp({
+                dim_x: dims[dim_x],
+                dim_y: dims[dim_y]}, method=interpolation_method)
+
+            # Save as binary file.
+            ds[fld].values.astype(float_type).tofile(f'{domain.work_dir}/{fld}_0.0000000')
+
+
+    """
+    Calculate location of nest for cross-sections.
+    """
+    if domain.child is not None:
+        # Approximate location child in parent:
+        x0 = (domain.xsize - domain.child.xsize) / 2.
+        y0 = (domain.ysize - domain.child.ysize) / 2.
+
+        # Exact location child in parent.
+        i0 = int(x0 / domain.dx)
+        j0 = int(y0 / domain.dy)
+
+        x0 = i0 * domain.dx
+        y0 = j0 * domain.dy
+
+        x1 = x0 + domain.child.xsize
+        y1 = y0 + domain.child.ysize
+
+        xz, yz = mlt.get_cross_locations_for_lbcs(
+                x0, y0,
+                x1, y1,
+                domain.dx, domain.dy,
+                domain.child.dx, domain.child.dy,
+                n_ghost, n_sponge)
+
+    """
+    Create new `.ini` file.
+    """
+    ini['grid']['itot'] = domain.itot
+    ini['grid']['jtot'] = domain.jtot
+    ini['grid']['ktot'] = ktot
+
+    ini['grid']['xsize'] = domain.xsize
+    ini['grid']['ysize'] = domain.ysize
+    ini['grid']['zsize'] = zsize
+
+    ini['boundary']['sbot[thl]'] = ths
+    ini['boundary']['sbot[qt]'] = qs
+
+    ini['advec']['swadvec'] = sw_advec
+
+    ini['time']['endtime'] = domain.end_time
+
+    if domain.parent is None:
+        # Outer domain with doubly-periodic BCs.
+        ini['pres']['sw_openbc'] = False
+        ini['boundary_lateral']['sw_openbc'] = False
+    else:
+        # Inner domain with open BCs.
+        ini['pres']['sw_openbc'] = True
+        ini['boundary_lateral']['sw_openbc'] = True
+        ini['boundary_lateral']['sw_sponge'] = sw_sponge
+        ini['boundary_lateral']['loadfreq'] = lbc_freq
+
+    if domain.child is not None:
+        ini['cross']['xz'] = list(xz)
+        ini['cross']['yz'] = list(yz)
+
+    ini['cross']['sampletime'] = lbc_freq
+
+    ini.save(f'{domain.work_dir}/rico.ini', allow_overwrite=True)
+
+
+    """
+    Copy necessary help scripts.
+    """
+    to_copy = [    
+        microhh_bin,
+        f'{microhh_path}/python/microhh_tools.py',
+        f'{microhh_path}/python/cross_to_nc.py',
+        f'{microhh_path}/python/3d_to_nc.py']
+
+    for f in to_copy:
+        target = '{}/{}'.format(domain.work_dir, f.split('/')[-1])
+        if not os.path.exists(target):
+            shutil.copy(f, domain.work_dir)
+
+#    """
+#    Yes, I'm lazy: create run script.
+#    """
+#    runscript = f'{domain.work_dir}/init_run.sh'
+#    with open(runscript, 'w') as f:
+#        f.write('mpiexec -n 8 ./microhh init rico\n\n')
+#
+#        f.write('mv thl_0.0000000 thl.0000000\n')
+#        f.write('mv qt_0.0000000 qt.0000000\n')
+#        f.write('mv qr_0.0000000 qr.0000000\n')
+#        f.write('mv nr_0.0000000 nr.0000000\n')
+#        f.write('mv u_0.0000000 u.0000000\n')
+#        f.write('mv v_0.0000000 v.0000000\n')
+#        f.write('mv w_0.0000000 w.0000000\n\n')
+#
+#        f.write('mpiexec -n 8 ./microhh run rico\n\n')
+#
+#        if domain.child is not None:
+#            tstart = domain.child.start_offset
+#            f.write(f'python cross_to_nc.py -n 12\n')
+#            f.write(f'python 3d_to_nc.py -v thl qt qr nr u v w -t0 {tstart} -t1 {tstart} -n 6')
+#
+#    st = os.stat(runscript)
+#    os.chmod(runscript, st.st_mode | stat.S_IEXEC)
---
+++ python/microhh_lbc_tools.py
@@ -0,0 +1,370 @@
+#
+#  MicroHH
+#  Copyright (c) 2011-2023 Chiel van Heerwaarden
+#  Copyright (c) 2011-2023 Thijs Heus
+#  Copyright (c) 2014-2023 Bart van Stratum
+#
+#  This file is part of MicroHH
+#
+#  MicroHH is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  MicroHH is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+import xarray as xr
+
+
+def get_cross_locations_for_lbcs(
+        xstart_nest, ystart_nest,
+        xend_nest, yend_nest,
+        dx_parent, dy_parent,
+        dx_child, dy_child,
+        n_ghost, n_sponge):
+    """
+    Get `xz` and `yz` cross-section locations for nesting.
+    NOTE: the resulting locations are a bit "spacious"
+          (i.e. might contain more locations than strictly needed).
+          This needs to be refined further...
+
+    Parameters:
+    -----------
+    xstart_nest : float
+        Start coordinate (m) child domain in x-direction.
+    ystart_nest : float
+        Start coordinate (m) child domain in y-direction.
+    xend_nest : float
+        End coordinate (m) child domain in x-direction.
+    yend_nest : float
+        End coordinate (m) child domain in y-direction.
+    dx_parent : float
+        Grid spacing parent domain in x-direction.
+    dy_parent : float
+        Grid spacing parent domain in y-direction.
+    dx_child : float
+        Grid spacing child domain in x-direction.
+    dy_child : float
+        Grid spacing child domain in y-direction.
+    n_ghost : int
+        Number of ghost cells.
+    n_buffer : int
+        Number of lateral sponge layer cells.
+    """
+
+    x0 = np.floor((xstart_nest - n_ghost  * dx_child) / dx_parent) * dx_parent - dx_parent/2
+    x1 = np.ceil((xstart_nest + n_sponge * dx_child) / dx_parent) * dx_parent + dx_parent/2
+    yzw = np.arange(x0, x1+1e-3, dx_parent)
+
+    x0 = np.floor((xend_nest - n_sponge * dx_child) / dx_parent) * dx_parent - dx_parent/2
+    x1 = np.ceil((xend_nest + n_ghost  * dx_child) / dx_parent) * dx_parent + dx_parent/2
+    yze = np.arange(x0, x1+1e-3, dx_parent)
+
+    yz = np.concatenate((yze, yzw))
+
+    y0 = np.floor((ystart_nest - n_ghost  * dy_child) / dy_parent) * dy_parent - dx_parent/2
+    y1 = np.ceil ((ystart_nest + n_sponge * dy_child) / dy_parent) * dy_parent + dx_parent/2
+    xzs = np.arange(y0, y1+1e-3, dy_parent)
+
+    y0 = np.floor((yend_nest - n_sponge * dy_child) / dy_parent) * dy_parent - dx_parent/2
+    y1 = np.ceil ((yend_nest + n_ghost  * dy_child) / dy_parent) * dy_parent + dx_parent/2
+    xzn = np.arange(y0, y1+1e-3, dy_parent)
+
+    xz = np.concatenate((xzn, xzs))
+
+    return xz, yz
+
+
+def get_lbc_xr_dataset(
+        fields,
+        xsize, ysize,
+        itot, jtot,
+        z, zh,
+        time,
+        n_ghost, n_sponge,
+        dtype=np.float64):
+    """
+    Create an Xarray Dataset that contains the correct
+    variables / dimensions / coordinates needed to
+    specify the lateral boundary conditions for an open-boundary
+    simulation with MicroHH.
+
+    Parameters:
+    ----------
+    fields : str
+        List with prognostic field names.
+    xsize : float
+        Size of LES domain in x-direction.
+    ysize : float
+        Size of LES domain in y-direction.
+    itot : int
+        Number of grid points in x-direction.
+    jtot : int
+        Number of grid points in y-direction.
+    z : np.ndarray
+        Array with the full level grid point heights.
+    zh : np.ndarray
+        Array with the half level grid point heights.
+    time : np.ndarray
+        Array with LBC times.
+    n_ghost : int
+        Number of ghost cells.
+    n_sponge : int
+        Number of lateral sponge layer cells.
+    dtype : np.float32 / np.float64
+        Datatype used by MicroHH (SP=float32, DP=float64).
+    """
+
+    nt = time.size
+    ktot = z.size
+
+    dx = xsize/itot
+    dy = ysize/jtot
+
+    x = np.arange(dx/2, xsize, dx)
+    xh = np.arange(0, xsize, dx)
+
+    y = np.arange(dy/2, ysize, dy)
+    yh = np.arange(0, ysize, dy)
+
+    nlbc = n_ghost + n_sponge
+
+    # Dimension sizes.
+    dims = {
+        'time': nt,
+        'x': itot + 2*n_ghost,
+        'xh': itot + 2*n_ghost,
+        'xgw': nlbc,
+        'xge': nlbc,
+        'xhgw': nlbc + 1,
+        'xhge': nlbc,
+        'y': jtot + 2*n_ghost,
+        'yh': jtot + 2*n_ghost,
+        'ygn': nlbc,
+        'ygs': nlbc,
+        'yhgs': nlbc + 1,
+        'yhgn': nlbc,
+        'z': ktot}
+
+    # Pad x,y dimensions with ghost cells.
+    xp = np.zeros(x.size+2*n_ghost)
+    xhp = np.zeros(x.size+2*n_ghost)
+
+    yp = np.zeros(y.size+2*n_ghost)
+    yhp = np.zeros(y.size+2*n_ghost)
+
+    xp[n_ghost:-n_ghost] = x
+    xhp[n_ghost:-n_ghost] = xh
+
+    yp[n_ghost:-n_ghost] = y
+    yhp[n_ghost:-n_ghost] = yh
+
+    for i in range(n_ghost):
+        xp[i] = x[0] - (n_ghost-i)*dx
+        xhp[i] = xh[0] - (n_ghost-i)*dx
+
+        yp[i] = y[0] - (n_ghost-i)*dy
+        yhp[i] = yh[0] - (n_ghost-i)*dy
+        
+        xp[itot+n_ghost+i] = x[-1] + (i+1)*dx
+        xhp[itot+n_ghost+i] = xh[-1] + (i+1)*dx
+
+        yp[jtot+n_ghost+i] = y[-1] + (i+1)*dy
+        yhp[jtot+n_ghost+i] = yh[-1] + (i+1)*dy
+    
+    # Define coordinates.
+    coords = {
+        'time': time,
+        'x': xp,
+        'xh': xhp,
+        'y': yp,
+        'yh': yhp,
+        'z': z,
+        'zh': zh,
+        'xgw': xp[:nlbc],
+        'xge': xp[itot+n_ghost-n_sponge:],
+        'xhgw': xhp[:nlbc+1],
+        'xhge': xhp[itot+n_ghost-n_sponge:],
+        'ygs': yp[:nlbc],
+        'ygn': yp[jtot+n_ghost-n_sponge:],
+        'yhgs': yhp[:nlbc+1],
+        'yhgn': yhp[jtot+n_ghost-n_sponge:]}
+
+    # Create Xarray dataset.
+    ds = xr.Dataset(coords=coords)
+
+    def get_dim_size(dim_in):
+        out = []
+        for dim in dim_in:
+            out.append(coords[dim].size)
+        return out
+
+    def add_var(name, dims):
+        dim_size = get_dim_size(dims)
+        ds[name] = (dims, np.zeros(dim_size, dtype=dtype))
+
+    for fld in fields:
+        if fld not in ('u','v','w'):
+            add_var(f'{fld}_west', ('time', 'z', 'y', 'xgw'))
+            add_var(f'{fld}_east', ('time', 'z', 'y', 'xge'))
+            add_var(f'{fld}_south', ('time', 'z', 'ygs', 'x'))
+            add_var(f'{fld}_north', ('time', 'z', 'ygn', 'x'))
+
+    if 'u' in fields:
+        add_var('u_west', ('time', 'z', 'y', 'xhgw'))
+        add_var('u_east', ('time', 'z', 'y', 'xhge'))
+        add_var('u_south', ('time', 'z', 'ygs', 'xh'))
+        add_var('u_north', ('time', 'z', 'ygn', 'xh'))
+
+    if 'v' in fields:
+        add_var('v_west', ('time', 'z', 'yh', 'xgw'))
+        add_var('v_east', ('time', 'z', 'yh', 'xge'))
+        add_var('v_south', ('time', 'z', 'yhgs', 'x'))
+        add_var('v_north', ('time', 'z', 'yhgn', 'x'))
+
+    if 'w' in fields:
+        add_var('w_west', ('time', 'zh', 'y', 'xgw'))
+        add_var('w_east', ('time', 'zh', 'y', 'xge'))
+        add_var('w_south', ('time', 'zh', 'ygs', 'x'))
+        add_var('w_north', ('time', 'zh', 'ygn', 'x'))
+
+    return ds
+
+
+def write_dataset_as_binaries(
+        lbc_ds,
+        dtype,
+        output_dir='.'):
+    """
+    Write all data variables from the Xarray Dataset `lbc_ds`
+    in binary format as input to MicroHH.
+
+    Parameters:
+    -----------
+    lbc_ds : Xarray.Dataset
+        Input Dataset, created by `get_lbc_xr_dataset()`
+    dtype : np.float32 || np.float64
+        Datatype
+    output_dir : str, optional
+        Output directory of binary files.
+    """
+
+    for var in lbc_ds:
+        lbc_ds[var].values.astype(dtype).tofile(f'{output_dir}/lbc_{var}.0000000')
+
+
+class Domain:
+    def __init__(
+            self, 
+            name,
+            itot,
+            jtot,
+            dx,
+            dy,
+            work_path,
+            end_time=None,
+            start_offset=0,
+            end_offset=0,
+            i0_in_parent=None,
+            j0_in_parent=None,
+            parent=None,
+            child=None,
+            center_in_parent=False):
+        """
+        Domain and nesting specification.
+
+        Parameters:
+        -----------
+        name : str
+            Name of domain. Used to create `work_path/name` work directory.
+        itot : int
+            Number of grid points in x-direction.
+        jtot : int
+            Number of grid points in y-direction.
+        dx : float
+            Grid spacing in x-direction.
+        dy : float
+            Grid spacing in y-direction.
+        work_path : str
+            Work directory.
+        end_time : int, optional
+            End time of experiment (s). Mandatory for outer domain.
+        start_offset : int, optional
+            Time offset start of child domain relative to parent.
+        end_offset : int, optional
+            Time offset end of child domain relative to parent.
+        i0_in_parent: int, optional
+            Start index x-direction child in parent domain. Can be auto-calculated with `center_in_parent=True`.
+        j0_in_parent: int, optional
+            Start index y-direction child in parent domain. Can be auto-calculated with `center_in_parent=True`.
+        parent : `Domain` instance, optional
+            Parent of current domain.
+        child : `Domain` instance, optional
+            Child of current domain.
+        center_in_parent : bool, optional
+            Center current domain in parent domain.
+        """
+
+        self.name = name
+
+        self.itot = itot
+        self.jtot = jtot
+
+        self.dx = dx
+        self.dy = dy
+
+        self.i0_in_parent = i0_in_parent
+        self.j0_in_parent = j0_in_parent
+
+        if parent is None:
+            # Outer domain.
+            if end_time is None:
+                raise Exception('You must specify `end_time` for the outer domain!')
+
+            self.start_offset = 0
+            self.end_offset = 0
+
+            self.start_time = 0
+            self.end_time = end_time
+
+        else:
+            # Inner domains.
+            if start_offset is None or end_offset is None:
+                raise Exception('You must specify `start_offset` and `end_offset` for inner domains!')
+
+            self.start_offset = start_offset
+            self.end_offset = end_offset
+
+            self.start_time = 0
+            self.end_time = parent.end_time + end_offset - start_offset
+
+        self.parent = parent
+        self.child = child
+
+        self.work_path = work_path
+        self.work_dir = f'{work_path}/{name}'
+
+        self.xsize = itot * dx
+        self.ysize = jtot * dy
+
+        if center_in_parent:
+            self.center_in_parent()
+
+
+    def center_in_parent(self):
+        """
+        Calculate `i0_in_parent` and `j0_in_parent`.
+        """
+        x0 = (self.parent.xsize - self.xsize) / 2.
+        y0 = (self.parent.ysize - self.ysize) / 2.
+
+        self.i0_in_parent = int(np.round(x0 / self.parent.dx))
+        self.j0_in_parent = int(np.round(y0 / self.parent.dy))
