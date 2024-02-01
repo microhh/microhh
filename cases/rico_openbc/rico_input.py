4a51dfeef1751c88437fef9b1b0c1650440d3f09
Bart van Stratum <bart@vanstratum.com>
2024-02-01 09:49:04 +0100

    Start at adding conditional statistics to test RICO nesting.

--- cases/rico_openbc/rico_input.py
+++ cases/rico_openbc/rico_input.py
@@ -27,7 +27,7 @@ def grid_linear_stretched(dz0, alpha, ktot):
     Simple linearly stretched grid where the grid spacing
     in each level increases with a factor `1+alpha`.
     """
-    dz = dz0 * (1 + alpha)**np.arange(ktot) 
+    dz = dz0 * (1 + alpha)**np.arange(ktot)
     zh = np.zeros(ktot+1)
     zh[1:]= np.cumsum(dz)
     z = 0.5 * (zh[1:] + zh[:-1])
@@ -117,6 +117,71 @@ def main():
     work_path = '.'
     #work_path = '/home/stratum2/scratch/rico_2i6'
 
+    """
+    Grid refinement test with:
+    - Periodic outer domain @ 100 m
+    - Open BC inner domains @ (100, 50, 33, 11) m.
+    """
+    d0 = mlt.Domain(
+            name = 'dom_0',
+            itot = 192,
+            jtot = 192,
+            dx = 100,
+            dy = 100,
+            end_time = 12*3600,
+            work_path = work_path)
+
+    d1 = mlt.Domain(
+            name = 'dom_1_100',
+            itot = 96,
+            jtot = 96,
+            dx = 100,
+            dy = 100,
+            center_in_parent = True,
+            start_offset = 9*3600,
+            end_offset = 0,
+            parent = d0,
+            work_path = work_path)
+
+    d2 = mlt.Domain(
+            name = 'dom_1_50',
+            itot = 192,
+            jtot = 192,
+            dx = 50,
+            dy = 50,
+            center_in_parent = True,
+            start_offset = 9*3600,
+            end_offset = 0,
+            parent = d0,
+            work_path = work_path)
+
+    d3 = mlt.Domain(
+            name = 'dom_1_33',
+            itot = 288,
+            jtot = 288,
+            dx = 100/3.,
+            dy = 100/3.,
+            center_in_parent = True,
+            start_offset = 9*3600,
+            end_offset = 0,
+            parent = d0,
+            work_path = work_path)
+
+    d4 = mlt.Domain(
+            name = 'dom_1_11',
+            itot = 864,
+            jtot = 864,
+            dx = 100/9.,
+            dy = 100/9.,
+            center_in_parent = True,
+            start_offset = 9*3600,
+            end_offset = 0,
+            parent = d0,
+            work_path = work_path)
+
+    d0.child = d1_100
+
+    """
     # Outer domain with doubly-periodic BCs.
     d0 = mlt.Domain(
             name = 'dom_0',
@@ -154,6 +219,7 @@ def main():
 
     d0.child = d1
     d1.child = d2
+    """
 
     """
     # Outer domain with doubly-periodic BCs.
@@ -190,10 +256,11 @@ def main():
             end_offset = 0,
             parent = d1,
             work_path = work_path)
-    """
 
     d0.child = d1
     d1.child = d2
+    """
+
 
     float_type = np.float64
     #microhh_path = '/home/bart/meteo/models/microhh'
@@ -201,12 +268,12 @@ def main():
 
     microhh_path = '/home/stratum2/models/microhh'
     microhh_bin = '/home/stratum2/models/microhh/build_dp_cpumpi/microhh'
-    
+
     #case = 'gcss'  # Original RICO
     case = 'ss08' # Moist RICO from Stevens/Seifert & Seifert/Heus
     #case = 'test' # More moist mixed-layer for testing
 
-    sw_advec = '2i5'
+    sw_advec = '2i6'
     sw_sponge = True
     n_ghost = 3
     n_sponge = 5 if sw_sponge else 0
@@ -215,13 +282,17 @@ def main():
     """
     Generate case input.
     """
-    # Select correct `Domain` instance. 
+    # Select correct `Domain` instance.
     if domain == 0:
         domain = d0
     elif domain == 1:
         domain = d1
     elif domain == 2:
         domain = d2
+    elif domain == 3:
+        domain = d3
+    elif domain == 4:
+        domain = d4
     else:
         raise Exception('Domain number out of range!')
 
@@ -231,11 +302,11 @@ def main():
 
     # Read base settings.
     ini = mht.Read_namelist('rico.ini.base')
-    
+
     # Linearly stretched vertical grid.
     ktot = 144
     z, zh, zsize = grid_linear_stretched(dz0=20, alpha=0.007, ktot=ktot)
-    
+
     # Define fields and vertical profiles.
     thl   = np.zeros(ktot)
     qt    = np.zeros(ktot)
@@ -246,61 +317,61 @@ def main():
     wls   = np.zeros(ktot)
     thlls = np.zeros(ktot)
     qtls  = np.zeros(ktot)
-    
+
     for k in range(ktot):
         # Liquid water potential temperature: same in GCSS and SS08
         if(z[k] < 740.):
             thl[k] = 297.9
         else:
-            thl[k] = 297.9 + (317.0 - 297.9)/(4000. - 740.) * (z[k] - 740.) 
-    
+            thl[k] = 297.9 + (317.0 - 297.9)/(4000. - 740.) * (z[k] - 740.)
+
         if(case == 'gcss'):
             if(z[k] < 740.):
                 qt[k] = 16.0 + (13.8 - 16.0) / 740. * z[k]
             elif(z[k] < 3260.):
-                qt[k] = 13.8 + (2.4 - 13.8) / (3260. - 740.) * (z[k] - 740.) 
+                qt[k] = 13.8 + (2.4 - 13.8) / (3260. - 740.) * (z[k] - 740.)
             else:
-                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.) 
-    
+                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.)
+
         elif(case == 'ss08'):
             if(z[k] < 740.):
                 qt[k] = 16.0 + (13.8 - 16.0) / 740. * z[k]
             elif(z[k] < 3260.):
-                qt[k] = 13.8 + (4.4 - 13.8) / (3260. - 740.) * (z[k] - 740.) 
+                qt[k] = 13.8 + (4.4 - 13.8) / (3260. - 740.) * (z[k] - 740.)
             else:
-                qt[k] = 4.4 + (3.6 - 4.4)/(4000. - 3260.) * (z[k] - 3260.) 
-    
+                qt[k] = 4.4 + (3.6 - 4.4)/(4000. - 3260.) * (z[k] - 3260.)
+
         elif(case == 'test'):
             q0 = 18.
             q1 = 15.8
             if(z[k] < 740.):
                 qt[k] = q0 + (q1 - q0) / 740. * z[k]
             elif(z[k] < 3260.):
-                qt[k] = q1 + (2.4 - q1) / (3260. - 740.) * (z[k] - 740.) 
+                qt[k] = q1 + (2.4 - q1) / (3260. - 740.) * (z[k] - 740.)
             else:
-                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.) 
-    
+                qt[k] = 2.4 + (1.8 - 2.4)/(4000. - 3260.) * (z[k] - 3260.)
+
         # Subsidence
         if(z[k] < 2260):
             wls[k] = -0.005 * (z[k] / 2260.)
         else:
             wls[k] = -0.005
-    
+
         # U and V component wind
         u[k] = -9.9 + 2.0e-3 * z[k]
         v[k] = -3.8
         ugeo[k] = u[k]
         vgeo[k] = v[k]
-    
+
         # Advective and radiative tendency thl
         thlls[k] = -2.5 / 86400.
-    
+
         # Advective tendency qt
         if(z[k] < 2980):
             qtls[k] = -1.0 / 86400. + (1.3456/ 86400.) * z[k] / 2980.
         else:
             qtls[k] = 4e-6
-    
+
     # normalize profiles to SI
     qt  /= 1000.
     qtls/= 1000.
@@ -332,25 +403,25 @@ def main():
     add_var(nc_group_init, 'qt_ls',  ('z'), qtls)
 
     nc_file.close()
-    
+
     # Calculate SST
-    ep = 287.04 / 461.5 
-    
+    ep = 287.04 / 461.5
+
     # Surface settings
     def esat(T):
-        c0 = 0.6105851e+03; c1 = 0.4440316e+02; c2 = 0.1430341e+01; c3 = 0.2641412e-01 
-        c4 = 0.2995057e-03; c5 = 0.2031998e-05; c6 = 0.6936113e-08; c7 = 0.2564861e-11 
-        c8 = -.3704404e-13 
+        c0 = 0.6105851e+03; c1 = 0.4440316e+02; c2 = 0.1430341e+01; c3 = 0.2641412e-01
+        c4 = 0.2995057e-03; c5 = 0.2031998e-05; c6 = 0.6936113e-08; c7 = 0.2564861e-11
+        c8 = -.3704404e-13
         x  = max(-80.,T-273.15)
         return c0+x*(c1+x*(c2+x*(c3+x*(c4+x*(c5+x*(c6+x*(c7+x*c8)))))))
-    
+
     def qsat(p, T):
         return ep*esat(T)/(p-(1.-ep)*esat(T))
-    
+
     ps  = 101540.
-    SST = 299.8 
+    SST = 299.8
     ths = SST / (ps/1.e5)**(287.04/1005.)
-    qs  = qsat(ps, SST) 
+    qs  = qsat(ps, SST)
 
     """
     Create LBCs & initial fields from parent domain.
@@ -405,7 +476,7 @@ def main():
             if 'y' in v:
                 lbc_ds[v] = lbc_ds[v] + ystart
 
-    
+
         print('Interpolating LBCs...')
         calls = []
         for fld in fields:
@@ -480,6 +551,11 @@ def main():
                 n_ghost, n_sponge)
 
     """
+    Create mask for conditionally sampled statistics.
+    """
+
+
+    """
     Create new `.ini` file.
     """
     ini['grid']['itot'] = domain.itot
@@ -522,7 +598,7 @@ def main():
     """
     Copy necessary help scripts.
     """
-    to_copy = [    
+    to_copy = [
         microhh_bin,
         f'{microhh_path}/python/microhh_tools.py',
         f'{microhh_path}/python/cross_to_nc.py',
