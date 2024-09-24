d94cd30bb48609dc937547fdbe37bfccfe4dd0f6
Bart van Stratum <bart@vanstratum.com>
2024-09-24 17:39:17 +0200

    Back to single `jaenschwalde` case, with options for chemistry, plume rise, etc.

--- cases/jaenschwalde/jaenschwalde_input.py
+++ cases/jaenschwalde/jaenschwalde_input.py
@@ -4,44 +4,94 @@ Profiles are slightly idealised, from 04:00 UTC ERA5 data.
 Wind is rotated to be perfectly westerly.
 """
 
-import matplotlib.pyplot as pl
 import numpy as np
+import pandas as pd
 import netCDF4 as nc
 
-pl.close('all'); pl.ion()
+import microhh_tools as mht
+import helpers as hlp
+from constants import *
 
+"""
+Settings.
+"""
 float_type = 'f8'
 
-Mair = 28.9664
-Mco2 = 44.01
-T0 = 273.15
-Rd = 287.04
-Rv = 461.5
-ep = Rd/Rv
+xsize = 6400
+ysize = 3200
+zsize = 4000
 
-def calc_esat(T):
-    a_ab = 611.21; b_ab = 18.678; c_ab = 234.5; d_ab = 257.14
-    return a_ab * np.exp((b_ab - ((T-T0) / c_ab)) * ((T-T0) / (d_ab + (T-T0))))
+itot = 64
+jtot = 32
+ktot = 96
 
-def calc_qsat(T, p):
-    esat = calc_esat(T)
-    return ep*esat / (p-(1.-ep)*esat)
-
-# Get number of vertical levels and size from .ini file
-with open('jaenschwalde.ini') as f:
-    for line in f:
-        if(line.split('=')[0]=='ktot'):
-            kmax = int(line.split('=')[1])
-        if(line.split('=')[0]=='zsize'):
-            zsize = float(line.split('=')[1])
-        if(line.split('=')[0]=='ysize'):
-            ysize = float(line.split('=')[1])
+start_hour = 4
+end_hour = 12
 
 # Enable resolved plume rise:
-sw_plume_rise = False
+sw_plume_rise = True
 
-# Vertical grid LES
-dz = zsize / kmax
+# Enable non-linear KPP chemistry:
+sw_chemistry = False
+
+
+"""
+Read base .ini file for case settings.
+"""
+ini = mht.Read_namelist('jaenschwalde.ini.base')
+
+
+"""
+Create case input.
+"""
+if (sw_chemistry):
+
+    # Read TUV output table.
+    columns = ['time', 'sza', 'jo31d', 'jh2o2', 'jno2', 'jno3', 'jn2o5', 'jch2or', 'jch2om', 'jch3o2h']
+    tuv = pd.read_table(
+            'jaenschwalde_tuv_output.txt',
+            sep='\\s+',
+            skiprows=12,
+            skipfooter=1,
+            engine='python',
+            names=columns,
+            index_col='time')
+    
+    # NOTE: `.loc` is value based, not on index.
+    tuv = tuv.loc[start_hour:end_hour]
+    
+    # Convert to seconds, and subtract starting time.
+    tuv.index *= 3600
+    tuv.index -= tuv.index.values[0]
+    
+    # Emissions (?)
+    emi_no = np.zeros(tuv.index.size)
+    emi_isop = np.zeros(tuv.index.size)
+
+    # Concentrations, for now constant with height.
+    species = {
+        'co': 1.1e-7,
+        'no': 3e-11,
+        'no2': 4e-10,
+        'hno3': 2e-9,
+        'h2o2': 1.1e-9,
+        'o3': 9e-8,
+        'hcho': 5e-10,
+        'ho2': 1.5e-11,
+        'oh': 1e-13,
+        'no3': 5e-14,
+        'n2o5': 8e-14,
+        'rooh': 3.6e-10,
+        'c3h6': 3.3e-9,
+        'ro2': 7e-12}
+else:
+    species = {}
+
+
+"""
+Define vertical grid and initial thl/qt/co2 profiles.
+"""
+dz = zsize / ktot
 z = np.arange(0.5*dz, zsize, dz)
 
 v_thl = np.array([285.7, 291.9, 293, 297.4, 307])
@@ -56,54 +106,80 @@ z_u = np.array([0, 270, 3000, 5000])
 v_u = np.array([2.3, 8.5, 0.6, 5.7])
 u = np.interp(z, z_u, v_u)
 
-v = np.zeros(kmax)
-co2 = np.zeros(kmax)
+v = np.zeros(ktot)
+co2 = np.zeros(ktot)
 
 # Surface fluxes, again idealised from ERA5.
-t0 = 4*3600
-t1 = 16*3600
-td1 = 12*3600
+t0 = start_hour*3600
+t1 = end_hour*3600
+td1 = 11.5*3600
 td2 = 14*3600
 
 time = np.linspace(t0, t1, 32)
-wthl = 0.17   * np.sin(np.pi * (time-t0) / td1)
+wthl = 0.17   * np.sin(np.pi * (time-t0-1800) / td1)
 wqt  = 8.3e-5 * np.sin(np.pi * (time-t0) / td2)
 
-# Write input NetCDF file
+
+"""
+Write input NetCDF file.
+"""
+def add_nc_var(name, dims, nc, data):
+    if dims is None:
+        var = nc.createVariable(name, np.float64)
+    else:
+        var = nc.createVariable(name, np.float64, dims)
+    var[:] = data
+
 nc_file = nc.Dataset('jaenschwalde_input.nc', mode='w', datamodel='NETCDF4', clobber=True)
 
-nc_file.createDimension('z', kmax)
-nc_z = nc_file.createVariable('z' , float_type, ('z'))
+nc_file.createDimension('z', ktot)
+add_nc_var('z', ('z'), nc_file, z)
 
+# Atmospheric input.
 nc_group_init = nc_file.createGroup('init');
-nc_u = nc_group_init.createVariable('u' , float_type, ('z'))
-nc_v = nc_group_init.createVariable('v' , float_type, ('z'))
-nc_th = nc_group_init.createVariable('thl', float_type, ('z'))
-nc_qt = nc_group_init.createVariable('qt', float_type, ('z'))
-nc_co2 = nc_group_init.createVariable('co2', float_type, ('z'))
-nc_co2_inflow = nc_group_init.createVariable('co2_inflow', float_type, ('z'))
 
-nc_z [:] = z[:]
-nc_u [:] = u[:]
-nc_v [:] = v[:]
-nc_th[:] = thl[:]
-nc_qt[:] = qt[:]
-nc_co2[:] = co2[:]
-nc_co2_inflow[:] = co2[:]
+add_nc_var('u', ('z'), nc_group_init, u)
+add_nc_var('v', ('z'), nc_group_init, v)
+add_nc_var('thl', ('z'), nc_group_init, thl)
+add_nc_var('qt', ('z'), nc_group_init, qt)
+add_nc_var('co2', ('z'), nc_group_init, co2)
+add_nc_var('co2_inflow', ('z'), nc_group_init, co2)
 
-nc_group_tdep = nc_file.createGroup('timedep');
-nc_group_tdep.createDimension("time_surface", time.size)
-nc_time_surface = nc_group_tdep.createVariable("time_surface", float_type, ("time_surface"))
-nc_thl_sbot = nc_group_tdep.createVariable("thl_sbot", float_type, ("time_surface"))
-nc_qt_sbot = nc_group_tdep.createVariable("qt_sbot" , float_type, ("time_surface"))
+nc_tdep = nc_file.createGroup('timedep');
+nc_tdep.createDimension("time_surface", time.size)
 
-nc_time_surface[:] = time
-nc_thl_sbot[:] = wthl
-nc_qt_sbot[:] = wqt
+add_nc_var('time_surface', ('time_surface'), nc_tdep, time-time[0])
+add_nc_var('thl_sbot', ('time_surface'), nc_tdep, wthl)
+add_nc_var('qt_sbot', ('time_surface'), nc_tdep, wqt)
+
+if (sw_chemistry):
+    # Chemistry input.
+    nc_chem = nc_file.createGroup('timedep_chem');
+    nc_chem.createDimension("time_chem", tuv.index.size)
+    
+    add_nc_var("time_chem", ('time_chem'), nc_chem, tuv.index)
+    add_nc_var("jo31d", ('time_chem'), nc_chem, tuv.jo31d)
+    add_nc_var("jh2o2", ('time_chem'), nc_chem, tuv.jh2o2)
+    add_nc_var("jno2", ('time_chem'), nc_chem, tuv.jno2)
+    add_nc_var("jno3", ('time_chem'), nc_chem, tuv.jno3)
+    add_nc_var("jn2o5", ('time_chem'), nc_chem, tuv.jn2o5)
+    add_nc_var("jch2or", ('time_chem'), nc_chem, tuv.jch2or)
+    add_nc_var("jch2om", ('time_chem'), nc_chem, tuv.jch2om)
+    add_nc_var("jch3o2h", ('time_chem'), nc_chem, tuv.jch3o2h)
+    add_nc_var("emi_isop", ('time_chem'), nc_chem, emi_isop)
+    add_nc_var("emi_no", ('time_chem'), nc_chem, emi_no)
+    
+    for name, value in species.items():
+        profile = np.ones(ktot, dtype=np.float64)*value
+        add_nc_var(name, ('z'), nc_group_init, profile)
+        add_nc_var('{}_inflow'.format(name), ('z'), nc_group_init, profile)
 
 nc_file.close()
 
-# Print .ini settings emissions:
+
+"""
+Define emissions.
+"""
 # Coordinates of central cooling tower (m):
 x0 = 1000
 y0 = ysize/2.
@@ -113,6 +189,7 @@ sigma_x = 25
 sigma_y = 25
 
 if sw_plume_rise:
+    # Emissions from tower height.
     z0 = 120
     sigma_z = 25
 else:
@@ -126,7 +203,10 @@ dy = 120
 ddx = 40
 
 # Strength of plumes, from the CoCO2 simulation protocol:
-strength_co2 = np.round(732.5 / 9. / Mco2, decimals=2)   # kmol(CO2) s-1
+strength_co2 = 732.5  / 9. / MCO2   # kmol(CO2) s-1
+strength_no2 = 0.0289 / 9. / MNO2   # kmol(NO2) s-1
+strength_no  = 0.359  / 9. / MNO    # kmol(NO) s-1
+strength_co  = 0.223  / 9. / MCO    # kmol(CO) s-1
 
 # Emission of heat and moisture. Numbers are from:
 # Effective pollutant emission heights for atmospheric transport modelling based on real-world information
@@ -138,66 +218,81 @@ Mp = 790      # Volume-flux (m3 s-1)
 pp = 1e5
 rhop = pp/(Rd*Tp)
 rhp = 1.
-qp = rhp * calc_qsat(Tp, pp)
+qp = rhp * hlp.calc_qsat(Tp, pp)
 
 strength_q = np.round(Mp * rhop * qp, decimals=2)
 strength_T = np.round(Mp * rhop * Tp, decimals=2)
 
-# Generate lists with model input:
-source_x0 = []
-source_y0 = []
-source_z0 = []
+# Emission input model.
+emi = hlp.Emissions()
 
+# Add emission from individual towers.
+# NOTE: Jaenschwalde has nine cooling towers, in three groups.
+#       In reality, only two groups are active at the same time.
+#       Here, we distribute the emissions over all nine towers......
 for j in range(-1,2):
     for i in range(-1,2):
-        source_x0.append( x0 + i*dx + j*ddx )
-        source_y0.append( y0 + j*dy )
-        source_z0.append( z0 )
+        x = x0 + i*dx + j*ddx
+        y = y0 + j*dy
+        z = z0
+            
+        emi.add('co2', strength_co2, True, x, y, z, sigma_x, sigma_y, sigma_z)
 
-def to_cs_list(lst, n=1):
-    """ From Python list to comma separated string """
-    out = ''
-    for i in range(n):
-        out += ','.join([str(x) for x in lst])
-        if i < n-1:
-            out += ','
-    return out
+        if (sw_chemistry):
+            emi.add('no2', strength_no2, True, x, y, z, sigma_x, sigma_y, sigma_z)
+            emi.add('no',  strength_no,  True, x, y, z, sigma_x, sigma_y, sigma_z)
+            emi.add('co',  strength_co,  True, x, y, z, sigma_x, sigma_y, sigma_z)
 
-def constant_list(value, n):
-    """ Create comma separated list with constant values """
-    lst = n*[value]
-    return to_cs_list(lst)
+        if sw_plume_rise:
+            emi.add('thl', strength_T, False, x, y, z, sigma_x, sigma_y, sigma_z)
+            emi.add('qt',  strength_q, False, x, y, z, sigma_x, sigma_y, sigma_z)
 
-if sw_plume_rise:
-    source_list = constant_list('co2', 9) + ',' + \
-                  constant_list('thl', 9) + ',' + \
-                  constant_list('qt',  9)
-    strength = constant_list(strength_co2, 9) + ',' + \
-               constant_list(strength_T, 9) + ',' + \
-               constant_list(strength_q, 9)
-    sw_vmr = constant_list('true', 9) + ',' + \
-             constant_list('false', 9) + ',' + \
-             constant_list('false',  9)
-    repeat = 3
+
+"""
+Add settings to .ini file.
+"""
+
+ini['grid']['itot'] = itot
+ini['grid']['jtot'] = jtot
+ini['grid']['ktot'] = ktot
+
+ini['grid']['xsize'] = xsize
+ini['grid']['ysize'] = ysize
+ini['grid']['zsize'] = zsize
+
+scalars = ['co2'] + list(species.keys())
+ini['advec']['fluxlimit_list'] = scalars
+ini['limiter']['limitlist'] = scalars
+ini['fields']['slist'] = scalars
+ini['boundary']['scalar_outflow'] = scalars
+
+ini['time']['endtime'] = (end_hour - start_hour) * 3600
+
+ini['source']['sourcelist'] = emi.source_list
+
+ini['chemistry']['swchemistry'] = sw_chemistry
+
+if (sw_chemistry):
+    crosslist='thl,qt,u,v,w,co2,co,no,no2,hno3,h2o2,o3,hcho,ho2,oh,no3,n2o5,rooh,c3h6,ro2,co2_path,no_path,no2_path,o3_path'
 else:
-    source_list = constant_list('co2', 9)
-    strength = constant_list(strength_co2, 9)
-    sw_vmr = constant_list('true', 9)
-    repeat = 1
+    crosslist='thl,qt,u,v,w,co2,co2_path'
 
-print('sourcelist={}'.format(source_list))
+ini['cross']['crosslist'] = crosslist
+ini['cross']['xz'] = ysize/2
 
-print('source_x0={}'.format(to_cs_list(source_x0, repeat)))
-print('source_y0={}'.format(to_cs_list(source_y0, repeat)))
-print('source_z0={}'.format(to_cs_list(source_z0, repeat)))
+ini['source']['source_x0'] = emi.x0
+ini['source']['source_y0'] = emi.y0
+ini['source']['source_z0'] = emi.z0
 
-print('sigma_x={}'.format(constant_list(sigma_x, repeat*9)))
-print('sigma_y={}'.format(constant_list(sigma_y, repeat*9)))
-print('sigma_z={}'.format(constant_list(sigma_z, repeat*9)))
+ini['source']['sigma_x'] = emi.sigma_x
+ini['source']['sigma_y'] = emi.sigma_y
+ini['source']['sigma_z'] = emi.sigma_z
 
-print('strength={}'.format(strength))
-print('swvmr={}'.format(sw_vmr))
+ini['source']['strength'] = emi.strength
+ini['source']['swvmr'] = emi.sw_vmr
 
-print('line_x={}'.format(constant_list(0, repeat*9)))
-print('line_y={}'.format(constant_list(0, repeat*9)))
-print('line_z={}'.format(constant_list(0, repeat*9)))
+ini['source']['line_x'] = emi.line_x
+ini['source']['line_y'] = emi.line_y
+ini['source']['line_z'] = emi.line_z
+
+ini.save('jaenschwalde.ini', allow_overwrite=True)
