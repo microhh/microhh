8220afcb6ab0252fd82e9c905e6aeae5a59483d4
Bart van Stratum <bart@vanstratum.com>
2024-10-25 11:18:47 +0200

    Added case tests to CI setup.

--- cases/run_ci_tests.py
+++ cases/run_ci_tests.py
@@ -4,12 +4,10 @@ import os
 sys.path.append('../python/')
 import microhh_tools as mht
 
-modes = ['cpu', 'cpumpi']
-precs = ['dp'] # ['dp', 'sp']
+modes = ['cpu']
+precs = ['dp', 'sp']
 
-#
 # Link executables to working directory
-#
 for prec in precs:
     for mode in modes:
         src = '../build_{}_{}/microhh'.format(prec, mode)
@@ -17,10 +15,8 @@ for prec in precs:
         if not os.path.exists(dst):
             os.symlink(src, dst)
 
-#
 # Short tests of init + run phase, to test if cases start
-#
-les_cases   = ['arm', 'bomex', 'drycblles', 'eady', 'gabls1', 'rico', 'sullivan2011']  # dycoms+lasso+rcemip missing
+les_cases   = ['arm', 'bomex', 'drycblles', 'gabls1', 'rico', 'sullivan2011']  # dycoms+lasso+rcemip missing
 dns_cases   = ['drycbl', 'ekman', 'drycblslope', 'moser180', 'moser600']    # prandtlslope missing
 
 les_options = {
@@ -55,9 +51,8 @@ for prec in precs:
                     dns_options, mpi_options,
                     microhh_exec, mode, case, experiment)
 
-#
+
 # Restart tests for a DNS + LES case
-#
 no_stats = {
         'stats': {'swstats': 0}, 'cross': {'swcross': 0}, 'column': {'swcolumn': 0}}
 
@@ -70,29 +65,27 @@ dns_options.update(no_stats)
 
 les_perturbations = {
         'default': {},
-        'vapor': {'thermo': {'swthermo': 'vapor'}},
         'fixed_basestate': {'thermo': {'swupdatebasestate': 0}},
-        'advec_2i3': {'advec': {'swadvec': '2i3'}},
-        'advec_2i4': {'advec': {'swadvec': '2i4'}}}
+        'advec_2i5': {'advec': {'swadvec': '2i5'}}}
 
 les_options.update(no_stats)
 
-#print('---------------------')
-#print('Running restart tests')
-#print('---------------------')
-#
-#for prec in precs:
-#    for mode in modes:
-#        microhh_exec = 'microhh_{}_{}'.format(prec, mode)
-#        experiment   = '{}_{}'.format(prec, mode)
-#
-#        err += mht.run_restart('drycbl',
-#                dns_options, mpi_options, dns_perturbations,
-#                microhh_exec, mode, 'drycbl', experiment)
-#
-#        err += mht.run_restart('bomex',
-#                les_options, mpi_options, les_perturbations,
-#                microhh_exec, mode, 'bomex', experiment)
+print('---------------------')
+print('Running restart tests')
+print('---------------------')
+
+for prec in precs:
+    for mode in modes:
+        microhh_exec = 'microhh_{}_{}'.format(prec, mode)
+        experiment   = '{}_{}'.format(prec, mode)
+
+        err += mht.run_restart('drycbl',
+                dns_options, mpi_options, dns_perturbations,
+                microhh_exec, mode, 'drycbl', experiment)
+
+        err += mht.run_restart('bomex',
+                les_options, mpi_options, les_perturbations,
+                microhh_exec, mode, 'bomex', experiment)
 
 if err > 0:
     sys.exit('One of more travis case tests failed!')
