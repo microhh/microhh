dd69205d2d92d898e75376d9e06184f9d53da031
Bart van Stratum <bart@vanstratum.com>
2025-02-05 18:22:20 +0100

    Copy back CO2 flux fields for statistics/crosses.

--- src/boundary_surface_lsm.cu
+++ src/boundary_surface_lsm.cu
@@ -1189,6 +1189,9 @@ void Boundary_surface_lsm<TF>::backward_device(Thermo<TF>& thermo)
     // Nearly all tile fields are used in the statistics:
     for (auto& tile : tiles)
         lsmk::backward_device_tile(tile.second, gd.ijcells);
+
+    cuda_safe_call(cudaMemcpy(an_co2.data(), an_co2_g, tf_memsize_ij, cudaMemcpyDeviceToHost));
+    cuda_safe_call(cudaMemcpy(resp_co2.data(), resp_co2_g, tf_memsize_ij, cudaMemcpyDeviceToHost));
 }
 
 template<typename TF>
