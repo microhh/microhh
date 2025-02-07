ce2250e2f692419ab63a54012690b970d534c7a2
Thijs Heus <6053388+thijsheus@users.noreply.github.com>
2025-02-07 10:35:56 -0500

    Fixing the base-state readin to fix a crash in the ARM case

--- include/thermo_buoy.h
+++ include/thermo_buoy.h
@@ -54,6 +54,7 @@ class Thermo_buoy : public Thermo<TF>
         void exec(const double, Stats<TF>&); ///< Add the tendencies belonging to the buoyancy.
         void create(Input&, Netcdf_handle&, Stats<TF>&, Column<TF>&, Cross<TF>&, Dump<TF>&, Timeloop<TF>&);
         unsigned long get_time_limit(unsigned long, double); ///< Compute the time limit (n/a for thermo_buoy)
+        void create_stats(Stats<TF>&) {};    ///< Initialization of the fields statistics.
 
         bool check_field_exists(std::string name);
 
