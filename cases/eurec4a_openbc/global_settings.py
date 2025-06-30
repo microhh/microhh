e8d29b082e32636bdc04f2df0e3c57f01a20549e
Bart van Stratum <bart@vanstratum.com>
2025-06-30 13:50:29 +0200

    Enable 3D hydrostatic pressure in EUREC4A setup.

--- cases/eurec4a_openbc/global_settings.py
+++ cases/eurec4a_openbc/global_settings.py
@@ -31,6 +31,6 @@ cosmo_path = '/home/scratch2/bart/eurec4a_cosmo/'
 work_path = 'develop_case'
 
 start_date = datetime(year=2020, month=2, day=1, hour=0)
-end_date   = datetime(year=2020, month=2, day=1, hour=1)
+end_date   = datetime(year=2020, month=2, day=1, hour=12)
 
 case = 'develop'
