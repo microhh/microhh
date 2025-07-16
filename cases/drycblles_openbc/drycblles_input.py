66f596d17cac7551b535190d2ed737f5cd4e3d26
Bart van Stratum <bart@vanstratum.com>
2025-07-16 12:35:49 +0200

    Working nested `drycblles` with direct LBC output from parent...

--- cases/drycblles_openbc/drycblles_input.py
+++ cases/drycblles_openbc/drycblles_input.py
@@ -109,7 +109,6 @@ Update .ini
 """
 ini['time']['endtime'] = endtime
 
-
 if domain == 'outer':
 
     xz, yz = mlt.get_cross_locations_for_lbcs(
@@ -165,85 +164,3 @@ elif domain == 'inner':
     ini['subdomain']['sw_subdomain'] = False
 
 ini.save('drycblles.ini', allow_overwrite=True)
-
-
-"""
-Create lateral boundaries
-"""
-class Timer:
-    def __init__(self):
-        self.tstart = datetime.now()
-        self.elapsed = 0
-
-    def pause(self):
-        self.elapsed += (datetime.now() - self.tstart).total_seconds()
-
-    def restart(self):
-        self.tstart = datetime.now()
-
-    def stop(self):
-        self.elapsed += (datetime.now() - self.tstart).total_seconds()
-        print(f'Elapsed: {self.elapsed}')
-
-
-if domain == 'inner':
-
-    fields = ['u', 'v', 'w', 'th', 's']
-
-    t = Timer()
-
-    # Read cross-sections.
-    xz = {}
-    yz = {}
-    for fld in fields:
-        xz[fld] = xr.open_dataset(f'outer/{fld}.xz.nc', decode_times=False)
-        yz[fld] = xr.open_dataset(f'outer/{fld}.yz.nc', decode_times=False)
-    time = xz[list(xz.keys())[0]].time.values
-
-    t.pause()
-
-    # Define nest grid.
-    x = np.arange(dx_sub/2, xsize_sub, dx_sub)
-    xh = np.arange(0, xsize_sub, dx_sub)
-    y = np.arange(dy_sub/2, ysize_sub, dy_sub)
-    yh = np.arange(0, ysize_sub, dy_sub)
-
-    lbc = lbc_input(fields, x, y, z, xh, yh, zh, time, n_ghost, n_sponge, x_offset=xstart_sub, y_offset=ystart_sub)
-
-    t.restart()
-
-    for loc in ['west', 'east', 'north', 'south']:
-        for fld in fields:
-            # Short cuts.
-            lbc_in = lbc[f'{fld}_{loc}']
-            dims = lbc_in.dims
-
-            # Dimensions in LBC file.
-            xloc, yloc = dims[3], dims[2]
-
-            # Dimensions in cross-section.
-            xloc_in = 'xh' if 'xh' in xloc else 'x'
-            yloc_in = 'yh' if 'yh' in yloc else 'y'
-
-            # Switch between yz and xz crosses.
-            cc = yz if loc in ['west','east'] else xz
-
-            # Interpolate!
-            ip = cc[fld].interp({yloc_in: lbc[yloc], xloc_in: lbc[xloc]}, method='nearest')
-
-            # Check if interpolation was success.
-            if np.any(np.isnan(ip[fld].values)):
-                raise Exception('Interpolated BCs contain NaNs!')
-
-            lbc_in[:] = ip[fld].values
-
-    t.stop()
-
-    #lbc.to_netcdf('lbc_old.nc')
-
-    # Save as binary input files.
-    for fld in fields:
-        for loc in ['west', 'east', 'north', 'south']:
-            for t, time in enumerate(lbc.time):
-                lbc_in = lbc[f'{fld}_{loc}'][t]
-                lbc_in.values.astype(dtype).tofile(f'inner/lbc_{fld}_{loc}.{int(time):07d}')
--- cases/drycblles_openbc/run_inner_outer.sh
+++ cases/drycblles_openbc/run_inner_outer.sh
@@ -3,23 +3,16 @@ rm outer/*
 
 rm *00*
 python drycblles_input.py outer
-#mpiexec -n 4 ./microhh init drycblles
-#mpiexec -n 4 ./microhh run drycblles
-
 ./microhh init drycblles
 ./microhh run drycblles
-
-#gdb --args ./microhh run drycblles
-
 python cross_to_nc.py -n 6
 mv *.nc outer/
 mv *00* outer/
 
-rm *00*
 python drycblles_input.py inner
-
-#./microhh init drycblles
-#./microhh run drycblles
-#python cross_to_nc.py -n 6
-#mv *.nc inner/
-#mv *00* inner/
+cp outer/lbc_* .
+./microhh init drycblles
+./microhh run drycblles
+python cross_to_nc.py -n 6
+mv *.nc inner/
+mv *00* inner/
